4a4a78d72ae085f22bf0cdc3cbe76bf346365394
TJ Sharp <tjsharp@pop-os.localdomain>
2021-10-26 07:25:30 -0700

    fmt

--- fuel-core/src/wasm_executor/database.rs
+++ fuel-core/src/wasm_executor/database.rs
@@ -271,7 +271,10 @@ mod tests {
         }
 
         let object_id = 4;
-        let columns = vec![FtColumn::ID(object_id), FtColumn::Address(Address::from([0x04; 32]))];
+        let columns = vec![
+            FtColumn::ID(object_id),
+            FtColumn::Address(Address::from([0x04; 32])),
+        ];
         let bytes = vec![0u8, 1u8, 2u8, 3u8];
         db.put_object(THING1_TYPE, columns, bytes.clone());
 
