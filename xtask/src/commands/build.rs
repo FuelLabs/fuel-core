2ac466c87ff0935539d9d4c78948d645ebf94d98
John Cub <alicancubukcuoglu@gmail.com>
2021-12-23 19:21:04 +0300

    Add `dump` xtask

--- xtask/src/commands/build.rs
+++ xtask/src/commands/build.rs
@@ -1,15 +1,10 @@
-use fuel_core::schema::build_schema;
+use super::dump::dump_schema;
 use std::{
     env,
-    fs::{self, File},
-    io::Write,
     path::{Path, PathBuf},
     process::Command,
 };
 use structopt::StructOpt;
-
-// stored in the root of the workspace
-const SCHEMA_URL: &str = "../assets/schema.sdl";
 
 #[derive(Debug, StructOpt)]
 pub struct BuildCommand {}
@@ -30,39 +25,10 @@ pub fn cargo_build_and_dump_schema() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-fn dump_schema() -> Result<(), Box<dyn std::error::Error>> {
-    let assets = env::var("CARGO_MANIFEST_DIR")
-        .map(PathBuf::from)
-        .map(|f| {
-            let f = f.as_path().join(SCHEMA_URL);
-            let dir = f.parent().expect("Failed to read assets dir");
-            fs::create_dir_all(dir).expect("Failed to create assets dir");
-
-            f
-        })
-        .expect("Failed to fetch assets path");
-
-    File::create(&assets)
-        .and_then(|mut f| {
-            f.write_all(build_schema().finish().sdl().as_bytes())?;
-            f.sync_all()
-        })
-        .expect("Failed to write SDL schema to temporary file");
-
-    Ok(())
-}
-
 fn project_root() -> PathBuf {
     Path::new(&env!("CARGO_MANIFEST_DIR"))
         .ancestors()
         .nth(1)
         .unwrap()
         .to_path_buf()
-}
-
-/// ensures that latest schema is always committed
-#[test]
-fn is_latest_schema_committed() {
-    let current_content = fs::read(SCHEMA_URL).unwrap();
-    assert_eq!(current_content, build_schema().finish().sdl().as_bytes());
 }
---
+++ xtask/src/commands/dump.rs
@@ -0,0 +1,43 @@
+use fuel_core::schema::build_schema;
+use std::{
+    env,
+    fs::{self, File},
+    io::Write,
+    path::PathBuf,
+};
+use structopt::StructOpt;
+
+// stored in the root of the workspace
+const SCHEMA_URL: &str = "../assets/schema.sdl";
+
+#[derive(Debug, StructOpt)]
+pub struct DumpCommand {}
+
+pub fn dump_schema() -> Result<(), Box<dyn std::error::Error>> {
+    let assets = env::var("CARGO_MANIFEST_DIR")
+        .map(PathBuf::from)
+        .map(|f| {
+            let f = f.as_path().join(SCHEMA_URL);
+            let dir = f.parent().expect("Failed to read assets dir");
+            fs::create_dir_all(dir).expect("Failed to create assets dir");
+
+            f
+        })
+        .expect("Failed to fetch assets path");
+
+    File::create(&assets)
+        .and_then(|mut f| {
+            f.write_all(build_schema().finish().sdl().as_bytes())?;
+            f.sync_all()
+        })
+        .expect("Failed to write SDL schema to temporary file");
+
+    Ok(())
+}
+
+/// ensures that latest schema is always committed
+#[test]
+fn is_latest_schema_committed() {
+    let current_content = fs::read(SCHEMA_URL).unwrap();
+    assert_eq!(current_content, build_schema().finish().sdl().as_bytes());
+}
