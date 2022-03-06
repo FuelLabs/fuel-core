43a386643dc7e2dc140112f46e40ea826ca71984
TJ Sharp <tjsharp@pop-os.localdomain>
2022-03-06 12:58:49 -0800

    Switch to axum

--- fuel-indexer/indexer/src/api.rs
+++ fuel-indexer/indexer/src/api.rs
@@ -1,16 +1,20 @@
 use crate::{IndexerConfig, SchemaManager};
 use async_std::sync::{Arc, RwLock};
+use axum::{
+    extract::{Extension, Json, Path},
+    routing::post,
+    AddExtensionLayer, Router,
+};
 use fuel_indexer_schema::db::{
     graphql::{GraphqlError, GraphqlQueryBuilder},
     tables::Schema,
 };
+use http::StatusCode;
 use serde::Deserialize;
+use serde_json::Value;
 use thiserror::Error;
 use tokio_postgres::{connect, types::Type, NoTls};
 use tracing::error;
-use warp::{http::StatusCode, reply::with_status, Filter, Reply};
-
-const MAX_JSON_BODY: u64 = 1024 * 16;
 
 #[derive(Debug, Error)]
 enum APIError {
@@ -18,48 +22,43 @@ enum APIError {
     PostgresError(#[from] tokio_postgres::Error),
     #[error("Query builder error {0:?}")]
     GraphqlError(#[from] GraphqlError),
+    #[error("Malformed query")]
+    MalformedQuery,
     #[error("Unexpected DB type {0:?}")]
     UnexpectedDBType(String),
-    #[error("Unexpected JSON type {0:?}")]
-    UnexpectedJSONType(String),
     #[error("Serde Error {0:?}")]
     SerdeError(#[from] serde_json::Error),
 }
 
-#[derive(Clone, Deserialize)]
+#[derive(Clone, Debug, Deserialize)]
 struct Query {
     query: String,
     #[allow(unused)] // TODO
     params: String,
 }
 
-fn extract_json() -> impl Filter<Extract = (Query,), Error = warp::Rejection> + Clone {
-    warp::body::content_length_limit(MAX_JSON_BODY).and(warp::body::json())
-}
-
 async fn query_graph(
-    name: String,
-    query: Query,
-    manager: Arc<RwLock<SchemaManager>>,
-    config: Arc<IndexerConfig>,
-) -> Result<impl warp::Reply, warp::Rejection> {
+    Path(name): Path<String>,
+    Json(query): Json<Query>,
+    Extension(config): Extension<Arc<IndexerConfig>>,
+    Extension(manager): Extension<Arc<RwLock<SchemaManager>>>,
+) -> (StatusCode, Json<Value>) {
     match manager.read().await.load_schema(&name) {
         Ok(schema) => match run_query(query, schema, config).await {
-            Ok(response) => Ok(with_status(response, StatusCode::OK).into_response()),
+            Ok(response) => (StatusCode::OK, Json(response)),
             Err(e) => {
                 error!("Query error {e:?}");
-                Ok(with_status(
-                    "Internal Server Error".to_string(),
-                    StatusCode::INTERNAL_SERVER_ERROR,
-                )
-                .into_response())
+                let res = Json(Value::String("Internal Server Error".into()));
+                (StatusCode::INTERNAL_SERVER_ERROR, res)
             }
         },
-        Err(e) => Ok(with_status(
-            format!("The graph {} was not found ({:?})", name, e),
+        Err(e) => (
             StatusCode::NOT_FOUND,
-        )
-        .into_response()),
+            Json(Value::String(format!(
+                "The graph {} was not found ({:?})",
+                name, e
+            ))),
+        ),
     }
 }
 
@@ -70,22 +69,17 @@ impl GraphQlApi {
         let sm = SchemaManager::new(&config.database_url).expect("SchemaManager create failed");
         let schema_manager = Arc::new(RwLock::new(sm));
         let config = Arc::new(config);
-        let mgr_filter = warp::any().map(move || schema_manager.clone());
+        let listen_on = config.listen_endpoint.clone();
 
-        let a_config = config.clone();
-        let rquery = warp::post()
-            .and(warp::path("graph"))
-            .and(warp::path::param())
-            .and(extract_json())
-            .and(mgr_filter.clone())
-            .and_then(
-                move |name: String, query: Query, manager: Arc<RwLock<SchemaManager>>| {
-                    let cfg = a_config.clone();
-                    query_graph(name, query, manager, cfg)
-                },
-            );
+        let app = Router::new()
+            .route("/graph/:name", post(query_graph))
+            .layer(AddExtensionLayer::new(schema_manager))
+            .layer(AddExtensionLayer::new(config));
 
-        warp::serve(rquery).run(config.listen_endpoint).await
+        axum::Server::bind(&listen_on)
+            .serve(app.into_make_service())
+            .await
+            .expect("Service failed to start");
     }
 }
 
@@ -93,7 +87,7 @@ async fn run_query(
     query: Query,
     schema: Schema,
     config: Arc<IndexerConfig>,
-) -> Result<String, APIError> {
+) -> Result<Value, APIError> {
     let (client, conn) = connect(&config.database_url, NoTls).await?;
     tokio::spawn(async move {
         if let Err(e) = conn.await {
@@ -109,19 +103,17 @@ async fn run_query(
 
     let mut response = Vec::new();
     for row in results {
-        assert_eq!(row.columns().len(), 1);
+        if row.columns().len() != 1 {
+            return Err(APIError::MalformedQuery);
+        }
         let c = &row.columns()[0];
         let value = match c.type_() {
-            &Type::JSON => row.get::<&str, serde_json::Value>(c.name()),
+            &Type::JSON => row.get::<&str, Value>(c.name()),
             t => return Err(APIError::UnexpectedDBType(format!("{t:?}"))),
         };
 
-        if let serde_json::Value::Object(obj) = value {
-            response.push(obj.clone());
-        } else {
-            return Err(APIError::UnexpectedJSONType(format!("{value:?}")));
-        }
+        response.push(value);
     }
 
-    Ok(serde_json::to_string(&response)?)
+    Ok(Value::Array(response))
 }
