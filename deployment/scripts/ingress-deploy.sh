dc922ffa63a56467c90282d9d3625b4eb5a2e6c5
rfuelsh <yuvraj.singh@fuel.sh>
2022-03-22 09:55:25 -0400

    Adding prometheus helm chart deploy script

--- deployment/scripts/ingress-deploy.sh
+++ deployment/scripts/ingress-deploy.sh
@@ -4,8 +4,10 @@ set -o allexport && source .env && set +o allexport
 
 if [ "${k8s_provider}" == "eks" ]; then
     echo " ...."
+    echo "Updating your kube context locally ...."
     aws eks update-kubeconfig --name ${TF_VAR_eks_cluster_name}
     cd ../ingress/${k8s_provider}
+    echo "Deploying cert-manager helm chart to ${TF_VAR_eks_cluster_name} ...."
     kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/aws/1.21/deploy.yaml
     helm repo add jetstack https://charts.jetstack.io
     helm repo update
@@ -17,6 +19,7 @@ if [ "${k8s_provider}" == "eks" ]; then
     mv ingress.yaml ingress.template
     envsubst < ingress.template > ingress.yaml
     rm ingress.template
+    echo "Deploying ingress to ${TF_VAR_eks_cluster_name} ...."
     kubectl apply -f ingress.yaml
 else
    echo "You have inputted a non-supported kubernetes provider in your .env"
