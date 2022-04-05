a9a9a2ad6ad737ff257a767d4a0d316911cacf45
TJ Sharp <tj.tommyj@gmail.com>
2022-04-05 17:34:05 -0400

    fix

--- fuel-indexer/api_server/src/lib.rs
+++ fuel-indexer/api_server/src/lib.rs
@@ -11,7 +11,7 @@ use axum::{
 };
 use diesel::sql_types::Text;
 use diesel::{Connection, QueryableByName, RunQueryDsl};
-use fuel_indexer_schema::{GraphqlError, GraphqlQueryBuilder, Schema};
+use fuel_indexer_schema::graphql::{GraphqlError, GraphqlQueryBuilder, Schema};
 use serde::Deserialize;
 use serde_json::Value;
 use std::collections::HashMap;
