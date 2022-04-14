601425ae2a2032d3d42fd789a2fee20f7885b18f
Johann <randomlikeabledude@gmail.com>
2022-04-13 21:34:21 -0600

    Fmt + Clippy

--- fuel-core/src/schema/tx/types.rs
+++ fuel-core/src/schema/tx/types.rs
@@ -226,7 +226,11 @@ impl Transaction {
 
         let transaction_in_pool = txpool.find(&[self.0.id()]).await;
 
-        if  transaction_in_pool.get(0).and_then(|y| y.as_ref()).is_some() {
+        if transaction_in_pool
+            .get(0)
+            .and_then(|tx| tx.as_ref())
+            .is_some()
+        {
             // TODO, fix this part where submitted time is lied about
             let time = chrono::Utc::now();
             Ok(Some(TransactionStatus::Submitted(SubmittedStatus(time))))
