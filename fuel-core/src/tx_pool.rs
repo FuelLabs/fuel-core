b8ccda61e606307e748260bd32f0049045677bff
Johann <randomlikeabledude@gmail.com>
2022-04-18 16:34:42 -0600

    last fix

--- fuel-core/src/tx_pool.rs
+++ fuel-core/src/tx_pool.rs
@@ -141,13 +141,11 @@ impl TxPool {
             },
             transactions: includable_txs,
         };
-
         // immediately execute block
         self.executor
             .execute(&mut block, ExecutionMode::Production)
             .await
             .map_err(Error::Execution)?;
-
         Ok(tx_id)
     }
 
