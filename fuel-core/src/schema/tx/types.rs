7ab9b57e185c2212402d68f4d9766aa11b0d974a
ControlCplusControlV <44706811+ControlCplusControlV@users.noreply.github.com>
2022-04-26 18:13:15 -0600

    Remove redundant check
    Co-authored-by: Brandon Kite <brandonkite92@gmail.com>

--- fuel-core/src/schema/tx/types.rs
+++ fuel-core/src/schema/tx/types.rs
@@ -221,7 +221,7 @@ impl Transaction {
 
         let transaction_in_pool = txpool.find_one(&self.0.id()).await;
 
-        if transaction_in_pool.is_some() && db.get_tx_status(&self.0.id()).is_err() {
+        if transaction_in_pool.is_some() {
             let time = transaction_in_pool.unwrap().submited_time();
             Ok(Some(TransactionStatus::Submitted(SubmittedStatus(time))))
         } else {
