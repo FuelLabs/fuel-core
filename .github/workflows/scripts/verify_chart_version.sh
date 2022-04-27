6e6c0080ed1262fa8c88dbbb05b9f4f8b824776f
Brandon Kite <brandonkite92@gmail.com>
2022-04-27 12:59:23 -0700

    more dasel fixing (#288)

--- .github/workflows/scripts/verify_chart_version.sh
+++ .github/workflows/scripts/verify_chart_version.sh
@@ -10,7 +10,7 @@ status() {
     printf "\e[32m\e[1m%${WIDTH}s\e[0m %s\n" "$1" "$2"
 }
 # install dasel
-curl -sSLf "$(curl -sSLf https://api.github.com/repos/tomwright/dasel/releases/latest | grep browser_download_url | grep linux_amd64 | cut -d\" -f 4)" -L -o dasel 
+curl -sSLf "https://github.com/TomWright/dasel/releases/download/v1.24.3/dasel_linux_amd64" -L -o dasel
 chmod +x dasel
 mv ./dasel /usr/local/bin/dasel
 # check appVersion with fuel-core
