11be66ea7944a2ba9e7af2fa3fdc15190b21e7d8
leviathanbeak88 <elvisdedic@outlook.com>
2022-05-05 12:32:55 +0200

    remove encode and decode methods directly from GossipsubMessage enum

--- fuel-p2p/src/gossipsub/messages.rs
+++ fuel-p2p/src/gossipsub/messages.rs
@@ -1,20 +1,7 @@
 use serde::{Deserialize, Serialize};
-use std::io;
 
 #[derive(Serialize, Deserialize, PartialEq, Debug)]
 pub enum GossipsubMessage {
     BroadcastNewTx,
     BroadcastNewBlock,
-}
-
-impl GossipsubMessage {
-    pub fn encode<T: serde::Serializer<Ok = ()>>(&self, serializer: T) -> Result<(), io::Error> {
-        serde::Serialize::serialize(self, serializer)
-            .map_err(|e| io::Error::new(io::ErrorKind::Other, e.to_string()))
-    }
-
-    pub fn decode<'a, T: serde::Deserializer<'a>>(deserializer: T) -> Result<Self, io::Error> {
-        serde::Deserialize::deserialize(deserializer)
-            .map_err(|e| io::Error::new(io::ErrorKind::Other, e.to_string()))
-    }
 }
