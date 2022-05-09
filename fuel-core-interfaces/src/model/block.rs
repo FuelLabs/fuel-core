680e6b0e5a1f78633244a9bf2b1fc13a684d5842
leviathanbeak88 <elvisdedic@outlook.com>
2022-05-09 15:14:53 +0200

    serialize time

--- fuel-core-interfaces/src/model/block.rs
+++ fuel-core-interfaces/src/model/block.rs
@@ -1,5 +1,5 @@
 pub use super::BlockHeight;
-use chrono::{DateTime, TimeZone, Utc};
+use chrono::{serde::ts_seconds, DateTime, TimeZone, Utc};
 use fuel_crypto::Hasher;
 use fuel_tx::{Address, Bytes32, Transaction};
 
@@ -22,6 +22,7 @@ pub struct FuelBlockHeader {
     /// Merkle root of transactions.
     pub transactions_root: Bytes32,
     /// The block producer time
+    #[serde(with = "ts_seconds")]
     pub time: DateTime<Utc>,
     /// The block producer public key
     pub producer: Address,
