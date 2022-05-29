b6f9d021fcbf8605b7ba03c6221b6634e5378379
leviathanbeak88 <elvisdedic@outlook.com>
2022-05-29 15:00:03 +0200

    add transaction parameters to chain info

--- fuel-client/src/client/schema/chain.rs
+++ fuel-client/src/client/schema/chain.rs
@@ -1,6 +1,22 @@
 use crate::client::schema::{block::Block, schema, U64};
 
 #[derive(cynic::QueryFragment, Debug)]
+#[cynic(schema_path = "./assets/schema.sdl")]
+pub struct ConsensusParameters {
+    pub contract_max_size: U64,
+    pub max_inputs: U64,
+    pub max_outputs: U64,
+    pub max_witnesses: U64,
+    pub max_gas_per_tx: U64,
+    pub max_script_length: U64,
+    pub max_script_data_length: U64,
+    pub max_static_contracts: U64,
+    pub max_storage_slots: U64,
+    pub max_predicate_length: U64,
+    pub max_predicate_data_length: U64,
+}
+
+#[derive(cynic::QueryFragment, Debug)]
 #[cynic(schema_path = "./assets/schema.sdl", graphql_type = "Query")]
 pub struct ChainQuery {
     pub chain: ChainInfo,
@@ -13,6 +29,7 @@ pub struct ChainInfo {
     pub name: String,
     pub peer_count: i32,
     pub latest_block: Block,
+    pub transaction_parameters: ConsensusParameters,
 }
 
 #[cfg(test)]
