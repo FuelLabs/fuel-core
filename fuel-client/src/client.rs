bb51acf31a0aa3005c7645ea8ca9aa56b17a8b6e
Elvis <elvisdedic@outlook.com>
2022-05-31 21:56:13 +0300

    add node info and version (#372)
    * add node info to graphql schema
    
    * fix field name in schema
    
    * rename to node_info
    
    * add node_info to client
    
    * add node info and version snapshots
    
    * remove version from tx
    
    * add node version to node info
    
    * add node version to node info in snapshot
    
    * remove node version test
    
    * add e2e node_info test

--- fuel-client/src/client.rs
+++ fuel-client/src/client.rs
@@ -86,6 +86,11 @@ impl FuelClient {
         self.query(query).await.map(|r| r.health)
     }
 
+    pub async fn node_info(&self) -> io::Result<schema::node_info::NodeInfo> {
+        let query = schema::node_info::QueryNodeInfo::build(());
+        self.query(query).await.map(|r| r.node_info)
+    }
+
     /// Default dry run, matching the exact configuration as the node
     pub async fn dry_run(&self, tx: &Transaction) -> io::Result<Vec<Receipt>> {
         self.dry_run_opt(tx, None).await
