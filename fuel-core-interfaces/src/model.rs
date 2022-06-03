faa538e17e6a283290d82ceff2550763fefdd2ca
leviathanbeak88 <elvisdedic@outlook.com>
2022-06-03 20:42:55 +0200

    add vote placeholder

--- fuel-core-interfaces/src/model.rs
+++ fuel-core-interfaces/src/model.rs
@@ -2,10 +2,12 @@ mod block;
 mod block_height;
 mod coin;
 mod txpool;
+mod vote;
 
 pub use block::{FuelBlock, FuelBlockDb, FuelBlockHeader};
 pub use block_height::BlockHeight;
 pub use coin::{Coin, CoinStatus};
 pub use txpool::{ArcTx, TxInfo};
+pub use vote::Vote;
 
 pub type DaBlockHeight = u64;
