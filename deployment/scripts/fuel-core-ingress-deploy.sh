73b23cbff4d0f09e0d537a805c53d7f989c12f76
rfuelsh <yuvraj.singh@fuel.sh>
2022-06-03 10:28:40 -0400

    removing monitoring ingress and prod issuer

--- deployment/scripts/fuel-core-ingress-deploy.sh
+++ deployment/scripts/fuel-core-ingress-deploy.sh
@@ -10,21 +10,11 @@ if [ "${k8s_provider}" == "eks" ]; then
     echo "Updating your kube context locally ...."
     aws eks update-kubeconfig --name ${TF_VAR_eks_cluster_name}
     cd ../ingress/${k8s_provider}
-    echo "Deploying production cluster issuer to ${TF_VAR_eks_cluster_name} ...."
-    mv prod-issuer.yaml prod-issuer.template
-    envsubst < prod-issuer.template > prod-issuer.yaml
-    rm prod-issuer.template
-    kubectl apply -f prod-issuer.yaml
     echo "Deploying fuel-core ingress to ${TF_VAR_eks_cluster_name} ...."
     mv fuel-core-ingress.yaml fuel-core-ingress.template
     envsubst < fuel-core-ingress.template > fuel-core-ingress.yaml
     rm fuel-core-ingress.template
     kubectl apply -f fuel-core-ingress.yaml
-    echo "Deploying monitoring ingress to ${TF_VAR_eks_cluster_name} ...."
-    mv monitoring-ingress.yaml monitoring-ingress.template
-    envsubst < monitoring-ingress.template > monitoring-ingress.yaml
-    rm monitoring-ingress.template
-    kubectl apply -f monitoring-ingress.yaml
 else
    echo "You have inputted a non-supported kubernetes provider in your .env"
 fi
