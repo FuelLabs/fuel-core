ab3105ebc016dedd3050d902f83be004580d5f52
Brandon Kite <brandonkite92@gmail.com>
2022-06-16 00:27:24 -0700

    checkpoint

--- ci/local-registry/generate-config-patch.sh
+++ ci/local-registry/generate-config-patch.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
+# This script is used to augment .cargo/config during CI to support publishing to a local registry
+# We use patches in .cargo/config to avoid using --allow-dirty when publishing, since we need to
+# modify just the workspace members to use our local registry instead of crates-io.
 
 # Setup local registry configuration
-echo ""
-echo ""
-echo "[registries]"
-echo "local-registry = { index = \"file:///tmp/local-registry\" }"
-echo ""
+
+dasel put string -f .cargo/config.toml -p toml ".registries.local-registry" '{ index = "file:///tmp/local-registry" }'
 
 # Generate patch definitions for all workspace members to use local registry
-echo "[patch.crates-io]"
-VERSION=$(< "fuel-core/Cargo.toml" dasel -r toml 'package.version')
-for member in $(< Cargo.toml dasel -r toml -w json 'workspace.members' | jq -r ".[]" | xargs -L 1 basename)
+for member in $(< Cargo.toml dasel -r toml -w json 'workspace.members' | jq -r ".[]")
 do
-  # non-publishable packages should have a version of 0.0.0,
-  # but overriding their version is harmless since they aren't used by publishable packages.
-  echo "$member = { version = \"$VERSION\", registry = \"local-registry\" }"
+  mkdir "$member/.cargo"
+  touch "$member/.cargo/config.toml"
+
+  PACKAGE=$(< "$member/Cargo.toml" dasel -r toml 'package.name')
+  VERSION=$(< "$member/Cargo.toml" dasel -r toml 'package.version')
+  # for any packages that are publishable
+  if ! grep -q ^"publish = false" "$member/Cargo.toml"; then
+    echo "$PACKAGE = { path = \"$member\", version = \"$VERSION\", registry = \"local-registry\" }";
+  fi
 done
