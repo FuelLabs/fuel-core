942dac2fad2b85b8624a9a0aa1f158e9982b22d4
Brandon Kite <brandonkite92@gmail.com>
2022-06-16 01:35:24 -0700

    checkpoint

--- ci/local-registry/generate-config-patch.sh
+++ ci/local-registry/generate-config-patch.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+set -e
+
 # This script is used to augment .cargo/config during CI to support publishing to a local registry
 # We use patches in .cargo/config to avoid using --allow-dirty when publishing, since we need to
 # modify just the workspace members to use our local registry instead of crates-io.
@@ -8,23 +11,20 @@ dasel put object -f .cargo/config.toml -p toml -t string "registries.local-regis
 
 # Generate patch definitions for all workspace members to use local registry
 for member in $(< Cargo.toml dasel -r toml -w json 'workspace.members' | jq -r ".[]"); do
-  mkdir "$member/.cargo"
+  echo "$member"
+  mkdir -p "$member/.cargo"
   touch "$member/.cargo/config.toml"
 
-  PACKAGE=$(< "$member/Cargo.toml" dasel -r toml 'package.name')
-  VERSION=$(< "$member/Cargo.toml" dasel -r toml 'package.version')
-
   for dependency in $(< "$member/Cargo.toml" dasel -r toml -m '.dependencies.-'); do
-    PATH=$(< "fuel-core/Cargo.toml" dasel -r toml -n ".dependencies.$dependency.path")
-    DEP_VERSION=$(< "fuel-core/Cargo.toml" dasel -r toml -n ".dependencies.$dependency.version")
-    if [[ $PATH_TYPE != 'null' && $DEP_VERSION != 'null' ]]; then
-      # add to patch config if dep is versioned and also has local path (indicating a workspace dep)
-      dasel put object -f "$member/.cargo/config.toml" -p toml -t string -t string "patch.crates-io.$dependency" registry="local-registry" path="$PATH"
+    TYPE=$(< "$member/Cargo.toml" dasel -r toml -n ".dependencies.$dependency.[@]")
+    if [[ $TYPE == 'map' ]]; then
+          DEP_VERSION=$(dasel -p toml -f "$member/Cargo.toml" -n "dependencies.$dependency.version")
+          DEP_PATH=$(dasel -p toml -f "$member/Cargo.toml" -n "dependencies.$dependency.path")
+          if [[ $DEP_PATH != "null" && $DEP_VERSION != "null" ]]; then
+            DEP_PATH=$(basename "$DEP_PATH")
+            # add to patch config if dep is versioned and also has local path (indicating a workspace dep)
+            dasel put object -f "$member/.cargo/config.toml" -p toml -t string -t string "patch.crates-io.$dependency" registry="local-registry" path="$DEP_PATH"
+          fi
     fi
   done
-
-  # for any packages that are publishable
-  if ! grep -q ^"publish = false" "$member/Cargo.toml"; then
-    echo "$PACKAGE = { path = \"$member\", version = \"$VERSION\", registry = \"local-registry\" }";
-  fi
 done
