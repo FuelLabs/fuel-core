dbf8067e879e9a90cd0e311031ccb887966348ca
ControlCplusControlV <randomlikeabledude@gmail.com>
2022-07-06 11:59:25 -0600

    moved logging

--- fuel-core/src/args.rs
+++ fuel-core/src/args.rs
@@ -1,8 +1,9 @@
-use crate::args::run::RunCommand;
-use crate::args::snapshot::SnapshotCommand;
+use crate::args::{run::RunCommand, snapshot::SnapshotCommand};
 use anyhow::Result;
 use clap::Parser;
-use std::path::PathBuf;
+use std::{env, path::PathBuf, str::FromStr};
+use tracing_subscriber::filter::EnvFilter;
+
 lazy_static::lazy_static! {
     pub static ref DEFAULT_DB_PATH: PathBuf = dirs::home_dir().unwrap().join(".fuel").join("db");
 }
@@ -28,8 +29,45 @@ pub enum Core {
     Run(RunCommand),
 }
 
+pub const LOG_FILTER: &str = "RUST_LOG";
+pub const HUMAN_LOGGING: &str = "HUMAN_LOGGING";
+
 pub async fn run_cli() -> Result<()> {
     let opt = Opt::parse();
+
+    let filter = match env::var_os(LOG_FILTER) {
+        Some(_) => EnvFilter::try_from_default_env().expect("Invalid `RUST_LOG` provided"),
+        None => EnvFilter::new("info"),
+    };
+
+    let human_logging = env::var_os(HUMAN_LOGGING)
+        .map(|s| {
+            bool::from_str(s.to_str().unwrap())
+                .expect("Expected `true` or `false` to be provided for `HUMAN_LOGGING`")
+        })
+        .unwrap_or(true);
+
+    let sub = tracing_subscriber::fmt::Subscriber::builder()
+        .with_writer(std::io::stderr)
+        .with_env_filter(filter);
+
+    if human_logging {
+        // use pretty logs
+        sub.with_ansi(true)
+            .with_level(true)
+            .with_line_number(true)
+            .init();
+    } else {
+        // use machine parseable structured logs
+        sub
+            // disable terminal colors
+            .with_ansi(false)
+            .with_level(true)
+            .with_line_number(true)
+            // use json
+            .json()
+            .init();
+    }
     match opt.command {
         Core::Snapshot(command) => snapshot::exec(command).await,
         Core::Run(command) => run::exec(command).await,
--- fuel-core/src/args/run.rs
+++ fuel-core/src/args/run.rs
@@ -1,18 +1,10 @@
+use crate::args::DEFAULT_DB_PATH;
 use crate::FuelService;
 use clap::Parser;
 use fuel_core::config::{Config, DbType, VMConfig};
-use std::str::FromStr;
 use std::{env, io, net, path::PathBuf};
 use strum::VariantNames;
 use tracing::{info, trace};
-use tracing_subscriber::filter::EnvFilter;
-
-lazy_static::lazy_static! {
-    pub static ref DEFAULT_DB_PATH: PathBuf = dirs::home_dir().unwrap().join(".fuel").join("db");
-}
-
-pub const LOG_FILTER: &str = "RUST_LOG";
-pub const HUMAN_LOGGING: &str = "HUMAN_LOGGING";
 
 #[derive(Debug, Clone, Parser)]
 pub struct RunCommand {
@@ -62,40 +54,6 @@ pub struct RunCommand {
 
 impl RunCommand {
     pub fn get_config(self) -> io::Result<Config> {
-        let filter = match env::var_os(LOG_FILTER) {
-            Some(_) => EnvFilter::try_from_default_env().expect("Invalid `RUST_LOG` provided"),
-            None => EnvFilter::new("info"),
-        };
-
-        let human_logging = env::var_os(HUMAN_LOGGING)
-            .map(|s| {
-                bool::from_str(s.to_str().unwrap())
-                    .expect("Expected `true` or `false` to be provided for `HUMAN_LOGGING`")
-            })
-            .unwrap_or(true);
-
-        let sub = tracing_subscriber::fmt::Subscriber::builder()
-            .with_writer(std::io::stderr)
-            .with_env_filter(filter);
-
-        if human_logging {
-            // use pretty logs
-            sub.with_ansi(true)
-                .with_level(true)
-                .with_line_number(true)
-                .init();
-        } else {
-            // use machine parseable structured logs
-            sub
-                // disable terminal colors
-                .with_ansi(false)
-                .with_level(true)
-                .with_line_number(true)
-                // use json
-                .json()
-                .init();
-        }
-
         let RunCommand {
             ip,
             port,
