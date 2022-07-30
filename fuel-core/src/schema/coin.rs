fd08ddce8ec9fabe4f19018a46fa71a3d43440d8
Matt <54373384+matt-user@users.noreply.github.com>
2022-07-29 23:12:30 -0400

    feat: improve correctness of pagination (#508)
    Co-authored-by: ControlCplusControlV <44706811+ControlCplusControlV@users.noreply.github.com>

--- fuel-core/src/schema/coin.rs
+++ fuel-core/src/schema/coin.rs
@@ -5,6 +5,7 @@ use crate::{
     schema::scalars::{Address, AssetId, UtxoId, U64},
     state::IterDirection,
 };
+use anyhow::anyhow;
 use async_graphql::{
     connection::{query, Connection, Edge, EmptyFields},
     Context, Enum, InputObject, Object,
@@ -113,6 +114,13 @@ impl CoinQuery {
                     (0, IterDirection::Forward)
                 };
 
+                if (first.is_some() && before.is_some())
+                    || (after.is_some() && before.is_some())
+                    || (last.is_some() && after.is_some())
+                {
+                    return Err(anyhow!("Wrong argument combination"));
+                }
+
                 let after = after.map(fuel_tx::UtxoId::from);
                 let before = before.map(fuel_tx::UtxoId::from);
 
@@ -181,7 +189,7 @@ impl CoinQuery {
                         .map(|item| Edge::new(UtxoId::from(item.0), item)),
                 );
 
-                Ok::<Connection<UtxoId, Coin>, KvStoreError>(connection)
+                Ok::<Connection<UtxoId, Coin>, anyhow::Error>(connection)
             },
         )
         .await
