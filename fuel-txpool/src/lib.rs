aacc5fa6ec5197dba8c0acdd9784d5f662523076
Brandon Vrooman <brandon.vrooman@gmail.com>
2022-08-23 10:10:06 -0400

    chore: Refactor txpool tests to remove usages of pre-seeded DummyDB  (#562)
    * Refactor tests WIP
    
    * More tests refactored WIP
    
    * Additional tests refactored
    
    * Use default instead of hardcoded amount
    
    * try_to_insert_tx2_missing_utxo
    
    * not_inserted_known_tx
    
    * faulty_t2_collided_on_contract_id_from_tx1
    
    * Remaining tests
    
    * Clippy
    
    * Replace Config::default with Default::default
    
    * Minor refactor
    
    * Extract mock_db
    
    * Yse mock_db in txpool service tests
    
    * Simplify tests with helper functions
    
    * Fix test inputs/outputs
    
    * Refactor expects
    
    * Remove unneeded tx clones
    
    * Minor refactor
    
    * whitespace

--- fuel-txpool/src/lib.rs
+++ fuel-txpool/src/lib.rs
@@ -4,6 +4,11 @@ pub mod service;
 pub mod txpool;
 pub mod types;
 
+#[cfg(test)]
+mod mock_db;
+#[cfg(test)]
+pub(crate) use mock_db::MockDb;
+
 pub use config::Config;
 pub use fuel_core_interfaces::txpool::Error;
 pub use service::{Service, ServiceBuilder};
