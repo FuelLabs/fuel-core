6e4a523b85e345bdcf2fb744b04fd193d20590e8
Hannes Karppila <hannes.karppila@gmail.com>
2022-08-30 22:16:01 +0300

    Transaction selection logic for the block producer (#581)

--- fuel-block-producer/src/config.rs
+++ fuel-block-producer/src/config.rs
@@ -3,10 +3,20 @@ use fuel_core_interfaces::common::{
     fuel_types::{Address, Word},
 };
 
-#[derive(Clone, Debug, Default)]
+#[derive(Clone, Debug)]
 pub struct Config {
     /// The primary identity associated with the validator (i.e. staking key)
     pub validator_id: Address,
     pub max_gas_per_block: Word,
     pub consensus_params: ConsensusParameters,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            max_gas_per_block: 1_000_000, // TODO: pick a reasonable default value
+            validator_id: Default::default(),
+            consensus_params: Default::default(),
+        }
+    }
 }
