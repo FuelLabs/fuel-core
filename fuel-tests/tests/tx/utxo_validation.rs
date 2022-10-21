083192400cb2300f13131c76640ef05d6edc114d
green <xgreenx9999@gmail.com>
2022-10-21 02:16:29 +0100

    Fix test in CI

--- fuel-tests/tests/tx/utxo_validation.rs
+++ fuel-tests/tests/tx/utxo_validation.rs
@@ -261,7 +261,10 @@ async fn concurrent_tx_submission_produces_expected_blocks() {
         .into_iter()
         .map(|tx| {
             let client = client.clone();
-            async move { client.submit(&tx).await }
+            async move {
+                let tx_id = client.submit(&tx).await.expect("Should submit transaction");
+                client.await_transaction_commit(&tx_id.to_string()).await
+            }
         })
         .collect_vec();
 
