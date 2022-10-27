bafb2c092bffe6ca0afa533e1fcece59eca3109b
Brandon Kite <brandonkite92@gmail.com>
2022-10-27 16:23:28 -0700

    disable musl binary builds  (#733)
    * disable musl binary builds as it is incompatible with libclang
    
    * remove musl entirely

--- ci/build-images.sh
+++ ci/build-images.sh
@@ -2,8 +2,4 @@
 
 docker build -t x86_64-linux-gnu -f Dockerfile.x86_64-unknown-linux-gnu-clang .
 
-docker build -t x86_64-linux-musl -f Dockerfile.x86_64-unknown-linux-musl-clang .
-
 docker build -t aarch64-linux-gnu -f Dockerfile.aarch64-unknown-linux-gnu-clang .
-
-docker build -t aarch64-linux-musl -f Dockerfile.aarch64-unknown-linux-musl-clang .
