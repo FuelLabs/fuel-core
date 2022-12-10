13ddf761b17c69d76a229b2c0092187a5649be9a
leviathanbeak88 <elvisdedic@outlook.com>
2022-12-10 10:23:41 +0100

    move to GenesisCommitment gensis file and implement for Genesis

--- fuel-chain-config/src/lib.rs
+++ fuel-chain-config/src/lib.rs
@@ -1,10 +1,6 @@
 pub mod config;
+mod genesis;
 mod serialization;
 
 pub use config::*;
-use fuel_core_interfaces::common::prelude::MerkleRoot;
-
-pub trait GenesisCommitment {
-    /// Calculates the merkle root of the state of the entity.
-    fn root(&mut self) -> anyhow::Result<MerkleRoot>;
-}
+pub use genesis::GenesisCommitment;
