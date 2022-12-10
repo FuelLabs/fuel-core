4ff16a6acc8efb33af59212322f5ec3b86259a61
leviathanbeak88 <elvisdedic@outlook.com>
2022-12-10 10:12:55 +0100

    move genesis root to chain-config

--- fuel-chain-config/src/lib.rs
+++ fuel-chain-config/src/lib.rs
@@ -2,3 +2,9 @@ pub mod config;
 mod serialization;
 
 pub use config::*;
+use fuel_core_interfaces::common::prelude::MerkleRoot;
+
+pub trait GenesisCommitment {
+    /// Calculates the merkle root of the state of the entity.
+    fn root(&mut self) -> anyhow::Result<MerkleRoot>;
+}
