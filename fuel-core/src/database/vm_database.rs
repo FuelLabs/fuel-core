45568df6f2286416b2aa1bdd463d7edb1b83b23f
Mitch Martin <111294749+mitch-fuel@users.noreply.github.com>
2022-12-14 02:23:50 -0600

    State Read/Write/Clear range implementation (#818)
    * vm_database modifications for batch read-write-clear of vm state
    
    Signed-off-by: Mitch Martin <mitch.martin@fuel.sh>
    
    * range inserts & deletes implemented, read in progress
    
    Signed-off-by: Mitch Martin <mitch.martin@fuel.sh>
    
    * draft of state range read
    
    Signed-off-by: Mitch Martin <mitch.martin@fuel.sh>
    
    * update cargo lock
    
    Signed-off-by: Mitch Martin <mitch.martin@fuel.sh>
    
    * update to match merged from main
    
    Signed-off-by: Mitch Martin <mitch.martin@fuel.sh>
    
    * add test stubs and slight modifications to read
    
    Signed-off-by: Mitch Martin <mitch.martin@fuel.sh>
    
    * added clear tests
    
    Signed-off-by: Mitch Martin <mitch.martin@fuel.sh>
    
    * fix compile errors, fix bug in remove_range, fix remove tests
    
    Signed-off-by: Mitch Martin <mitch.martin@fuel.sh>
    
    * fix identical bug from remove in insert
    
    Signed-off-by: Mitch Martin <mitch.martin@fuel.sh>
    
    * updated tests and debugging
    
    Signed-off-by: Mitch Martin <mitch.martin@fuel.sh>
    
    * first 2 insert tests functional
    
    Signed-off-by: Mitch Martin <mitch.martin@fuel.sh>
    
    * wip
    
    * first read sequential read test working, fixed some bugs with current_key
    
    * finish impl of remaining read tests
    
    * insert tests complete
    
    Signed-off-by: Mitch Martin <mitch.martin@fuel.sh>
    
    * fix clippy issues
    
    Signed-off-by: Mitch Martin <mitch.martin@fuel.sh>
    
    * address clippy issues
    
    Signed-off-by: Mitch Martin <mitch.martin@fuel.sh>
    
    * fix assert_eq to assert
    
    Signed-off-by: Mitch Martin <mitch.martin@fuel.sh>
    
    * resolve clippy issues, mostly !is_none -> is_some
    
    Signed-off-by: Mitch Martin <mitch.martin@fuel.sh>
    
    * fix clippy issues
    
    Signed-off-by: Mitch Martin <mitch.martin@fuel.sh>
    
    * fix errant variable initialization
    
    Signed-off-by: Mitch Martin <mitch.martin@fuel.sh>
    
    * fix clippy errors
    
    * Simplified the code of range operations (#830)
    
    * Simplified the code of range operations
    
    * Simplified tests
    
    * simplify key generation for tests
    
    Signed-off-by: Mitch Martin <mitch.martin@fuel.sh>
    
    * pull in latest fuel-vm
    
    * start using test_case for insertion tests
    
    * refactor remaining insertion tests into test_case
    
    * over -> with
    
    * re-arrange
    
    * simplify insert test return type by automatically verifying the results.
    
    * add test for insertion keyspace overrun
    
    * use test_case for range removes
    
    * convert read tests to test_case, remove tests involving multiple contract ids
    
    * cleanup
    
    * integration test
    
    Signed-off-by: Mitch Martin <mitch.martin@fuel.sh>
    Co-authored-by: Voxelot <brandonkite92@gmail.com>
    Co-authored-by: Green Baneling <XgreenX9999@gmail.com>
    Co-authored-by: Tom <tomrgowan@gmail.com>

--- fuel-core/src/database/vm_database.rs
+++ fuel-core/src/database/vm_database.rs
@@ -1,4 +1,13 @@
-use crate::database::Database;
+use crate::{
+    database::{
+        Column,
+        Database,
+    },
+    state::{
+        IterDirection,
+        MultiKey,
+    },
+};
 use anyhow::anyhow;
 use fuel_core_interfaces::{
     common::{
@@ -22,6 +31,7 @@ use fuel_core_interfaces::{
     model::FuelConsensusHeader,
     not_found,
 };
+use primitive_types::U256;
 use std::borrow::Cow;
 
 /// Used to store metadata relevant during the execution of a transaction
@@ -31,6 +41,19 @@ pub struct VmDatabase {
     current_timestamp: Tai64,
     coinbase: Address,
     database: Database,
+}
+
+trait IncreaseStorageKey {
+    fn increase(&mut self) -> anyhow::Result<()>;
+}
+
+impl IncreaseStorageKey for U256 {
+    fn increase(&mut self) -> anyhow::Result<()> {
+        *self = self.checked_add(1.into()).ok_or_else(|| {
+            Error::Other(anyhow!("range op exceeded available keyspace"))
+        })?;
+        Ok(())
+    }
 }
 
 impl Default for VmDatabase {
@@ -142,28 +165,412 @@ impl InterpreterStorage for VmDatabase {
 
     fn merkle_contract_state_range(
         &self,
-        _id: &ContractId,
-        _start_key: &Bytes32,
-        _range: Word,
+        contract_id: &ContractId,
+        start_key: &Bytes32,
+        range: Word,
     ) -> Result<Vec<Option<Cow<Bytes32>>>, Self::DataError> {
-        unimplemented!()
+        // TODO: Optimization: Iterate only over `range` elements.
+        let mut iterator = self.database.iter_all::<Vec<u8>, Bytes32>(
+            Column::ContractsState,
+            Some(contract_id.as_ref().to_vec()),
+            Some(MultiKey::new(&(contract_id, start_key)).into()),
+            Some(IterDirection::Forward),
+        );
+        let range = range as usize;
+
+        let mut expected_key = U256::from_big_endian(start_key.as_ref());
+        let mut results = vec![];
+
+        while results.len() < range {
+            let entry = iterator.next().transpose()?;
+
+            if entry.is_none() {
+                // We out of `contract_id` prefix
+                break
+            }
+
+            let (multikey, value) =
+                entry.expect("We did a check before, so the entry should be `Some`");
+            let actual_key = U256::from_big_endian(&multikey[32..]);
+
+            while (expected_key <= actual_key) && results.len() < range {
+                if expected_key == actual_key {
+                    // We found expected key, put value into results
+                    results.push(Some(Cow::Owned(value)));
+                } else {
+                    // Iterator moved beyond next expected key, push none until we find the key
+                    results.push(None);
+                }
+                expected_key.increase()?;
+            }
+        }
+
+        // Fill not initialized slots with `None`.
+        while results.len() < range {
+            results.push(None);
+            expected_key.increase()?;
+        }
+
+        Ok(results)
     }
 
     fn merkle_contract_state_insert_range(
         &mut self,
-        _contract: &ContractId,
-        _start_key: &Bytes32,
-        _values: &[Bytes32],
+        contract_id: &ContractId,
+        start_key: &Bytes32,
+        values: &[Bytes32],
     ) -> Result<Option<()>, Self::DataError> {
-        unimplemented!()
+        let mut current_key = U256::from_big_endian(start_key.as_ref());
+        // verify key is in range
+        current_key
+            .checked_add(U256::from(values.len()))
+            .ok_or_else(|| {
+                Error::Other(anyhow!("range op exceeded available keyspace"))
+            })?;
+
+        let mut key_bytes = [0u8; 32];
+        let mut found_unset = false;
+        for value in values {
+            current_key.to_big_endian(&mut key_bytes);
+
+            let option = self.database.insert::<_, _, Bytes32>(
+                MultiKey::new(&(contract_id, key_bytes)).as_ref(),
+                Column::ContractsState,
+                value,
+            )?;
+
+            found_unset |= option.is_none();
+
+            current_key.increase()?;
+        }
+
+        if found_unset {
+            Ok(None)
+        } else {
+            Ok(Some(()))
+        }
     }
 
     fn merkle_contract_state_remove_range(
         &mut self,
-        _contract: &ContractId,
-        _start_key: &Bytes32,
-        _range: Word,
+        contract_id: &ContractId,
+        start_key: &Bytes32,
+        range: Word,
     ) -> Result<Option<()>, Self::DataError> {
-        unimplemented!()
+        let mut found_unset = false;
+
+        let mut current_key = U256::from_big_endian(start_key.as_ref());
+
+        for _ in 0..range {
+            let mut key_bytes = [0u8; 32];
+            current_key.to_big_endian(&mut key_bytes);
+
+            let option = self.database.remove::<Bytes32>(
+                MultiKey::new(&(contract_id, key_bytes)).as_ref(),
+                Column::ContractsState,
+            )?;
+
+            found_unset |= option.is_none();
+
+            current_key.increase()?;
+        }
+
+        if found_unset {
+            Ok(None)
+        } else {
+            Ok(Some(()))
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use test_case::test_case;
+
+    fn u256_to_bytes32(u: U256) -> Bytes32 {
+        let mut bytes = [0u8; 32];
+        u.to_big_endian(&mut bytes);
+        Bytes32::from(bytes)
+    }
+
+    const fn key(k: u8) -> [u8; 32] {
+        [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, k,
+        ]
+    }
+
+    #[test_case(
+        &[], key(0), 1
+        => Ok(vec![None])
+        ; "read single uninitialized value"
+    )]
+    #[test_case(
+        &[(key(0), [0; 32])], key(0), 1
+        => Ok(vec![Some([0; 32])])
+        ; "read single initialized value"
+    )]
+    #[test_case(
+        &[], key(0), 3
+        => Ok(vec![None, None, None])
+        ; "read uninitialized range"
+    )]
+    #[test_case(
+        &[(key(1), [1; 32]), (key(2), [2; 32])], key(0), 3
+        => Ok(vec![None, Some([1; 32]), Some([2; 32])])
+        ; "read uninitialized start range"
+    )]
+    #[test_case(
+        &[(key(0), [0; 32]), (key(2), [2; 32])], key(0), 3
+        => Ok(vec![Some([0; 32]), None, Some([2; 32])])
+        ; "read uninitialized middle range"
+    )]
+    #[test_case(
+        &[(key(0), [0; 32]), (key(1), [1; 32])], key(0), 3
+        => Ok(vec![Some([0; 32]), Some([1; 32]), None])
+        ; "read uninitialized end range"
+    )]
+    #[test_case(
+        &[(key(0), [0; 32]), (key(1), [1; 32]), (key(2), [2; 32])], key(0), 3
+        => Ok(vec![Some([0; 32]), Some([1; 32]), Some([2; 32])])
+        ; "read fully initialized range"
+    )]
+    #[test_case(
+        &[(key(0), [0; 32]), (key(1), [1; 32]), (key(2), [2; 32])], key(0), 2
+        => Ok(vec![Some([0; 32]), Some([1; 32])])
+        ; "read subset of initialized range"
+    )]
+    #[test_case(
+        &[(key(0), [0; 32]), (key(2), [2; 32])], key(0), 2
+        => Ok(vec![Some([0; 32]), None])
+        ; "read subset of partially set range without running too far"
+    )]
+    #[test_case(
+        &[], *u256_to_bytes32(U256::MAX), 2
+        => Err(())
+        ; "read fails on uninitialized range if keyspace exceeded"
+    )]
+    #[test_case(
+        &[(*u256_to_bytes32(U256::MAX), [0; 32])], *u256_to_bytes32(U256::MAX), 2
+        => Err(())
+        ; "read fails on partially initialized range if keyspace exceeded"
+    )]
+    fn read_sequential_range(
+        prefilled_slots: &[([u8; 32], [u8; 32])],
+        start_key: [u8; 32],
+        range: u64,
+    ) -> Result<Vec<Option<[u8; 32]>>, ()> {
+        let db = VmDatabase::default();
+
+        let contract_id = ContractId::new([0u8; 32]);
+
+        // prefill db
+        for (key, value) in prefilled_slots {
+            let multi_key = MultiKey::new(&(contract_id.as_ref(), key));
+            db.database
+                .insert::<_, _, Bytes32>(
+                    &multi_key,
+                    Column::ContractsState,
+                    Bytes32::new(*value),
+                )
+                .unwrap();
+        }
+
+        // perform sequential read
+        Ok(db
+            .merkle_contract_state_range(&contract_id, &Bytes32::new(start_key), range)
+            .map_err(|_| ())?
+            .into_iter()
+            .map(|v| v.map(Cow::into_owned).map(|v| *v))
+            .collect())
+    }
+
+    #[test_case(
+        &[], key(0), &[[1; 32]]
+        => Ok(false)
+        ; "insert single value over uninitialized range"
+    )]
+    #[test_case(
+        &[(key(0), [0; 32])], key(0), &[[1; 32]]
+        => Ok(true)
+        ; "insert single value over initialized range"
+    )]
+    #[test_case(
+        &[], key(0), &[[1; 32], [2; 32]]
+        => Ok(false)
+        ; "insert multiple slots over uninitialized range"
+    )]
+    #[test_case(
+        &[(key(1), [0; 32]), (key(2), [0; 32])], key(0), &[[1; 32], [2; 32], [3; 32]]
+        => Ok(false)
+        ; "insert multiple slots with uninitialized start of the range"
+    )]
+    #[test_case(
+        &[(key(0), [0; 32]), (key(2), [0; 32])], key(0), &[[1; 32], [2; 32], [3; 32]]
+        => Ok(false)
+        ; "insert multiple slots with uninitialized middle of the range"
+    )]
+    #[test_case(
+        &[(key(0), [0; 32]), (key(1), [0; 32])], key(0), &[[1; 32], [2; 32], [3; 32]]
+        => Ok(false)
+        ; "insert multiple slots with uninitialized end of the range"
+    )]
+    #[test_case(
+        &[(key(0), [0; 32]), (key(1), [0; 32]), (key(2), [0; 32])], key(0), &[[1; 32], [2; 32], [3; 32]]
+        => Ok(true)
+        ; "insert multiple slots over initialized range"
+    )]
+    #[test_case(
+        &[(key(0), [0; 32]), (key(1), [0; 32]), (key(2), [0; 32]), (key(3), [0; 32])], key(1), &[[1; 32], [2; 32]]
+        => Ok(true)
+        ; "insert multiple slots over sub-range of prefilled data"
+    )]
+    #[test_case(
+        &[], *u256_to_bytes32(U256::MAX), &[[1; 32], [2; 32]]
+        => Err(())
+        ; "insert fails if start_key + range > u256::MAX"
+    )]
+    fn insert_range(
+        prefilled_slots: &[([u8; 32], [u8; 32])],
+        start_key: [u8; 32],
+        insertion_range: &[[u8; 32]],
+    ) -> Result<bool, ()> {
+        let mut db = VmDatabase::default();
+
+        let contract_id = ContractId::new([0u8; 32]);
+
+        // prefill db
+        for (key, value) in prefilled_slots {
+            let multi_key = MultiKey::new(&(contract_id.as_ref(), key));
+            db.database
+                .insert::<_, _, Bytes32>(
+                    &multi_key,
+                    Column::ContractsState,
+                    Bytes32::new(*value),
+                )
+                .unwrap();
+        }
+
+        // test insert range
+        let insert_status = db
+            .merkle_contract_state_insert_range(
+                &contract_id,
+                &Bytes32::new(start_key),
+                &insertion_range
+                    .iter()
+                    .map(|v| Bytes32::new(*v))
+                    .collect::<Vec<_>>(),
+            )
+            .map_err(|_| ())
+            .map(|v| v.is_some());
+
+        // check stored data
+        let results: Vec<_> = (0..insertion_range.len())
+            .filter_map(|i| {
+                let current_key =
+                    U256::from_big_endian(&start_key).checked_add(i.into())?;
+                let current_key = u256_to_bytes32(current_key);
+                let result = db
+                    .merkle_contract_state(&contract_id, &current_key)
+                    .unwrap()
+                    .map(Cow::into_owned)
+                    .map(|b| *b);
+                result
+            })
+            .collect();
+
+        // verify all data from insertion request is actually inserted if successful
+        // or not inserted at all if unsuccessful
+        if insert_status.is_ok() {
+            assert_eq!(insertion_range, results);
+        } else {
+            assert_eq!(results.len(), 0);
+        }
+
+        insert_status
+    }
+
+    #[test_case(
+        &[], [0; 32], 1
+        => (vec![], false)
+        ; "remove single value over uninitialized range"
+    )]
+    #[test_case(
+        &[([0; 32], [0; 32])], [0; 32], 1
+        => (vec![], true)
+        ; "remove single value over initialized range"
+    )]
+    #[test_case(
+        &[], [0; 32], 2
+        => (vec![], false)
+        ; "remove multiple slots over uninitialized range"
+    )]
+    #[test_case(
+        &[([0; 32], [0; 32]), (key(1), [0; 32])], [0; 32], 2
+        => (vec![], true)
+        ; "remove multiple slots over initialized range"
+    )]
+    #[test_case(
+        &[(key(1), [0; 32]), (key(2), [0; 32])], [0; 32], 3
+        => (vec![], false)
+        ; "remove multiple slots over partially uninitialized start range"
+    )]
+    #[test_case(
+        &[([0; 32], [0; 32]), (key(1), [0; 32])], [0; 32], 3
+        => (vec![], false)
+        ; "remove multiple slots over partially uninitialized end range"
+    )]
+    #[test_case(
+        &[([0; 32], [0; 32]), (key(2), [0; 32])], [0; 32], 3
+        => (vec![], false)
+        ; "remove multiple slots over partially uninitialized middle range"
+    )]
+    fn remove_range(
+        prefilled_slots: &[([u8; 32], [u8; 32])],
+        start_key: [u8; 32],
+        remove_count: Word,
+    ) -> (Vec<[u8; 32]>, bool) {
+        let mut db = VmDatabase::default();
+
+        let contract_id = ContractId::new([0u8; 32]);
+
+        // prefill db
+        for (key, value) in prefilled_slots {
+            let multi_key = MultiKey::new(&(contract_id.as_ref(), key));
+            db.database
+                .insert::<_, _, Bytes32>(
+                    &multi_key,
+                    Column::ContractsState,
+                    Bytes32::new(*value),
+                )
+                .unwrap();
+        }
+
+        // test remove range
+        let remove_status = db
+            .merkle_contract_state_remove_range(
+                &contract_id,
+                &Bytes32::new(start_key),
+                remove_count,
+            )
+            .unwrap()
+            .is_some();
+
+        // check stored data
+        let results: Vec<_> = (0..(remove_count as usize))
+            .filter_map(|i| {
+                let current_key = U256::from_big_endian(&start_key) + i;
+                let current_key = u256_to_bytes32(current_key);
+                let result = db
+                    .merkle_contract_state(&contract_id, &current_key)
+                    .unwrap()
+                    .map(Cow::into_owned)
+                    .map(|b| *b);
+                result
+            })
+            .collect();
+
+        (results, remove_status)
     }
 }
