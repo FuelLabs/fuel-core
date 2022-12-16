95ce93590b8a862357fadf0536810d6030ab6212
green <xgreenx9999@gmail.com>
2022-12-16 20:03:24 +0000

    Fixed deps for `fuel-core-relayer`

--- ci_checks.sh
+++ ci_checks.sh
@@ -2,8 +2,8 @@ cargo +nightly fmt --all -- --check &&
 cargo sort -w --check &&
 source .github/workflows/scripts/verify_openssl.sh &&
 cargo clippy --all-targets --all-features &&
-cargo check --locked &&
-cargo check --all-features --locked &&
+cargo make check --locked &&
+cargo make check --all-features --locked &&
 cargo test --all-features --workspace &&
 cargo test --no-default-features --workspace &&
 cargo test --manifest-path version-compatibility/Cargo.toml --workspace
