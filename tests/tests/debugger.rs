e93bdf118656449ed664bae7a6f2def6123c4810
Hannes Karppila <hannes.karppila@gmail.com>
2022-12-16 18:49:18 +0200

    fmt

--- tests/tests/debugger.rs
+++ tests/tests/debugger.rs
@@ -5,7 +5,10 @@ use fuel_core::service::{
     FuelService,
 };
 use fuel_core_client::client::FuelClient;
-use fuel_core_types::{fuel_types::ContractId, fuel_tx::Transaction};
+use fuel_core_types::{
+    fuel_tx::Transaction,
+    fuel_types::ContractId,
+};
 
 /// Tests that debugger doesn't produce any errors with a running local node,
 /// and also verifies that breakpoints are working as they should
