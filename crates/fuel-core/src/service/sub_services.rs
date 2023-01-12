81f686f68f5224839d0036a006e9dec31b8cf3b8
green <xgreenx9999@gmail.com>
2023-01-12 01:54:30 +0000

    Missed `Database` for `dap.rs`.

--- crates/fuel-core/src/service/sub_services.rs
+++ crates/fuel-core/src/service/sub_services.rs
@@ -122,7 +122,8 @@ pub fn init_sub_services(
     };
 
     // TODO: Figure out on how to move it into `fuel-core-graphql-api`.
-    let schema = dap::init(build_schema(), config.chain_conf.transaction_parameters);
+    let schema = dap::init(build_schema(), config.chain_conf.transaction_parameters)
+        .data(database.clone());
     let graph_ql = crate::fuel_core_graphql_api::service::new_service(
         GraphQLConfig {
             addr: config.addr,
