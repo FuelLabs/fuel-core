4f1e02dad0cb6c38b3389eec8008caa30be7520d
Tom <tomrgowan@gmail.com>
2023-01-20 14:53:40 +1100

    had flag around the wrong way

--- crates/fuel-core/src/service/config.rs
+++ crates/fuel-core/src/service/config.rs
@@ -115,7 +115,7 @@ impl Config {
         // If manual block production then require trigger never or instant.
         anyhow::ensure!(
             !self.manual_blocks_enabled
-                || !matches!(trigger, Trigger::Never | Trigger::Instant),
+                || matches!(trigger, Trigger::Never | Trigger::Instant),
             "Cannot use manual block production unless trigger mode is never or instant."
         );
 
