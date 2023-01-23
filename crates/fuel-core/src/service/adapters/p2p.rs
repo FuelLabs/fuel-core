9501e79d22362de808a7ec714889b75431079470
leviathanbeak88 <elvisdedic@outlook.com>
2023-01-23 18:50:36 +0100

    implement p2p adapter

--- crates/fuel-core/src/service/adapters/p2p.rs
+++ crates/fuel-core/src/service/adapters/p2p.rs
@@ -6,9 +6,16 @@ use fuel_core_p2p::ports::{
 };
 use fuel_core_services::stream::BoxStream;
 use fuel_core_storage::Result as StorageResult;
-use fuel_core_types::blockchain::{
-    primitives::BlockHeight,
-    SealedBlock,
+use fuel_core_types::{
+    blockchain::{
+        primitives::{
+            BlockHeight,
+            BlockId,
+        },
+        SealedBlock,
+        SealedBlockHeader,
+    },
+    fuel_tx::Transaction,
 };
 
 #[async_trait::async_trait]
@@ -18,6 +25,20 @@ impl P2pDb for Database {
         height: &BlockHeight,
     ) -> StorageResult<Option<SealedBlock>> {
         self.get_sealed_block_by_height(height)
+    }
+
+    async fn get_sealed_header(
+        &self,
+        height: BlockHeight,
+    ) -> StorageResult<Option<SealedBlockHeader>> {
+        self.get_sealed_header(height).await
+    }
+
+    async fn get_transactions(
+        &self,
+        block_id: BlockId,
+    ) -> StorageResult<Option<Vec<Transaction>>> {
+        self.get_transactions(block_id).await
     }
 }
 
