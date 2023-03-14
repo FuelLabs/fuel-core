528da73b13ea485e56f8ff52579a901e42e1bd02
Voxelot <brandonkite92@gmail.com>
2023-03-14 16:25:33 -0700

    repro of issue with deploying large contract

---
+++ bin/e2e-test-client/src/tests/contracts.rs
@@ -0,0 +1,16 @@
+use fuel_core_e2e_client::test_context::TestContext;
+use libtest_mimic::Failed;
+use tokio::time::timeout;
+
+// deploy a large contract (7mb)
+pub async fn deploy_large_contract(ctx: &TestContext) -> Result<(), Failed> {
+    // generate large bytecode
+    let bytecode = vec![0u8; 7_000_000];
+
+    let deployment_request = ctx.alice.deploy_contract(bytecode);
+
+    // wait for contract to deploy
+    timeout(ctx.config.sync_timeout(), deployment_request).await??;
+
+    Ok(())
+}
