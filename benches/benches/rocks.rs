9ee77f51a82e6643ebece7a1cc7ca859637f1254
Tom <tomrgowan@gmail.com>
2023-03-22 09:43:22 +1100

    WIP

---
+++ benches/benches/rocks.rs
@@ -0,0 +1,14 @@
+
+use criterion::{
+    black_box,
+    criterion_group,
+    criterion_main,
+    BenchmarkId,
+    Criterion,
+};
+
+fn rocks(c: &mut Criterion) {
+}
+
+criterion_group!(benches, rocks);
+criterion_main!(benches);
