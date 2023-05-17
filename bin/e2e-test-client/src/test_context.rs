9ce8dd348abc4cfb6302873ef756ea1d14244ed9
Brandon Vrooman <brandon.vrooman@gmail.com>
2023-05-17 17:13:36 -0400

    Update test_context.rs

--- bin/e2e-test-client/src/test_context.rs
+++ bin/e2e-test-client/src/test_context.rs
@@ -205,7 +205,7 @@ impl Wallet {
         let tx = self
             .transfer_tx(destination, transfer_amount, asset_id)
             .await?;
-        let tx_id = tx.id(&self.consensus_params.clone().into());
+        let tx_id = tx.id(&self.consensus_params);
         let status = self.client.submit_and_await_commit(&tx).await?;
 
         // we know the transferred coin should be output 0 from above
