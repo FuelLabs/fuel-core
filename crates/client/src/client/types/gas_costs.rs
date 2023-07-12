6836dfa832ffcce2cdd6d923aed51d09f985a321
Mitchell Turner <james.mitchell.turner@gmail.com>
2023-07-11 22:46:05 -0700

    Expose gas cost in chain info (#1244)
    https://github.com/FuelLabs/fuel-core/issues/1221
    
    This PR adds:
    - new GraphQL schema for `GasCosts` inside the `ChainInfo` schema
    - public `GasCosts` types for client
    - appropriate conversions
    - check in tests to confirm gas costs are included in chain info

---
+++ crates/client/src/client/types/gas_costs.rs
@@ -0,0 +1,160 @@
+use crate::client::schema;
+
+macro_rules! include_from_impls {
+    ($(#[$meta:meta])* $vis:vis struct $name:ident {
+        $($field_vis:vis $field_name:ident: $field_type:ty,)*
+    }) => {
+        $vis struct $name {
+            $($field_vis $field_name: $field_type,)*
+        }
+
+        impl From<$name> for fuel_core_types::fuel_vm::GasCosts {
+           fn from(value: $name) -> Self {
+               let values = fuel_core_types::fuel_vm::GasCostsValues {
+                   $($field_name: value.$field_name.into(),)*
+               };
+               Self::new(values)
+           }
+        }
+
+        impl From<schema::chain::GasCosts> for $name {
+            fn from(value: schema::chain::GasCosts) -> Self {
+                Self {
+                    $($field_name: value.$field_name.into(),)*
+                }
+            }
+
+        }
+    }
+}
+
+include_from_impls! {
+    #[derive(Clone, Debug)]
+    pub struct GasCosts {
+        pub add: u64,
+        pub addi: u64,
+        pub aloc: u64,
+        pub and: u64,
+        pub andi: u64,
+        pub bal: u64,
+        pub bhei: u64,
+        pub bhsh: u64,
+        pub burn: u64,
+        pub cb: u64,
+        pub cfei: u64,
+        pub cfsi: u64,
+        pub croo: u64,
+        pub div: u64,
+        pub divi: u64,
+        pub ecr: u64,
+        pub eq: u64,
+        pub exp: u64,
+        pub expi: u64,
+        pub flag: u64,
+        pub gm: u64,
+        pub gt: u64,
+        pub gtf: u64,
+        pub ji: u64,
+        pub jmp: u64,
+        pub jne: u64,
+        pub jnei: u64,
+        pub jnzi: u64,
+        pub jmpf: u64,
+        pub jmpb: u64,
+        pub jnzf: u64,
+        pub jnzb: u64,
+        pub jnef: u64,
+        pub jneb: u64,
+        pub k256: u64,
+        pub lb: u64,
+        pub log: u64,
+        pub lt: u64,
+        pub lw: u64,
+        pub mcpi: u64,
+        pub mint: u64,
+        pub mlog: u64,
+        pub mod_op: u64,
+        pub modi: u64,
+        pub move_op: u64,
+        pub movi: u64,
+        pub mroo: u64,
+        pub mul: u64,
+        pub muli: u64,
+        pub mldv: u64,
+        pub noop: u64,
+        pub not: u64,
+        pub or: u64,
+        pub ori: u64,
+        pub ret: u64,
+        pub rvrt: u64,
+        pub s256: u64,
+        pub sb: u64,
+        pub scwq: u64,
+        pub sll: u64,
+        pub slli: u64,
+        pub srl: u64,
+        pub srli: u64,
+        pub srw: u64,
+        pub sub: u64,
+        pub subi: u64,
+        pub sw: u64,
+        pub sww: u64,
+        pub swwq: u64,
+        pub time: u64,
+        pub tr: u64,
+        pub tro: u64,
+        pub wdcm: u64,
+        pub wqcm: u64,
+        pub wdop: u64,
+        pub wqop: u64,
+        pub wdml: u64,
+        pub wqml: u64,
+        pub wddv: u64,
+        pub wqdv: u64,
+        pub wdmd: u64,
+        pub wqmd: u64,
+        pub wdam: u64,
+        pub wqam: u64,
+        pub wdmm: u64,
+        pub wqmm: u64,
+        pub xor: u64,
+        pub xori: u64,
+
+        pub call: DependentCost,
+        pub ccp: DependentCost,
+        pub csiz: DependentCost,
+        pub ldc: DependentCost,
+        pub logd: DependentCost,
+        pub mcl: DependentCost,
+        pub mcli: DependentCost,
+        pub mcp: DependentCost,
+        pub meq: DependentCost,
+        pub retd: DependentCost,
+        pub smo: DependentCost,
+        pub srwq: DependentCost,
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct DependentCost {
+    pub base: u64,
+    pub dep_per_unit: u64,
+}
+
+impl From<DependentCost> for fuel_core_types::fuel_vm::DependentCost {
+    fn from(value: DependentCost) -> Self {
+        Self {
+            base: value.base,
+            dep_per_unit: value.dep_per_unit,
+        }
+    }
+}
+
+impl From<schema::chain::DependentCost> for DependentCost {
+    fn from(value: schema::chain::DependentCost) -> Self {
+        Self {
+            base: value.base.into(),
+            dep_per_unit: value.dep_per_unit.into(),
+        }
+    }
+}
--- crates/fuel-core/src/schema/chain.rs
+++ crates/fuel-core/src/schema/chain.rs
@@ -19,11 +19,24 @@ use async_graphql::{
     Context,
     Object,
 };
-use fuel_core_types::fuel_tx;
+use fuel_core_types::{
+    fuel_tx,
+    fuel_vm,
+};
 
 pub struct ChainInfo;
 
 pub struct ConsensusParameters(fuel_tx::ConsensusParameters);
+
+pub struct GasCosts(fuel_vm::GasCosts);
+
+pub struct DependentCost(fuel_vm::DependentCost);
+
+impl From<fuel_vm::DependentCost> for DependentCost {
+    fn from(value: fuel_vm::DependentCost) -> Self {
+        Self(value)
+    }
+}
 
 #[Object]
 impl ConsensusParameters {
@@ -89,6 +102,420 @@ impl ConsensusParameters {
 }
 
 #[Object]
+impl GasCosts {
+    async fn add(&self) -> U64 {
+        self.0.add.into()
+    }
+
+    async fn addi(&self) -> U64 {
+        self.0.addi.into()
+    }
+
+    async fn aloc(&self) -> U64 {
+        self.0.aloc.into()
+    }
+
+    async fn and(&self) -> U64 {
+        self.0.and.into()
+    }
+
+    async fn andi(&self) -> U64 {
+        self.0.andi.into()
+    }
+
+    async fn bal(&self) -> U64 {
+        self.0.bal.into()
+    }
+
+    async fn bhei(&self) -> U64 {
+        self.0.bhei.into()
+    }
+
+    async fn bhsh(&self) -> U64 {
+        self.0.bhsh.into()
+    }
+
+    async fn burn(&self) -> U64 {
+        self.0.burn.into()
+    }
+
+    async fn cb(&self) -> U64 {
+        self.0.cb.into()
+    }
+
+    async fn cfei(&self) -> U64 {
+        self.0.cfei.into()
+    }
+
+    async fn cfsi(&self) -> U64 {
+        self.0.cfsi.into()
+    }
+
+    async fn croo(&self) -> U64 {
+        self.0.croo.into()
+    }
+
+    async fn div(&self) -> U64 {
+        self.0.div.into()
+    }
+
+    async fn divi(&self) -> U64 {
+        self.0.divi.into()
+    }
+
+    async fn ecr(&self) -> U64 {
+        self.0.ecr.into()
+    }
+
+    async fn eq(&self) -> U64 {
+        self.0.eq.into()
+    }
+
+    async fn exp(&self) -> U64 {
+        self.0.exp.into()
+    }
+
+    async fn expi(&self) -> U64 {
+        self.0.expi.into()
+    }
+
+    async fn flag(&self) -> U64 {
+        self.0.flag.into()
+    }
+
+    async fn gm(&self) -> U64 {
+        self.0.gm.into()
+    }
+
+    async fn gt(&self) -> U64 {
+        self.0.gt.into()
+    }
+
+    async fn gtf(&self) -> U64 {
+        self.0.gtf.into()
+    }
+
+    async fn ji(&self) -> U64 {
+        self.0.ji.into()
+    }
+
+    async fn jmp(&self) -> U64 {
+        self.0.jmp.into()
+    }
+
+    async fn jne(&self) -> U64 {
+        self.0.jne.into()
+    }
+
+    async fn jnei(&self) -> U64 {
+        self.0.jnei.into()
+    }
+
+    async fn jnzi(&self) -> U64 {
+        self.0.jnzi.into()
+    }
+
+    async fn jmpf(&self) -> U64 {
+        self.0.jmpf.into()
+    }
+
+    async fn jmpb(&self) -> U64 {
+        self.0.jmpb.into()
+    }
+
+    async fn jnzf(&self) -> U64 {
+        self.0.jnzf.into()
+    }
+
+    async fn jnzb(&self) -> U64 {
+        self.0.jnzb.into()
+    }
+
+    async fn jnef(&self) -> U64 {
+        self.0.jnef.into()
+    }
+
+    async fn jneb(&self) -> U64 {
+        self.0.jneb.into()
+    }
+
+    async fn k256(&self) -> U64 {
+        self.0.k256.into()
+    }
+
+    async fn lb(&self) -> U64 {
+        self.0.lb.into()
+    }
+
+    async fn log(&self) -> U64 {
+        self.0.log.into()
+    }
+
+    async fn lt(&self) -> U64 {
+        self.0.lt.into()
+    }
+
+    async fn lw(&self) -> U64 {
+        self.0.lw.into()
+    }
+
+    async fn mcpi(&self) -> U64 {
+        self.0.mcpi.into()
+    }
+
+    async fn mint(&self) -> U64 {
+        self.0.mint.into()
+    }
+
+    async fn mlog(&self) -> U64 {
+        self.0.mlog.into()
+    }
+
+    async fn mod_op(&self) -> U64 {
+        self.0.mod_op.into()
+    }
+
+    async fn modi(&self) -> U64 {
+        self.0.modi.into()
+    }
+
+    async fn move_op(&self) -> U64 {
+        self.0.move_op.into()
+    }
+
+    async fn movi(&self) -> U64 {
+        self.0.movi.into()
+    }
+
+    async fn mroo(&self) -> U64 {
+        self.0.mroo.into()
+    }
+
+    async fn mul(&self) -> U64 {
+        self.0.mul.into()
+    }
+
+    async fn muli(&self) -> U64 {
+        self.0.muli.into()
+    }
+
+    async fn mldv(&self) -> U64 {
+        self.0.mldv.into()
+    }
+
+    async fn noop(&self) -> U64 {
+        self.0.noop.into()
+    }
+
+    async fn not(&self) -> U64 {
+        self.0.not.into()
+    }
+
+    async fn or(&self) -> U64 {
+        self.0.or.into()
+    }
+
+    async fn ori(&self) -> U64 {
+        self.0.ori.into()
+    }
+
+    async fn ret(&self) -> U64 {
+        self.0.ret.into()
+    }
+
+    async fn rvrt(&self) -> U64 {
+        self.0.rvrt.into()
+    }
+
+    async fn s256(&self) -> U64 {
+        self.0.s256.into()
+    }
+
+    async fn sb(&self) -> U64 {
+        self.0.sb.into()
+    }
+
+    async fn scwq(&self) -> U64 {
+        self.0.scwq.into()
+    }
+
+    async fn sll(&self) -> U64 {
+        self.0.sll.into()
+    }
+
+    async fn slli(&self) -> U64 {
+        self.0.slli.into()
+    }
+
+    async fn srl(&self) -> U64 {
+        self.0.srl.into()
+    }
+
+    async fn srli(&self) -> U64 {
+        self.0.srli.into()
+    }
+
+    async fn srw(&self) -> U64 {
+        self.0.srw.into()
+    }
+
+    async fn sub(&self) -> U64 {
+        self.0.sub.into()
+    }
+
+    async fn subi(&self) -> U64 {
+        self.0.subi.into()
+    }
+
+    async fn sw(&self) -> U64 {
+        self.0.sw.into()
+    }
+
+    async fn sww(&self) -> U64 {
+        self.0.sww.into()
+    }
+
+    async fn swwq(&self) -> U64 {
+        self.0.swwq.into()
+    }
+
+    async fn time(&self) -> U64 {
+        self.0.time.into()
+    }
+
+    async fn tr(&self) -> U64 {
+        self.0.tr.into()
+    }
+
+    async fn tro(&self) -> U64 {
+        self.0.tro.into()
+    }
+
+    async fn wdcm(&self) -> U64 {
+        self.0.wdcm.into()
+    }
+
+    async fn wqcm(&self) -> U64 {
+        self.0.wqcm.into()
+    }
+
+    async fn wdop(&self) -> U64 {
+        self.0.wdop.into()
+    }
+
+    async fn wqop(&self) -> U64 {
+        self.0.wqop.into()
+    }
+
+    async fn wdml(&self) -> U64 {
+        self.0.wdml.into()
+    }
+
+    async fn wqml(&self) -> U64 {
+        self.0.wqml.into()
+    }
+
+    async fn wddv(&self) -> U64 {
+        self.0.wddv.into()
+    }
+
+    async fn wqdv(&self) -> U64 {
+        self.0.wqdv.into()
+    }
+
+    async fn wdmd(&self) -> U64 {
+        self.0.wdmd.into()
+    }
+
+    async fn wqmd(&self) -> U64 {
+        self.0.wqmd.into()
+    }
+
+    async fn wdam(&self) -> U64 {
+        self.0.wdam.into()
+    }
+
+    async fn wqam(&self) -> U64 {
+        self.0.wqam.into()
+    }
+
+    async fn wdmm(&self) -> U64 {
+        self.0.wdmm.into()
+    }
+
+    async fn wqmm(&self) -> U64 {
+        self.0.wqmm.into()
+    }
+
+    async fn xor(&self) -> U64 {
+        self.0.xor.into()
+    }
+
+    async fn xori(&self) -> U64 {
+        self.0.xori.into()
+    }
+
+    async fn call(&self) -> DependentCost {
+        self.0.call.into()
+    }
+
+    async fn ccp(&self) -> DependentCost {
+        self.0.ccp.into()
+    }
+
+    async fn csiz(&self) -> DependentCost {
+        self.0.csiz.into()
+    }
+
+    async fn ldc(&self) -> DependentCost {
+        self.0.ldc.into()
+    }
+
+    async fn logd(&self) -> DependentCost {
+        self.0.logd.into()
+    }
+
+    async fn mcl(&self) -> DependentCost {
+        self.0.mcl.into()
+    }
+
+    async fn mcli(&self) -> DependentCost {
+        self.0.mcli.into()
+    }
+
+    async fn mcp(&self) -> DependentCost {
+        self.0.mcp.into()
+    }
+
+    async fn meq(&self) -> DependentCost {
+        self.0.meq.into()
+    }
+
+    async fn retd(&self) -> DependentCost {
+        self.0.retd.into()
+    }
+
+    async fn smo(&self) -> DependentCost {
+        self.0.smo.into()
+    }
+
+    async fn srwq(&self) -> DependentCost {
+        self.0.srwq.into()
+    }
+}
+
+#[Object]
+impl DependentCost {
+    async fn base(&self) -> U64 {
+        self.0.base.into()
+    }
+
+    async fn dep_per_unit(&self) -> U64 {
+        self.0.dep_per_unit.into()
+    }
+}
+
+#[Object]
 impl ChainInfo {
     async fn name(&self, ctx: &Context<'_>) -> async_graphql::Result<String> {
         let data: &Database = ctx.data_unchecked();
@@ -123,6 +550,12 @@ impl ChainInfo {
         let config = ctx.data_unchecked::<GraphQLConfig>();
 
         Ok(ConsensusParameters(config.transaction_parameters))
+    }
+
+    async fn gas_costs(&self, ctx: &Context<'_>) -> async_graphql::Result<GasCosts> {
+        let config = ctx.data_unchecked::<GraphQLConfig>();
+
+        Ok(GasCosts(config.gas_costs.clone()))
     }
 }
 
