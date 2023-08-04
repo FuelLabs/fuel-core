8baf6521a3767c1ce126dc63ae94ae097e3bcc5c
Brandon Vrooman <brandon.vrooman@gmail.com>
2023-08-04 14:13:40 -0400

    Simplify

--- benches/benches/import.rs
+++ benches/benches/import.rs
@@ -80,21 +80,6 @@ struct Input {
     executes: Duration,
 }
 
-#[derive(Debug, Default, PartialEq, Eq, PartialOrd, Ord, Clone)]
-struct Count {
-    headers: usize,
-    transactions: usize,
-    consensus: usize,
-    executes: usize,
-    blocks: usize,
-}
-
-#[derive(Debug, Default, PartialEq, Eq)]
-struct Counts {
-    now: Count,
-    max: Count,
-}
-
 pub(crate) fn empty_header(h: BlockHeight) -> SourcePeer<SealedBlockHeader> {
     let mut header = BlockHeader::default();
     header.consensus.height = h;
@@ -113,36 +98,29 @@ pub(crate) fn empty_header(h: BlockHeight) -> SourcePeer<SealedBlockHeader> {
     }
 }
 
-type SharedCounts = SharedMutex<Counts>;
-
-struct PressurePeerToPeerPort(MockPeerToPeerPort, [Duration; 2], SharedCounts);
-struct PressureBlockImporterPort(MockBlockImporterPort, Duration, SharedCounts);
-
-struct PressureConsensusPort(MockConsensusPort, Duration, SharedCounts);
+struct PressurePeerToPeerPort(MockPeerToPeerPort, [Duration; 2]);
+struct PressureBlockImporterPort(MockBlockImporterPort, Duration);
+struct PressureConsensusPort(MockConsensusPort, Duration);
 
 #[async_trait::async_trait]
 impl PeerToPeerPort for PressurePeerToPeerPort {
     fn height_stream(&self) -> BoxStream<BlockHeight> {
         self.0.height_stream()
     }
+
     async fn get_sealed_block_header(
         &self,
         height: BlockHeight,
     ) -> anyhow::Result<Option<SourcePeer<SealedBlockHeader>>> {
         tokio::time::sleep(self.1[0]).await;
-        self.2.apply(|c| c.inc_headers());
-        self.2.apply(|c| {
-            c.inc_blocks();
-        });
         self.0.get_sealed_block_header(height).await
     }
+
     async fn get_transactions(
         &self,
         block_id: SourcePeer<BlockId>,
     ) -> anyhow::Result<Option<Vec<Transaction>>> {
         tokio::time::sleep(self.1[1]).await;
-        self.2.apply(|c| c.inc_transactions());
-        self.2.apply(|c| c.dec_headers());
         self.0.get_transactions(block_id).await
     }
 }
@@ -160,14 +138,6 @@ impl BlockImporterPort for PressureBlockImporterPort {
         })
         .await
         .unwrap();
-        self.2.apply(|c| {
-            c.dec_transactions();
-            c.inc_executes()
-        });
-        self.2.apply(|c| {
-            c.dec_executes();
-            c.dec_blocks();
-        });
         self.0.execute_and_commit(block).await
     }
 }
@@ -179,76 +149,36 @@ impl ConsensusPort for PressureConsensusPort {
     }
 
     async fn await_da_height(&self, da_height: &DaBlockHeight) -> anyhow::Result<()> {
-        self.2.apply(|c| c.inc_consensus());
         tokio::time::sleep(self.1).await;
-        self.2.apply(|c| c.dec_consensus());
         self.0.await_da_height(da_height).await
     }
 }
 
 impl PressurePeerToPeerPort {
-    fn new(counts: SharedCounts, delays: [Duration; 2]) -> Self {
+    fn new(delays: [Duration; 2]) -> Self {
         let mut mock = MockPeerToPeerPort::default();
         mock.expect_get_sealed_block_header()
             .returning(|h| Ok(Some(empty_header(h))));
         mock.expect_get_transactions()
             .returning(|_| Ok(Some(vec![])));
-        Self(mock, delays, counts)
+        Self(mock, delays)
     }
 }
 
 impl PressureBlockImporterPort {
-    fn new(counts: SharedCounts, delays: Duration) -> Self {
+    fn new(delays: Duration) -> Self {
         let mut mock = MockBlockImporterPort::default();
         mock.expect_execute_and_commit().returning(move |_| Ok(()));
-        Self(mock, delays, counts)
+        Self(mock, delays)
     }
 }
 
 impl PressureConsensusPort {
-    fn new(counts: SharedCounts, delays: Duration) -> Self {
+    fn new(delays: Duration) -> Self {
         let mut mock = MockConsensusPort::default();
         mock.expect_await_da_height().returning(|_| Ok(()));
         mock.expect_check_sealed_header().returning(|_| Ok(true));
-        Self(mock, delays, counts)
-    }
-}
-
-impl Counts {
-    fn inc_headers(&mut self) {
-        self.now.headers += 1;
-        self.max.headers = self.max.headers.max(self.now.headers);
-    }
-    fn dec_headers(&mut self) {
-        self.now.headers -= 1;
-    }
-    fn inc_transactions(&mut self) {
-        self.now.transactions += 1;
-        self.max.transactions = self.max.transactions.max(self.now.transactions);
-    }
-    fn dec_transactions(&mut self) {
-        self.now.transactions -= 1;
-    }
-    fn inc_consensus(&mut self) {
-        self.now.consensus += 1;
-        self.max.consensus = self.max.consensus.max(self.now.consensus);
-    }
-    fn dec_consensus(&mut self) {
-        self.now.consensus -= 1;
-    }
-    fn inc_executes(&mut self) {
-        self.now.executes += 1;
-        self.max.executes = self.max.executes.max(self.now.executes);
-    }
-    fn dec_executes(&mut self) {
-        self.now.executes -= 1;
-    }
-    fn inc_blocks(&mut self) {
-        self.now.blocks += 1;
-        self.max.blocks = self.max.blocks.max(self.now.blocks);
-    }
-    fn dec_blocks(&mut self) {
-        self.now.blocks -= 1;
+        Self(mock, delays)
     }
 }
 
@@ -259,24 +189,20 @@ async fn test() {
         executes: Duration::from_millis(10),
         ..Default::default()
     };
-    let state = State::new(None, 50);
     let params = Config {
         max_get_header_requests: 10,
         max_get_txns_requests: 10,
     };
-
-    let counts = SharedCounts::new(Default::default());
+    let state = State::new(None, 50);
     let state = SharedMutex::new(state);
 
-    let p2p = Arc::new(PressurePeerToPeerPort::new(
-        counts.clone(),
-        [input.headers, input.transactions],
-    ));
-    let executor = Arc::new(PressureBlockImporterPort::new(
-        counts.clone(),
-        input.executes,
-    ));
-    let consensus = Arc::new(PressureConsensusPort::new(counts.clone(), input.consensus));
+    let p2p = Arc::new(PressurePeerToPeerPort::new([
+        input.headers,
+        input.transactions,
+    ]));
+
+    let executor = Arc::new(PressureBlockImporterPort::new(input.executes));
+    let consensus = Arc::new(PressureConsensusPort::new(input.consensus));
     let notify = Arc::new(Notify::new());
 
     let (_tx, shutdown) = tokio::sync::watch::channel(fuel_core_services::State::Started);
