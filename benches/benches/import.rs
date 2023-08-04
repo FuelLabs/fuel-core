3b349ef4f318dfdbc762ffd4775846a189edbd52
Brandon Vrooman <brandon.vrooman@gmail.com>
2023-08-04 14:48:28 -0400

    Update import.rs

--- benches/benches/import.rs
+++ benches/benches/import.rs
@@ -1,9 +1,6 @@
 use criterion::{
-    async_executor::AsyncExecutor,
     criterion_group,
     criterion_main,
-    measurement::WallTime,
-    BenchmarkGroup,
     Criterion,
 };
 use fuel_core_benches::import::{
@@ -43,13 +40,14 @@ async fn test() {
         executes: Duration::from_millis(10),
         ..Default::default()
     };
+
+    let state = State::new(None, 50);
+    let state = SharedMutex::new(state);
+    let notify = Arc::new(Notify::new());
     let params = Config {
         max_get_header_requests: 10,
         max_get_txns_requests: 10,
     };
-    let state = State::new(None, 50);
-    let state = SharedMutex::new(state);
-
     let p2p = Arc::new(PressurePeerToPeerPort::new([
         input.headers,
         input.transactions,
@@ -57,7 +55,6 @@ async fn test() {
     let executor = Arc::new(PressureBlockImporterPort::new(input.executes));
     let consensus = Arc::new(PressureConsensusPort::new(input.consensus));
 
-    let notify = Arc::new(Notify::new());
     let (_tx, shutdown) = tokio::sync::watch::channel(fuel_core_services::State::Started);
     let mut watcher = shutdown.into();
 
@@ -69,7 +66,7 @@ async fn test() {
 fn import_one(c: &mut Criterion) {
     let rt = Runtime::new().unwrap();
     let mut group = c.benchmark_group("import");
-    group.bench_function("import one", |b| b.to_async(&rt).iter(|| test()));
+    group.bench_function("import one", |b| b.to_async(&rt).iter(test));
 }
 
 criterion_group!(benches, import_one);
