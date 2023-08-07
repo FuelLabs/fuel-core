4d375670657743004e5dd98e0e8a6d4114fdb165
Brandon Vrooman <brandon.vrooman@gmail.com>
2023-08-07 11:30:24 -0400

    Version tests

--- benches/benches/import.rs
+++ benches/benches/import.rs
@@ -2,6 +2,8 @@ use criterion::{
     black_box,
     criterion_group,
     criterion_main,
+    measurement::WallTime,
+    BenchmarkGroup,
     Criterion,
 };
 use fuel_core_benches::import::{
@@ -21,6 +23,10 @@ use fuel_core_sync::{
     state::State,
 };
 use std::{
+    fmt::{
+        Display,
+        Formatter,
+    },
     sync::Arc,
     time::Duration,
 };
@@ -35,7 +41,7 @@ use tokio::{
 type PressureImport =
     Import<PressurePeerToPeerPort, PressureBlockImporterPort, PressureConsensusPort>;
 
-#[derive(Default)]
+#[derive(Default, Clone, Copy)]
 struct Durations {
     headers: Duration,
     consensus: Duration,
@@ -78,220 +84,97 @@ fn create_import(
     (import, tx, watcher)
 }
 
-async fn test(import: &PressureImport, shutdown: &mut StateWatcher) {
-    import.import(shutdown).await.unwrap();
+#[derive(Clone, Copy)]
+enum Version {
+    V1,
+    V2,
+    V3,
 }
 
-fn import_one(c: &mut Criterion) {
-    let rt = Runtime::new().unwrap();
+impl Display for Version {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Version::V1 => write!(f, "v1"),
+            Version::V2 => write!(f, "v2"),
+            Version::V3 => write!(f, "v3"),
+        }
+    }
+}
+
+async fn import_version_switch(
+    import: PressureImport,
+    version: Version,
+    shutdown: &mut StateWatcher,
+) {
+    match version {
+        Version::V1 => import.import(shutdown).await.unwrap(),
+        Version::V2 => import.import_v2(shutdown).await.unwrap(),
+        Version::V3 => import.import_v3(shutdown).await.unwrap(),
+    };
+}
+
+fn bench_imports(c: &mut Criterion) {
+    let bench_import = |group: &mut BenchmarkGroup<WallTime>,
+                        version: Version,
+                        n: u32,
+                        durations: Durations,
+                        buffer_size: usize| {
+        let name = format!(
+            "import {version} - {n} * {d_h}/{d_c}/{d_t}/{d_e} - {sz}",
+            version = version,
+            n = n,
+            d_h = durations.headers.as_millis(),
+            d_c = durations.consensus.as_millis(),
+            d_t = durations.transactions.as_millis(),
+            d_e = durations.executes.as_millis(),
+            sz = buffer_size
+        );
+        group.bench_function(name, move |b| {
+            let rt = Runtime::new().unwrap();
+            b.to_async(&rt).iter_custom(|iters| async move {
+                let mut elapsed_time = Duration::default();
+                for _ in 0..iters {
+                    let state = State::new(None, n);
+                    let shared_state = SharedMutex::new(state);
+                    let (import, _tx, mut shutdown) =
+                        create_import(shared_state, durations, buffer_size, buffer_size);
+                    import.notify_one();
+                    let start = std::time::Instant::now();
+                    black_box(
+                        import_version_switch(import, version, &mut shutdown).await,
+                    );
+                    elapsed_time += start.elapsed();
+                }
+                elapsed_time
+            })
+        });
+    };
+
     let mut group = c.benchmark_group("import");
-    group.bench_function("import v1 - 500 * 5/5/5/15 - 10/10", |b| {
-        b.to_async(&rt).iter_custom(|iters| async move {
-            let mut elapsed_time = Duration::default();
-            for _ in 0..iters {
-                let state = State::new(None, 500);
-                let shared_state = SharedMutex::new(state);
-                let input = Durations {
-                    headers: Duration::from_millis(5),
-                    consensus: Duration::from_millis(5),
-                    transactions: Duration::from_millis(5),
-                    executes: Duration::from_millis(15),
-                };
-                let (import, _tx, mut shutdown) =
-                    create_import(shared_state, input, 10, 10);
-                import.notify_one();
-                let start = std::time::Instant::now();
-                black_box(import.import(&mut shutdown).await.unwrap());
-                elapsed_time += start.elapsed();
-            }
-            elapsed_time
-        })
-    });
 
-    group.bench_function("import v1 - 500 * 5/5/5/15 - 100/100", |b| {
-        b.to_async(&rt).iter_custom(|iters| async move {
-            let mut elapsed_time = Duration::default();
-            for _ in 0..iters {
-                let state = State::new(None, 500);
-                let shared_state = SharedMutex::new(state);
-                let input = Durations {
-                    headers: Duration::from_millis(5),
-                    consensus: Duration::from_millis(5),
-                    transactions: Duration::from_millis(5),
-                    executes: Duration::from_millis(15),
-                };
-                let (import, _tx, mut shutdown) =
-                    create_import(shared_state, input, 100, 100);
-                import.notify_one();
-                let start = std::time::Instant::now();
-                black_box(import.import(&mut shutdown).await.unwrap());
-                elapsed_time += start.elapsed();
-            }
-            elapsed_time
-        })
-    });
+    let durations = Durations {
+        headers: Duration::from_millis(5),
+        consensus: Duration::from_millis(5),
+        transactions: Duration::from_millis(5),
+        executes: Duration::from_millis(10),
+    };
+    let n = 50;
 
-    group.bench_function("import v1 - 500 * 5/5/5/15 - 500/500", |b| {
-        b.to_async(&rt).iter_custom(|iters| async move {
-            let mut elapsed_time = Duration::default();
-            for _ in 0..iters {
-                let state = State::new(None, 500);
-                let shared_state = SharedMutex::new(state);
-                let input = Durations {
-                    headers: Duration::from_millis(5),
-                    consensus: Duration::from_millis(5),
-                    transactions: Duration::from_millis(5),
-                    executes: Duration::from_millis(15),
-                };
-                let (import, _tx, mut shutdown) =
-                    create_import(shared_state, input, 500, 500);
-                import.notify_one();
-                let start = std::time::Instant::now();
-                black_box(import.import(&mut shutdown).await.unwrap());
-                elapsed_time += start.elapsed();
-            }
-            elapsed_time
-        })
-    });
+    // V1
+    bench_import(&mut group, Version::V1, n, durations, 5);
+    bench_import(&mut group, Version::V1, n, durations, 10);
+    bench_import(&mut group, Version::V1, n, durations, 50);
 
-    group.bench_function("import v2 - 500 * 5/5/5/15 - 10", |b| {
-        b.to_async(&rt).iter_custom(|iters| async move {
-            let mut elapsed_time = Duration::default();
-            for _ in 0..iters {
-                let state = State::new(None, 500);
-                let shared_state = SharedMutex::new(state);
-                let input = Durations {
-                    headers: Duration::from_millis(5),
-                    consensus: Duration::from_millis(5),
-                    transactions: Duration::from_millis(5),
-                    executes: Duration::from_millis(15),
-                };
-                let (import, _tx, mut shutdown) =
-                    create_import(shared_state, input, 0, 10);
-                import.notify_one();
-                let start = std::time::Instant::now();
-                black_box(import.import_v2(&mut shutdown).await.unwrap());
-                elapsed_time += start.elapsed();
-            }
-            elapsed_time
-        })
-    });
+    // V2
+    bench_import(&mut group, Version::V2, n, durations, 5);
+    bench_import(&mut group, Version::V2, n, durations, 10);
+    bench_import(&mut group, Version::V2, n, durations, 50);
 
-    group.bench_function("import v2 - 500 * 5/5/5/15 - 100", |b| {
-        b.to_async(&rt).iter_custom(|iters| async move {
-            let mut elapsed_time = Duration::default();
-            for _ in 0..iters {
-                let state = State::new(None, 500);
-                let shared_state = SharedMutex::new(state);
-                let input = Durations {
-                    headers: Duration::from_millis(5),
-                    consensus: Duration::from_millis(5),
-                    transactions: Duration::from_millis(5),
-                    executes: Duration::from_millis(15),
-                };
-                let (import, _tx, mut shutdown) =
-                    create_import(shared_state, input, 0, 100);
-                import.notify_one();
-                let start = std::time::Instant::now();
-                black_box(import.import_v2(&mut shutdown).await.unwrap());
-                elapsed_time += start.elapsed();
-            }
-            elapsed_time
-        })
-    });
-
-    group.bench_function("import v2 - 500 * 5/5/5/15 - 500", |b| {
-        b.to_async(&rt).iter_custom(|iters| async move {
-            let mut elapsed_time = Duration::default();
-            for _ in 0..iters {
-                let state = State::new(None, 500);
-                let shared_state = SharedMutex::new(state);
-                let input = Durations {
-                    headers: Duration::from_millis(5),
-                    consensus: Duration::from_millis(5),
-                    transactions: Duration::from_millis(5),
-                    executes: Duration::from_millis(15),
-                };
-                let (import, _tx, mut shutdown) =
-                    create_import(shared_state, input, 0, 500);
-                import.notify_one();
-                let start = std::time::Instant::now();
-                black_box(import.import_v2(&mut shutdown).await.unwrap());
-                elapsed_time += start.elapsed();
-            }
-            elapsed_time
-        })
-    });
-
-    group.bench_function("import v3 - 500 * 5/5/5/15 - 10", |b| {
-        b.to_async(&rt).iter_custom(|iters| async move {
-            let mut elapsed_time = Duration::default();
-            for _ in 0..iters {
-                let state = State::new(None, 500);
-                let shared_state = SharedMutex::new(state);
-                let input = Durations {
-                    headers: Duration::from_millis(5),
-                    consensus: Duration::from_millis(5),
-                    transactions: Duration::from_millis(5),
-                    executes: Duration::from_millis(15),
-                };
-                let (import, _tx, mut shutdown) =
-                    create_import(shared_state, input, 0, 10);
-                import.notify_one();
-                let start = std::time::Instant::now();
-                black_box(import.import_v3(&mut shutdown).await.unwrap());
-                elapsed_time += start.elapsed();
-            }
-            elapsed_time
-        })
-    });
-
-    group.bench_function("import v3 - 500 * 5/5/5/15 - 100", |b| {
-        b.to_async(&rt).iter_custom(|iters| async move {
-            let mut elapsed_time = Duration::default();
-            for _ in 0..iters {
-                let state = State::new(None, 500);
-                let shared_state = SharedMutex::new(state);
-                let input = Durations {
-                    headers: Duration::from_millis(5),
-                    consensus: Duration::from_millis(5),
-                    transactions: Duration::from_millis(5),
-                    executes: Duration::from_millis(15),
-                };
-                let (import, _tx, mut shutdown) =
-                    create_import(shared_state, input, 0, 100);
-                import.notify_one();
-                let start = std::time::Instant::now();
-                black_box(import.import_v3(&mut shutdown).await.unwrap());
-                elapsed_time += start.elapsed();
-            }
-            elapsed_time
-        })
-    });
-
-    group.bench_function("import v3 - 500 * 5/5/5/15 - 500", |b| {
-        b.to_async(&rt).iter_custom(|iters| async move {
-            let mut elapsed_time = Duration::default();
-            for _ in 0..iters {
-                let state = State::new(None, 500);
-                let shared_state = SharedMutex::new(state);
-                let input = Durations {
-                    headers: Duration::from_millis(5),
-                    consensus: Duration::from_millis(5),
-                    transactions: Duration::from_millis(5),
-                    executes: Duration::from_millis(15),
-                };
-                let (import, _tx, mut shutdown) =
-                    create_import(shared_state, input, 0, 500);
-                import.notify_one();
-                let start = std::time::Instant::now();
-                black_box(import.import_v3(&mut shutdown).await.unwrap());
-                elapsed_time += start.elapsed();
-            }
-            elapsed_time
-        })
-    });
+    // V3
+    bench_import(&mut group, Version::V3, n, durations, 5);
+    bench_import(&mut group, Version::V3, n, durations, 10);
+    bench_import(&mut group, Version::V3, n, durations, 50);
 }
 
-criterion_group!(benches, import_one);
+criterion_group!(benches, bench_imports);
 criterion_main!(benches);
