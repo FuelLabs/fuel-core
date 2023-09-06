250e19cd984b615bc523d0bec04aa981384c13d4
Brandon Vrooman <brandon.vrooman@gmail.com>
2023-09-05 22:07:20 -0400

    Migrate to canonical serialization

--- crates/fuel-core/src/schema/tx/types.rs
+++ crates/fuel-core/src/schema/tx/types.rs
@@ -60,7 +60,7 @@ use fuel_core_types::{
         Chargeable,
         Executable,
     },
-    fuel_types::bytes::SerializableVec,
+    fuel_types::canonical::SerializedSize,
     fuel_vm::ProgramState as VmProgramState,
     services::{
         txpool,
