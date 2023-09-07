f3f2f4148a57c53eed7d1ed57ec0816befd03a3b
Brandon Kite <brandonkite92@gmail.com>
2023-09-06 23:22:43 -0700

    remove metrics flag since we're leaking into all builds anyways

--- crates/metrics/src/graphql_metrics.rs
+++ crates/metrics/src/graphql_metrics.rs
@@ -40,6 +40,6 @@ impl GraphqlMetrics {
 lazy_static! {
     pub static ref GRAPHQL_METRICS: GraphqlMetrics = GraphqlMetrics::new();
     // recommended bucket defaults for API response times
-    static ref BUCKETS: Vec<f64> =
+    pub static ref BUCKETS: Vec<f64> =
         vec![0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1.0, 2.5, 5.0, 10.0];
 }
