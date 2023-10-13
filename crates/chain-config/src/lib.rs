662812cc388fff2534a08d81a8bd80ba2caba1e9
Brandon Vrooman <brandon.vrooman@gmail.com>
2023-10-13 12:39:09 -0400

    Add comment for default consensus key

--- crates/chain-config/src/lib.rs
+++ crates/chain-config/src/lib.rs
@@ -11,6 +11,11 @@ pub use genesis::GenesisCommitment;
 
 /// A default secret key to use for testing purposes only
 pub fn default_consensus_dev_key() -> SecretKey {
+    // Derived from:
+    //  - Mnemonic phrase: "winner alley monkey elephant sun off boil hope toward boss bronze dish"
+    //  - Path: "m/44'/60'/0'/0/0"
+    // Equivalent to:
+    //  `SecretKey::new_from_mnemonic_phrase_with_path(..)`
     let bytes: [u8; 32] = [
         0xfb, 0xe4, 0x91, 0x78, 0xda, 0xc2, 0xdf, 0x5f, 0xde, 0xa7, 0x4a, 0x11, 0xa9,
         0x0f, 0x99, 0x77, 0x62, 0x5f, 0xe0, 0x23, 0xcd, 0xf6, 0x41, 0x4b, 0xfd, 0x63,
