c239be8015334e691165c6935e30861fba842dbc
Turner <james.mitchell.turner@gmail.com>
2023-10-13 10:49:48 -0700

    Unify utils

--- benches/benches/set/alu.rs
+++ benches/benches/set/alu.rs
@@ -14,26 +14,14 @@ use fuel_core_types::fuel_asm::wideint::{
     MulArgs,
 };
 
-/// Allocates a byte array from heap and initializes it. Then points `reg` to it.
-fn aloc_bytearray<const S: usize>(reg: u8, v: [u8; S]) -> Vec<Instruction> {
-    let mut ops = vec![op::movi(reg, S as u32), op::aloc(reg)];
-    for (i, b) in v.iter().enumerate() {
-        if *b != 0 {
-            ops.push(op::movi(reg, *b as u32));
-            ops.push(op::sb(RegId::HP, reg, i as u16));
-        }
-    }
-    ops.push(op::move_(reg, RegId::HP));
-    ops
-}
+#[path = "../utils.rs"]
+mod utils;
 
-fn make_u128(reg: u8, v: u128) -> Vec<Instruction> {
-    aloc_bytearray(reg, v.to_be_bytes())
-}
-
-fn make_u256(reg: u8, v: U256) -> Vec<Instruction> {
-    aloc_bytearray(reg, v.to_be_bytes())
-}
+use utils::{
+    aloc_bytearray,
+    make_u128,
+    make_u256,
+};
 
 pub fn run(c: &mut Criterion) {
     run_group_ref(
