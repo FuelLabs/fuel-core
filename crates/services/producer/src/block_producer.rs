4c750453d1c40d86a65c5ba0780691a76a5cf005
Brandon Vrooman <brandon.vrooman@gmail.com>
2023-10-13 14:50:47 -0400

    Clean up

--- crates/services/producer/src/block_producer.rs
+++ crates/services/producer/src/block_producer.rs
@@ -237,7 +237,7 @@ where
         //  return a new error.
         // block 0 is reserved for genesis
         if height == 0u32.into() {
-            Err(Error::GenesisBlock).map_err(Into::into)
+            Err(Error::GenesisBlock.into())
         } else {
             // get info from previous block height
             let prev_height = height - 1u32.into();
