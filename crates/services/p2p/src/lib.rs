6c95b541a8ddea1605b69edea0d510057f01cc95
Green Baneling <XgreenX9999@gmail.com>
2023-10-23 11:39:14 +0200

    Release 0.20.8 (#1435)
    Beta 4 authority node punished some sentries because of some invalid
    transactions they sent because of block race.
    
    The fix is to not punish the reserved peer for sending an invalid
    transaction.
    
    ---------
    
    Co-authored-by: Brandon Kite <brandonkite92@gmail.com>

--- crates/services/p2p/src/lib.rs
+++ crates/services/p2p/src/lib.rs
@@ -1,14 +1,14 @@
-mod behavior;
+pub mod behavior;
 pub mod codecs;
 pub mod config;
-mod discovery;
-mod gossipsub;
-mod heartbeat;
-mod p2p_service;
-mod peer_manager;
-mod peer_report;
+pub mod discovery;
+pub mod gossipsub;
+pub mod heartbeat;
+pub mod p2p_service;
+pub mod peer_manager;
+pub mod peer_report;
 pub mod ports;
-mod request_response;
+pub mod request_response;
 pub mod service;
 
 pub use gossipsub::config as gossipsub_config;
