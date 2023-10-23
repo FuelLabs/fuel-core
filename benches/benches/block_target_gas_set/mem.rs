8c2abd7a1e95a17530ac5841e7fbe8701360197d
Turner <james.mitchell.turner@gmail.com>
2023-10-23 11:59:41 -0700

    WIP

---
+++ benches/benches/block_target_gas_set/mem.rs
@@ -0,0 +1,23 @@
+use crate::*;
+
+// ALOC: Allocate memory
+// CFE: Extend call frame
+// CFEI: Extend call frame immediate
+// CFS: Shrink call frame
+// CFSI: Shrink call frame immediate
+// LB: Load byte
+// LW: Load word
+// MCL: Memory clear
+// MCLI: Memory clear immediate
+// MCP: Memory copy
+// MCPI: Memory copy immediate
+// MEQ: Memory equality
+// POPH: Pop a set of high registers from stack
+// POPL: Pop a set of low registers from stack
+// PSHH: Push a set of high registers to stack
+// PSHL: Push a set of low registers to stack
+// SB: Store byte
+// SW: Store word
+pub fn run_mem(group: &mut BenchmarkGroup<WallTime>) {
+    todo!()
+}
