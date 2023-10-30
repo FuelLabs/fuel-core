773b8e1ee79f763577cc08ec7799c3ce9ebc1bb4
Turner <james.mitchell.turner@gmail.com>
2023-10-30 14:49:52 -0700

    Add `bal` benches

--- benches/benches/block_target_gas_set/contract.rs
+++ benches/benches/block_target_gas_set/contract.rs
@@ -2,6 +2,7 @@ use crate::{
     utils::arb_dependent_cost_values,
     *,
 };
+use fuel_core::service::FuelService;
 use fuel_core_storage::{
     tables::ContractsRawCode,
     StorageAsMut,
@@ -40,6 +41,10 @@ use fuel_core_types::{
 // TR: Transfer coins to contract
 // TRO: Transfer coins to output
 pub fn run_contract(group: &mut BenchmarkGroup<WallTime>) {
+    let contract_id = ContractId::zeroed();
+    let (mut service, rt) = service_with_contract_id(contract_id);
+    let mut rng = rand::rngs::StdRng::seed_from_u64(2322u64);
+
     //     run_group_ref(
     //         &mut c.benchmark_group("bal"),
     //         "bal",
@@ -51,82 +56,64 @@ pub fn run_contract(group: &mut BenchmarkGroup<WallTime>) {
     //             ])
     //             .with_dummy_contract(contract),
     //     );
-
-    // let contract_id = ContractId::zeroed();
-    // let asset_id = AssetId::zeroed();
-    // run(
-    //     "contract/bal opcode",
-    //     group,
-    //     [
-    //         op::gtf_args(0x10, 0x00, GTFArgs::ScriptData),
-    //         op::addi(0x11, 0x10, asset_id.len().try_into().unwrap()),
-    //         op::bal(0x10, 0x10, 0x11),
-    //         op::jmpb(RegId::ZERO, 0),
-    //     ]
-    //     .to_vec(),
-    //     asset_id
-    //         .into_iter()
-    //         .chain(contract_id.into_iter())
-    //         .collect(),
-    // );
-
-    // This breaks the benchmarking
-    // for i in arb_dependent_cost_values() {
-    //     let id = format!("flow/retd_contract opcode {:?}", i);
-    //     run(
-    //         &id,
-    //         group,
-    //         vec![
-    //             op::movi(0x10, i),
-    //             op::retd(RegId::ONE, 0x10),
-    //             op::jmpb(RegId::ZERO, 0),
-    //         ]
-    //         .to_vec(),
-    //         vec![],
-    //     );
-    // }
-
-    //     let mut call = c.benchmark_group("call");
-    //
-    //     for i in linear.clone() {
-    //         let mut code = vec![0u8; i as usize];
-    //
-    //         rng.fill_bytes(&mut code);
-    //
-    //         let code = ContractCode::from(code);
-    //         let id = code.id;
-    //
-    //         let data = id
-    //             .iter()
-    //             .copied()
-    //             .chain((0 as Word).to_be_bytes().iter().copied())
-    //             .chain((0 as Word).to_be_bytes().iter().copied())
-    //             .chain(AssetId::default().iter().copied())
-    //             .collect();
-    //
-    //         let prepare_script = vec![
-    //             op::gtf_args(0x10, 0x00, GTFArgs::ScriptData),
-    //             op::addi(0x11, 0x10, ContractId::LEN.try_into().unwrap()),
-    //             op::addi(0x11, 0x11, WORD_SIZE.try_into().unwrap()),
-    //             op::addi(0x11, 0x11, WORD_SIZE.try_into().unwrap()),
-    //             op::movi(0x12, 100_000),
-    //         ];
-    //
-    //         call.throughput(Throughput::Bytes(i));
-    //
-    //         run_group_ref(
-    //             &mut call,
-    //             format!("{i}"),
-    //             VmBench::new(op::call(0x10, RegId::ZERO, 0x11, 0x12))
-    //                 .with_db(db.checkpoint())
-    //                 .with_contract_code(code)
-    //                 .with_data(data)
-    //                 .with_prepare_script(prepare_script),
-    //         );
-    //     }
+    let asset_id = AssetId::zeroed();
     let contract_id = ContractId::zeroed();
-    let (mut service, rt) = service_with_contract_id(contract_id);
-    let mut rng = rand::rngs::StdRng::seed_from_u64(2322u64);
+    let contract_instructions = vec![op::bal(0x13, 0x11, 0x10), op::jmpb(RegId::ZERO, 0)];
+
+    let instructions = vec![
+        op::gtf_args(0x10, 0x00, GTFArgs::ScriptData),
+        op::addi(0x11, 0x10, ContractId::LEN.try_into().unwrap()),
+        op::addi(0x11, 0x11, WORD_SIZE.try_into().unwrap()),
+        op::addi(0x11, 0x11, WORD_SIZE.try_into().unwrap()),
+        op::movi(0x12, 100_000),
+        op::call(0x10, RegId::ZERO, 0x11, 0x12),
+    ];
+
+    replace_contract_in_service(&mut service, &contract_id, contract_instructions);
+
+    let script_data: Vec<_> = contract_id
+        .iter()
+        .copied()
+        .chain((0 as Word).to_be_bytes().iter().copied())
+        .chain((0 as Word).to_be_bytes().iter().copied())
+        .chain(AssetId::default().iter().copied())
+        .collect();
+
+    let id = "contract/bal contract";
+    run_with_service(
+        id,
+        group,
+        instructions,
+        script_data.clone(),
+        &service,
+        contract_id,
+        &rt,
+        &mut rng,
+    );
+
+    let instructions = vec![
+        op::gtf_args(0x10, 0x00, GTFArgs::ScriptData),
+        op::addi(0x11, 0x10, ContractId::LEN.try_into().unwrap()),
+        op::addi(0x11, 0x11, WORD_SIZE.try_into().unwrap()),
+        op::addi(0x11, 0x11, WORD_SIZE.try_into().unwrap()),
+        op::movi(0x12, 100_000),
+        op::bal(0x13, 0x11, 0x10),
+        op::jmpb(RegId::ZERO, 0),
+    ];
+
+    let id = "contract/bal script";
+    run_with_service(
+        id,
+        group,
+        instructions,
+        script_data,
+        &service,
+        contract_id,
+        &rt,
+        &mut rng,
+    );
+
+    // Call
 
     for size in arb_dependent_cost_values() {
         let mut contract_instructions = std::iter::repeat(op::noop())
@@ -143,17 +130,8 @@ pub fn run_contract(group: &mut BenchmarkGroup<WallTime>) {
             op::call(0x10, RegId::ZERO, 0x11, 0x12),
             op::jmpb(RegId::ZERO, 0),
         ];
-        let contract_bytecode: Vec<_> = contract_instructions
-            .iter()
-            .map(|x| x.to_bytes())
-            .flatten()
-            .collect();
-        service
-            .shared
-            .database
-            .storage_as_mut::<ContractsRawCode>()
-            .insert(&contract_id, &contract_bytecode)
-            .unwrap();
+
+        replace_contract_in_service(&mut service, &contract_id, contract_instructions);
 
         let script_data = contract_id
             .iter()
@@ -175,4 +153,22 @@ pub fn run_contract(group: &mut BenchmarkGroup<WallTime>) {
             &mut rng,
         );
     }
+}
+
+fn replace_contract_in_service(
+    service: &mut FuelService,
+    contract_id: &ContractId,
+    contract_instructions: Vec<Instruction>,
+) {
+    let contract_bytecode: Vec<_> = contract_instructions
+        .iter()
+        .map(|x| x.to_bytes())
+        .flatten()
+        .collect();
+    service
+        .shared
+        .database
+        .storage_as_mut::<ContractsRawCode>()
+        .insert(contract_id, &contract_bytecode)
+        .unwrap();
 }
