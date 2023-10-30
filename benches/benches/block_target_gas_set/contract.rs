dc2aa9db978d799d1ee5d78a1195cf66596414d4
Turner <james.mitchell.turner@gmail.com>
2023-10-30 15:40:26 -0700

    Finish simple benches, bring over example code for complex ones

--- benches/benches/block_target_gas_set/contract.rs
+++ benches/benches/block_target_gas_set/contract.rs
@@ -113,6 +113,57 @@ pub fn run_contract(group: &mut BenchmarkGroup<WallTime>) {
         &mut rng,
     );
 
+    //     run_group_ref(
+    //         &mut c.benchmark_group("bhei"),
+    //         "bhei",
+    //         VmBench::new(op::bhei(0x10)),
+    //     );
+    run(
+        "contract/bhei",
+        group,
+        vec![op::bhei(0x10),
+            op::jmpb(RegId::ZERO, 0)],
+        vec![],
+    )
+
+    //     run_group_ref(
+    //         &mut c.benchmark_group("bhsh"),
+    //         "bhsh",
+    //         VmBench::new(op::bhsh(0x10, RegId::ZERO)).with_prepare_script(vec![
+    //             op::movi(0x10, Bytes32::LEN.try_into().unwrap()),
+    //             op::aloc(0x10),
+    //             op::move_(0x10, RegId::HP),
+    //         ]),
+    //     );
+    run(
+        "contract/bhsh",
+        group,
+        vec![
+            op::movi(0x10, Bytes32::LEN.try_into().unwrap()),
+            op::aloc(0x10),
+            op::move_(0x10, RegId::HP),
+            op::bhsh(0x10, RegId::ZERO),
+            op::jmpb(RegId::ZERO, 0)],
+        vec![],
+    )
+
+    //     run_group_ref(
+    //         &mut c.benchmark_group("burn"),
+    //         "burn",
+    //         VmBench::contract_using_db(rng, db.checkpoint(), op::burn(RegId::ONE, RegId::HP))
+    //             .expect("failed to prepare contract")
+    //             .prepend_prepare_script(vec![op::movi(0x10, 32), op::aloc(0x10)]),
+    //     );
+
+    // run(
+    //     "contract/burn",
+    //     group,
+    //     vec![op::movi(0x10, 32), op::aloc(0x10),
+    //         op::burn(RegId::ONE, RegId::HP),
+    //         op::jmpb(RegId::ZERO, 0)],
+    //     vec![],
+    // );
+
     // Call
 
     for size in arb_dependent_cost_values() {
@@ -153,6 +204,384 @@ pub fn run_contract(group: &mut BenchmarkGroup<WallTime>) {
             &mut rng,
         );
     }
+
+    //     run_group_ref(
+    //         &mut c.benchmark_group("cb"),
+    //         "cb",
+    //         VmBench::new(op::cb(0x10)).with_prepare_script(vec![
+    //             op::movi(0x10, Bytes32::LEN.try_into().unwrap()),
+    //             op::aloc(0x10),
+    //             op::move_(0x10, RegId::HP),
+    //         ]),
+    //     );
+    run(
+        "contract/cb",
+        group,
+        vec![
+            op::movi(0x10, Bytes32::LEN.try_into().unwrap()),
+            op::aloc(0x10),
+            op::move_(0x10, RegId::HP),
+            op::cb(0x10),
+            op::jmpb(RegId::ZERO, 0)],
+        vec![],
+    );
+
+    //     let mut ccp = c.benchmark_group("ccp");
+    //
+    //     for i in linear.clone() {
+    //         let mut code = vec![0u8; i as usize];
+    //
+    //         rng.fill_bytes(&mut code);
+    //
+    //         let code = ContractCode::from(code);
+    //         let id = code.id;
+    //
+    //         let data = id
+    //             .iter()
+    //             .copied()
+    //             .chain((0 as Word).to_be_bytes().iter().copied())
+    //             .chain((0 as Word).to_be_bytes().iter().copied())
+    //             .chain(AssetId::default().iter().copied())
+    //             .collect();
+    //
+    //         let prepare_script = vec![
+    //             op::gtf_args(0x10, 0x00, GTFArgs::ScriptData),
+    //             op::addi(0x11, 0x10, ContractId::LEN.try_into().unwrap()),
+    //             op::addi(0x11, 0x11, WORD_SIZE.try_into().unwrap()),
+    //             op::addi(0x11, 0x11, WORD_SIZE.try_into().unwrap()),
+    //             op::movi(0x12, 100_000),
+    //             op::movi(0x13, i.try_into().unwrap()),
+    //             op::movi(0x14, i.try_into().unwrap()),
+    //             op::movi(0x15, i.try_into().unwrap()),
+    //             op::add(0x15, 0x15, 0x15),
+    //             op::addi(0x15, 0x15, 32),
+    //             op::aloc(0x15),
+    //             op::move_(0x15, RegId::HP),
+    //         ];
+    //
+    //         ccp.throughput(Throughput::Bytes(i));
+    //
+    //         run_group_ref(
+    //             &mut ccp,
+    //             format!("{i}"),
+    //             VmBench::new(op::ccp(0x15, 0x10, RegId::ZERO, 0x13))
+    //                 .with_contract_code(code)
+    //                 .with_data(data)
+    //                 .with_prepare_script(prepare_script),
+    //         );
+    //     }
+    //
+    //     ccp.finish();
+
+    //     {
+    //         let mut input = VmBench::contract(rng, op::croo(0x14, 0x16))
+    //             .expect("failed to prepare contract");
+    //         input.post_call.extend(vec![
+    //             op::gtf_args(0x16, 0x00, GTFArgs::ScriptData),
+    //             op::movi(0x15, 2000),
+    //             op::aloc(0x15),
+    //             op::move_(0x14, RegId::HP),
+    //         ]);
+    //         run_group_ref(&mut c.benchmark_group("croo"), "croo", input);
+    //     }
+
+    //     let mut csiz = c.benchmark_group("csiz");
+    //
+    //     for i in linear.clone() {
+    //         let mut code = vec![0u8; i as usize];
+    //
+    //         rng.fill_bytes(&mut code);
+    //
+    //         let code = ContractCode::from(code);
+    //         let id = code.id;
+    //
+    //         let data = id.iter().copied().collect();
+    //
+    //         let prepare_script = vec![op::gtf_args(0x10, 0x00, GTFArgs::ScriptData)];
+    //
+    //         csiz.throughput(Throughput::Bytes(i));
+    //
+    //         run_group_ref(
+    //             &mut csiz,
+    //             format!("{i}"),
+    //             VmBench::new(op::csiz(0x11, 0x10))
+    //                 .with_contract_code(code)
+    //                 .with_data(data)
+    //                 .with_prepare_script(prepare_script),
+    //         );
+    //     }
+    //
+    //     csiz.finish();
+
+    //     let mut ldc = c.benchmark_group("ldc");
+    //
+    //     for i in linear.clone() {
+    //         let mut code = vec![0u8; i as usize];
+    //
+    //         rng.fill_bytes(&mut code);
+    //
+    //         let code = ContractCode::from(code);
+    //         let id = code.id;
+    //
+    //         let data = id
+    //             .iter()
+    //             .copied()
+    //             .chain((0 as Word).to_be_bytes().iter().copied())
+    //             .chain((0 as Word).to_be_bytes().iter().copied())
+    //             .chain(AssetId::default().iter().copied())
+    //             .collect();
+    //
+    //         let prepare_script = vec![
+    //             op::gtf_args(0x10, 0x00, GTFArgs::ScriptData),
+    //             op::addi(0x11, 0x10, ContractId::LEN.try_into().unwrap()),
+    //             op::addi(0x11, 0x11, WORD_SIZE.try_into().unwrap()),
+    //             op::addi(0x11, 0x11, WORD_SIZE.try_into().unwrap()),
+    //             op::movi(0x12, 100_000),
+    //             op::movi(0x13, i.try_into().unwrap()),
+    //         ];
+    //
+    //         ldc.throughput(Throughput::Bytes(i));
+    //
+    //         run_group_ref(
+    //             &mut ldc,
+    //             format!("{i}"),
+    //             VmBench::new(op::ldc(0x10, RegId::ZERO, 0x13))
+    //                 .with_contract_code(code)
+    //                 .with_data(data)
+    //                 .with_prepare_script(prepare_script),
+    //         );
+    //     }
+    //
+    //     ldc.finish();
+
+    // log
+
+    // logd
+
+    //     run_group_ref(
+    //         &mut c.benchmark_group("mint"),
+    //         "mint",
+    //         VmBench::contract_using_db(
+    //             rng,
+    //             db.checkpoint(),
+    //             op::mint(RegId::ONE, RegId::ZERO),
+    //         )
+    //         .expect("failed to prepare contract"),
+    //     );
+
+    // retd
+
+    // rvrt
+
+    //     let mut smo = c.benchmark_group("smo");
+    //
+    //     for i in linear.clone() {
+    //         let mut input = VmBench::contract_using_db(
+    //             rng,
+    //             db.checkpoint(),
+    //             op::smo(0x15, 0x16, 0x17, 0x18),
+    //         )
+    //         .expect("failed to prepare contract");
+    //         input.post_call.extend(vec![
+    //             op::gtf_args(0x15, 0x00, GTFArgs::ScriptData),
+    //             // Offset 32 + 8 + 8 + 32
+    //             op::addi(0x15, 0x15, 32 + 8 + 8 + 32), // target address pointer
+    //             op::addi(0x16, 0x15, 32),              // data ppinter
+    //             op::movi(0x17, i.try_into().unwrap()), // data length
+    //             op::movi(0x18, 10),                    // coins to send
+    //         ]);
+    //         input.data.extend(
+    //             Address::new([1u8; 32])
+    //                 .iter()
+    //                 .copied()
+    //                 .chain(vec![2u8; i as usize]),
+    //         );
+    //         let predicate = op::ret(RegId::ONE).to_bytes().to_vec();
+    //         let owner = Input::predicate_owner(&predicate);
+    //         let coin_input = Input::coin_predicate(
+    //             Default::default(),
+    //             owner,
+    //             Word::MAX,
+    //             AssetId::zeroed(),
+    //             Default::default(),
+    //             Default::default(),
+    //             Default::default(),
+    //             predicate,
+    //             vec![],
+    //         );
+    //         input.inputs.push(coin_input);
+    //         smo.throughput(Throughput::Bytes(i));
+    //         run_group_ref(&mut smo, format!("{i}"), input);
+    //     }
+    //
+    //     smo.finish();
+
+    //     let mut scwq = c.benchmark_group("scwq");
+    //
+    //     for i in linear.clone() {
+    //         let start_key = Bytes32::zeroed();
+    //         let data = start_key.iter().copied().collect::<Vec<_>>();
+    //
+    //         let post_call = vec![
+    //             op::gtf_args(0x10, 0x00, GTFArgs::ScriptData),
+    //             op::addi(0x11, 0x10, ContractId::LEN.try_into().unwrap()),
+    //             op::addi(0x11, 0x11, WORD_SIZE.try_into().unwrap()),
+    //             op::addi(0x11, 0x11, WORD_SIZE.try_into().unwrap()),
+    //             op::movi(0x12, i as u32),
+    //         ];
+    //         let mut bench =
+    //             VmBench::contract_using_db(rng, db.checkpoint(), op::scwq(0x11, 0x29, 0x12))
+    //                 .expect("failed to prepare contract")
+    //                 .with_post_call(post_call);
+    //         bench.data.extend(data);
+    //
+    //         scwq.throughput(Throughput::Bytes(i));
+    //
+    //         run_group_ref(&mut scwq, format!("{i}"), bench);
+    //     }
+    //
+    //     scwq.finish();
+
+    //     let mut srwq = c.benchmark_group("srwq");
+    //
+    //     for i in linear.clone() {
+    //         let start_key = Bytes32::zeroed();
+    //         let data = start_key.iter().copied().collect::<Vec<_>>();
+    //
+    //         let post_call = vec![
+    //             op::movi(0x16, i as u32),
+    //             op::movi(0x17, 2000),
+    //             op::move_(0x15, 0x16),
+    //             op::muli(0x15, 0x15, 32),
+    //             op::addi(0x15, 0x15, 1),
+    //             op::aloc(0x15),
+    //             op::move_(0x14, RegId::HP),
+    //         ];
+    //         let mut bench = VmBench::contract(rng, op::srwq(0x14, 0x11, 0x27, 0x16))
+    //             .expect("failed to prepare contract")
+    //             .with_post_call(post_call)
+    //             .with_prepare_db(move |mut db| {
+    //                 let slots = (0u64..i).map(|key_number| {
+    //                     let mut key = Bytes32::zeroed();
+    //                     key.as_mut()[..8].copy_from_slice(&key_number.to_be_bytes());
+    //                     (key, key)
+    //                 });
+    //                 db.database_mut()
+    //                     .init_contract_state(&contract, slots)
+    //                     .unwrap();
+    //
+    //                 Ok(db)
+    //             });
+    //         bench.data.extend(data);
+    //         srwq.throughput(Throughput::Bytes(i));
+    //         run_group_ref(&mut srwq, format!("{i}"), bench);
+    //     }
+    //
+    //     srwq.finish();
+
+    //     run_group_ref(
+    //         &mut c.benchmark_group("sww"),
+    //         "sww",
+    //         VmBench::contract_using_db(
+    //             rng,
+    //             db.checkpoint(),
+    //             op::sww(RegId::ZERO, 0x29, RegId::ONE),
+    //         )
+    //         .expect("failed to prepare contract"),
+    //     );
+
+    //      let mut swwq = c.benchmark_group("swwq");
+    //
+    //     for i in linear.clone() {
+    //         let start_key = Bytes32::zeroed();
+    //         let data = start_key.iter().copied().collect::<Vec<_>>();
+    //
+    //         let post_call = vec![
+    //             op::gtf_args(0x10, 0x00, GTFArgs::ScriptData),
+    //             op::addi(0x11, 0x10, ContractId::LEN.try_into().unwrap()),
+    //             op::addi(0x11, 0x11, WORD_SIZE.try_into().unwrap()),
+    //             op::addi(0x11, 0x11, WORD_SIZE.try_into().unwrap()),
+    //             op::movi(0x12, i as u32),
+    //         ];
+    //         let mut bench = VmBench::contract_using_db(
+    //             rng,
+    //             db.checkpoint(),
+    //             op::swwq(0x10, 0x11, 0x20, 0x12),
+    //         )
+    //         .expect("failed to prepare contract")
+    //         .with_post_call(post_call);
+    //         bench.data.extend(data);
+    //
+    //         swwq.throughput(Throughput::Bytes(i));
+    //
+    //         run_group_ref(&mut swwq, format!("{i}"), bench);
+    //     }
+    //
+    //     swwq.finish();
+
+    //    run_group_ref(
+    //         &mut c.benchmark_group("time"),
+    //         "time",
+    //         VmBench::new(op::time(0x11, 0x10)).with_prepare_script(vec![op::movi(0x10, 0)]),
+    //     );
+
+    run(
+        "contract/time",
+        group,
+        vec![op::movi(0x10, 0),
+            op::time(0x11, 0x10),
+            op::jmpb(RegId::ZERO, 0)],
+        vec![],
+    )
+
+    //     {
+    //         let mut input =
+    //             VmBench::contract_using_db(rng, db.checkpoint(), op::tr(0x15, 0x14, 0x15))
+    //                 .expect("failed to prepare contract");
+    //         input
+    //             .prepare_script
+    //             .extend(vec![op::movi(0x15, 2000), op::movi(0x14, 100)]);
+    //         run_group_ref(&mut c.benchmark_group("tr"), "tr", input);
+    //     }
+
+    //    {
+    //         let mut input = VmBench::contract_using_db(
+    //             rng,
+    //             db.checkpoint(),
+    //             op::tro(RegId::ZERO, 0x15, 0x14, RegId::HP),
+    //         )
+    //         .expect("failed to prepare contract");
+    //         let coin_output = Output::variable(Address::zeroed(), 100, AssetId::zeroed());
+    //         input.outputs.push(coin_output);
+    //         let predicate = op::ret(RegId::ONE).to_bytes().to_vec();
+    //         let owner = Input::predicate_owner(&predicate);
+    //         let coin_input = Input::coin_predicate(
+    //             Default::default(),
+    //             owner,
+    //             1000,
+    //             AssetId::zeroed(),
+    //             Default::default(),
+    //             Default::default(),
+    //             Default::default(),
+    //             predicate,
+    //             vec![],
+    //         );
+    //         input.inputs.push(coin_input);
+    //
+    //         let index = input.outputs.len() - 1;
+    //         input.prepare_script.extend(vec![
+    //             op::movi(0x14, 100),
+    //             op::movi(0x15, index.try_into().unwrap()),
+    //             op::movi(0x20, 32),
+    //             op::aloc(0x20),
+    //         ]);
+    //         for (i, v) in (*AssetId::zeroed()).into_iter().enumerate() {
+    //             input.prepare_script.push(op::movi(0x20, v as u32));
+    //             input.prepare_script.push(op::sb(RegId::HP, 0x20, i as u16));
+    //         }
+    //
+    //         run_group_ref(&mut c.benchmark_group("tro"), "tro", input);
+    //     }
 }
 
 fn replace_contract_in_service(
