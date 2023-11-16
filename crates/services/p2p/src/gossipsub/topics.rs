c75314d70d6d4a011834260f84d8d96dca2034fc
Brandon Vrooman <brandon.vrooman@fuel.sh>
2023-11-16 15:37:48 -0500

    chore: Remove unused gossipsub items (#1491)
    Related issues:
    - Closes https://github.com/FuelLabs/fuel-core/issues/1376
    
    This PR removes unused gossipsub request and repsonse items `NewBlock`
    and `ConsensusVote`. This includes implementation details, such as
    structs and constants, as well as tests.

--- crates/services/p2p/src/gossipsub/topics.rs
+++ crates/services/p2p/src/gossipsub/topics.rs
@@ -11,8 +11,6 @@ use super::messages::{
 
 pub type GossipTopic = Sha256Topic;
 pub const NEW_TX_GOSSIP_TOPIC: &str = "new_tx";
-pub const NEW_BLOCK_GOSSIP_TOPIC: &str = "new_block";
-pub const CON_VOTE_GOSSIP_TOPIC: &str = "consensus_vote";
 
 /// Holds used Gossipsub Topics
 /// Each field contains TopicHash and GossipTopic itself
@@ -20,22 +18,14 @@ pub const CON_VOTE_GOSSIP_TOPIC: &str = "consensus_vote";
 #[derive(Debug)]
 pub struct GossipsubTopics {
     new_tx_topic: (TopicHash, GossipTopic),
-    new_block_topic: (TopicHash, GossipTopic),
-    consensus_vote_topic: (TopicHash, GossipTopic),
 }
 
 impl GossipsubTopics {
     pub fn new(network_name: &str) -> Self {
         let new_tx_topic = Topic::new(format!("{NEW_TX_GOSSIP_TOPIC}/{network_name}"));
-        let new_block_topic =
-            Topic::new(format!("{NEW_BLOCK_GOSSIP_TOPIC}/{network_name}"));
-        let consensus_vote_topic =
-            Topic::new(format!("{CON_VOTE_GOSSIP_TOPIC}/{network_name}"));
 
         Self {
             new_tx_topic: (new_tx_topic.hash(), new_tx_topic),
-            new_block_topic: (new_block_topic.hash(), new_block_topic),
-            consensus_vote_topic: (consensus_vote_topic.hash(), consensus_vote_topic),
         }
     }
 
@@ -44,18 +34,10 @@ impl GossipsubTopics {
         &self,
         incoming_topic: &TopicHash,
     ) -> Option<GossipTopicTag> {
-        let GossipsubTopics {
-            new_tx_topic,
-            new_block_topic,
-            consensus_vote_topic,
-        } = &self;
+        let GossipsubTopics { new_tx_topic } = &self;
 
         match incoming_topic {
             hash if hash == &new_tx_topic.0 => Some(GossipTopicTag::NewTx),
-            hash if hash == &new_block_topic.0 => Some(GossipTopicTag::NewBlock),
-            hash if hash == &consensus_vote_topic.0 => {
-                Some(GossipTopicTag::ConsensusVote)
-            }
             _ => None,
         }
     }
@@ -67,10 +49,6 @@ impl GossipsubTopics {
         outgoing_request: &GossipsubBroadcastRequest,
     ) -> GossipTopic {
         match outgoing_request {
-            GossipsubBroadcastRequest::ConsensusVote(_) => {
-                self.consensus_vote_topic.1.clone()
-            }
-            GossipsubBroadcastRequest::NewBlock(_) => self.new_block_topic.1.clone(),
             GossipsubBroadcastRequest::NewTx(_) => self.new_tx_topic.1.clone(),
         }
     }
@@ -79,13 +57,7 @@ impl GossipsubTopics {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use fuel_core_types::{
-        blockchain::{
-            block::Block,
-            consensus::ConsensusVote,
-        },
-        fuel_tx::Transaction,
-    };
+    use fuel_core_types::fuel_tx::Transaction;
     use libp2p::gossipsub::Topic;
     use std::sync::Arc;
 
@@ -94,50 +66,19 @@ mod tests {
         let network_name = "fuel_test_network";
         let new_tx_topic: GossipTopic =
             Topic::new(format!("{NEW_TX_GOSSIP_TOPIC}/{network_name}"));
-        let new_block_topic: GossipTopic =
-            Topic::new(format!("{NEW_BLOCK_GOSSIP_TOPIC}/{network_name}"));
-        let consensus_vote_topic: GossipTopic =
-            Topic::new(format!("{CON_VOTE_GOSSIP_TOPIC}/{network_name}"));
 
         let gossipsub_topics = GossipsubTopics::new(network_name);
 
         // Test matching Topic Hashes
         assert_eq!(gossipsub_topics.new_tx_topic.0, new_tx_topic.hash());
-        assert_eq!(gossipsub_topics.new_block_topic.0, new_block_topic.hash());
-        assert_eq!(
-            gossipsub_topics.consensus_vote_topic.0,
-            consensus_vote_topic.hash()
-        );
 
         // Test given a TopicHash that `get_gossipsub_tag()` returns matching `GossipTopicTag`
         assert_eq!(
             gossipsub_topics.get_gossipsub_tag(&new_tx_topic.hash()),
             Some(GossipTopicTag::NewTx)
         );
-        assert_eq!(
-            gossipsub_topics.get_gossipsub_tag(&new_block_topic.hash()),
-            Some(GossipTopicTag::NewBlock)
-        );
-        assert_eq!(
-            gossipsub_topics.get_gossipsub_tag(&consensus_vote_topic.hash()),
-            Some(GossipTopicTag::ConsensusVote)
-        );
 
         // Test given a `GossipsubBroadcastRequest` that `get_gossipsub_topic()` returns matching `Topic`
-        let broadcast_req =
-            GossipsubBroadcastRequest::ConsensusVote(Arc::new(ConsensusVote::default()));
-        assert_eq!(
-            gossipsub_topics.get_gossipsub_topic(&broadcast_req).hash(),
-            consensus_vote_topic.hash()
-        );
-
-        let broadcast_req =
-            GossipsubBroadcastRequest::NewBlock(Arc::new(Block::default()));
-        assert_eq!(
-            gossipsub_topics.get_gossipsub_topic(&broadcast_req).hash(),
-            new_block_topic.hash()
-        );
-
         let broadcast_req =
             GossipsubBroadcastRequest::NewTx(Arc::new(Transaction::default_test_tx()));
         assert_eq!(
