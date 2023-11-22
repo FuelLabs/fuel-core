65dd6f2413391a18f50cffd8b6c0c7621f0dbb5c
Brandon Kite <brandonkite92@gmail.com>
2023-11-22 15:13:24 -0800

    Tool for generating fee collection contract (#1501)
    Closes #1409 
    
    Handing off to @Dentosal 
    
    The tests contain example of how to withdraw tokens.
    
    ---------
    
    Co-authored-by: Hannes Karppila <hannes.karppila@gmail.com>
    Co-authored-by: xgreenx <xgreenx9999@gmail.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -51,6 +51,7 @@ Description of the upcoming release here.
 - [#1263](https://github.com/FuelLabs/fuel-core/pull/1263): Add gas benchmarks for `ED19` and `ECR1` instructions.
 - [#1331](https://github.com/FuelLabs/fuel-core/pull/1331): Add peer reputation reporting to block import code
 - [#1405](https://github.com/FuelLabs/fuel-core/pull/1405): Use correct names for service metrics.
+- [#1501](https://github.com/FuelLabs/fuel-core/pull/1501): Add a CLI command for generating a fee collection contract.
 
 ### Changed
 
---
+++ bin/fuel-core/src/cli/fee_contract.rs
@@ -0,0 +1,40 @@
+use clap::Parser;
+use fuel_core_chain_config::fee_collection_contract;
+use fuel_core_types::fuel_tx::Address;
+use std::{
+    fs::OpenOptions,
+    io::Write,
+    path::PathBuf,
+};
+
+#[derive(Debug, Parser)]
+pub struct Command {
+    /// Address to withdraw fees to
+    withdrawal_address: Address,
+    /// Output file. If not provided, will print hex representation to stdout.
+    #[clap(short, long)]
+    output: Option<PathBuf>,
+    /// Overwrite output file if it exists. No effect if `output` is not provided.
+    #[clap(short, long)]
+    force: bool,
+}
+
+pub async fn exec(cmd: Command) -> anyhow::Result<()> {
+    let contract = fee_collection_contract::generate(cmd.withdrawal_address);
+
+    if let Some(output) = cmd.output.as_ref() {
+        let mut open_opt = OpenOptions::new();
+        if cmd.force {
+            open_opt.create(true).write(true).truncate(true);
+        } else {
+            open_opt.create_new(true).write(true);
+        }
+
+        let mut file = open_opt.open(output)?;
+        file.write_all(&contract)?;
+    } else {
+        println!("{}", hex::encode(contract));
+    }
+
+    Ok(())
+}
