e4a5cf5e4a976ccb3954dd3fa41b2106f3be3fd1
Hannes Karppila <hannes.karppila@gmail.com>
2023-11-22 19:32:30 +0200

    Add a test case with missing variable output

--- crates/chain-config/src/fee_collection_contract.rs
+++ crates/chain-config/src/fee_collection_contract.rs
@@ -41,15 +41,6 @@ pub fn generate(address: Address) -> Vec<u8> {
 
 #[cfg(test)]
 mod tests {
-    // TODO: Tests to write for this contract:
-    // 1. Happy path withdrawal case for block producer
-    //    Deploy the contract for the block producer's address
-    //    Run some blocks and accumulate fees
-    //    Attempt to withdraw the collected fees to the BP's address
-    //      (note that currently we allow anyone to initiate this withdrawal)
-    // 2. Unhappy case where tx doesn't have the expected variable output set
-    // 3. Edge case, withdrawal is attempted when there are no fees collected (shouldn't revert, but the BP's balance should be the same)
-
     use super::*;
     use crate::SecretKey;
 
@@ -140,10 +131,46 @@ mod tests {
         }
     }
 
+    /// This makes a block with a single transaction that has a fee,
+    /// so that the coinbase fee is collected into the contract
+    async fn make_block_with_fee(rng: &mut StdRng, ctx: &TestContext) {
+        let old_balance = ctx
+            .client
+            .contract_balance(&ctx.contract_id, None)
+            .await
+            .unwrap();
+
+        // Run a script that does nothing, but will cause fee collection
+        let tx = TransactionBuilder::script(
+            [op::ret(RegId::ONE)].into_iter().collect(),
+            vec![],
+        )
+        .add_unsigned_coin_input(
+            SecretKey::random(rng),
+            rng.gen(),
+            1000,
+            Default::default(),
+            Default::default(),
+            Default::default(),
+        )
+        .gas_price(1)
+        .script_gas_limit(1_000_000)
+        .finalize_as_transaction();
+        let tx_status = ctx.client.submit_and_await_commit(&tx).await.unwrap();
+        assert!(matches!(tx_status, TransactionStatus::Success { .. }));
+
+        // Now the coinbase fee should be reflected in the contract balance
+        let new_balance = ctx
+            .client
+            .contract_balance(&ctx.contract_id, None)
+            .await
+            .unwrap();
+        assert_eq!(new_balance, old_balance.checked_add(1).unwrap());
+    }
+
     async fn collect_fees(ctx: &TestContext) {
         let TestContext {
             client,
-            address,
             contract_id,
             ..
         } = ctx;
@@ -188,14 +215,6 @@ mod tests {
 
         let tx_status = client.submit_and_await_commit(&tx).await.unwrap();
         assert!(matches!(tx_status, TransactionStatus::Success { .. }));
-
-        // Make sure that the full balance was been withdrawn
-        let contract_balance = client.contract_balance(contract_id, None).await.unwrap();
-        assert_eq!(contract_balance, 0);
-
-        // Make sure that the full balance was been withdrawn
-        let asset_balance = client.balance(address, None).await.unwrap();
-        assert_eq!(asset_balance, 10);
     }
 
     #[tokio::test]
@@ -204,33 +223,8 @@ mod tests {
 
         let ctx = setup(rng).await;
 
-        for i in 0..10 {
-            // Run a script that does nothing, but will cause fee collection
-            let tx = TransactionBuilder::script(
-                [op::ret(RegId::ONE)].into_iter().collect(),
-                vec![],
-            )
-            .add_unsigned_coin_input(
-                SecretKey::random(rng),
-                rng.gen(),
-                1000,
-                Default::default(),
-                Default::default(),
-                Default::default(),
-            )
-            .gas_price(1)
-            .script_gas_limit(1_000_000)
-            .finalize_as_transaction();
-            let tx_status = ctx.client.submit_and_await_commit(&tx).await.unwrap();
-            assert!(matches!(tx_status, TransactionStatus::Success { .. }));
-
-            // Now the coinbase fee should be reflected in the contract balance
-            let contract_balance = ctx
-                .client
-                .contract_balance(&ctx.contract_id, None)
-                .await
-                .unwrap();
-            assert_eq!(contract_balance, i + 1);
+        for _ in 0..10 {
+            make_block_with_fee(rng, &ctx).await;
         }
 
         collect_fees(&ctx).await;
@@ -248,45 +242,36 @@ mod tests {
         assert_eq!(asset_balance, 10);
     }
 
+    /// Attempts fee collection when no balance has accumulated yet
     #[tokio::test]
     async fn no_fees_collected_yet() {
         let rng = &mut StdRng::seed_from_u64(0);
 
-        // Make contract that coinbase fees are collected into
-        let address: Address = rng.gen();
-        let salt: Salt = rng.gen();
-        let contract = generate(address);
-        let witness: Witness = contract.into();
-        let mut create_tx = TransactionBuilder::create(witness.clone(), salt, vec![])
-            .add_random_fee_input()
-            .finalize();
-        create_tx
-            .precompute(&ChainId::default())
-            .expect("tx should be valid");
-        let contract_id = create_tx.metadata().as_ref().unwrap().contract_id;
+        let ctx = setup(rng).await;
+        collect_fees(&ctx).await;
 
-        // Start up a node
-        let mut config = Config::local_node();
-        config.debug = true;
-        config.block_producer.coinbase_recipient = Some(contract_id);
-        let node = FuelService::new_node(config).await.unwrap();
-        let client = FuelClient::from(node.bound_address);
-
-        // Submit contract creation tx
-        let tx_status = client
-            .submit_and_await_commit(&create_tx.into())
+        // Make sure that the balance is still zero
+        let contract_balance = ctx
+            .client
+            .contract_balance(&ctx.contract_id, None)
             .await
             .unwrap();
-        assert!(matches!(tx_status, TransactionStatus::Success { .. }));
-        let bh = client.produce_blocks(1, None).await.unwrap();
-        assert_eq!(bh, BlockHeight::new(2));
-
-        // No fees should have been collected yet
-        let contract_balance =
-            client.contract_balance(&(contract_id), None).await.unwrap();
         assert_eq!(contract_balance, 0);
 
-        // Now call the fee collection contract to withdraw the fees
+        // There were no coins to withdraw
+        let asset_balance = ctx.client.balance(&ctx.address, None).await.unwrap();
+        assert_eq!(asset_balance, 0);
+    }
+
+    #[tokio::test]
+    async fn missing_variable_output() {
+        let rng = &mut StdRng::seed_from_u64(0);
+
+        let ctx = setup(rng).await;
+        make_block_with_fee(rng, &ctx).await;
+
+        // Now call the fee collection contract to withdraw the fees,
+        // but unlike in the happy path, we don't add the variable output
         let (script, _) = script_with_data_offset!(
             data_offset,
             vec![
@@ -300,9 +285,9 @@ mod tests {
         );
         let tx = TransactionBuilder::script(
             script.into_iter().collect(),
-            (*contract_id)
+            (*ctx.contract_id)
                 .into_iter()
-                .chain((VM_MAX_RAM - (AssetId::LEN as u64)).to_be_bytes())
+                .chain((VM_MAX_RAM.checked_sub(AssetId::LEN as u64)).unwrap().to_be_bytes())
                 .chain(0u64.to_be_bytes())
                 .collect(),
         )
@@ -314,26 +299,25 @@ mod tests {
             Default::default(),
             Default::default(),
             Default::default(),
-            contract_id,
+            ctx.contract_id,
         ))
         .add_output(Output::contract(1, Default::default(), Default::default()))
-        .add_output(Output::variable(
-            Default::default(),
-            Default::default(),
-            Default::default(),
-        ))
         .finalize_as_transaction();
 
-        let tx_status = client.submit_and_await_commit(&tx).await.unwrap();
-        assert!(matches!(tx_status, TransactionStatus::Success { .. }));
+        let tx_status = ctx.client.submit_and_await_commit(&tx).await.unwrap();
+        let TransactionStatus::Failure { reason, .. } = tx_status else {
+            panic!("Expected failure");
+        };
+        assert_eq!(reason, "OutputNotFound");
 
-        // Make sure that the full balance was been withdrawn
-        let contract_balance =
-            client.contract_balance(&(contract_id), None).await.unwrap();
-        assert_eq!(contract_balance, 0);
-
-        // Make sure that the full balance was been withdrawn
-        let asset_balance = client.balance(&address, None).await.unwrap();
+        // Make sure that nothing was withdrawn
+        let contract_balance = ctx
+            .client
+            .contract_balance(&ctx.contract_id, None)
+            .await
+            .unwrap();
+        assert_eq!(contract_balance, 1);
+        let asset_balance = ctx.client.balance(&ctx.address, None).await.unwrap();
         assert_eq!(asset_balance, 0);
     }
 }
