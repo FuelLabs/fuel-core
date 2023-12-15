d11271289e2dad2e56f64f2ababfd12803db5ab6
Hannes Karppila <hannes.karppila@gmail.com>
2023-12-15 12:28:40 +0200

    Remove a debug print

--- firehose-fuel/codec/console_reader.go
+++ firehose-fuel/codec/console_reader.go
@@ -94,7 +94,6 @@ func (r *ConsoleReader) next() (out *pbfuel.Block, err error) {
 			if err := proto.Unmarshal(bytes, block); err != nil {
 				return nil, fmt.Errorf("Failed to parse block: %s", err)
 			}
-			fmt.Printf("block %v\n", block)
 			return block, nil
 		default:
 			if r.logger.Core().Enabled(zap.DebugLevel) {
