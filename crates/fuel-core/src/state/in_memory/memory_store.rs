7e02c25116a5ce7ef5d494229d6236fc270c56c6
Green Baneling <XgreenX9999@gmail.com>
2023-12-19 15:15:32 +0100

    Remove duplicating logic in the `KeyValueStore` trait (#1559)
    Preparation before start work on
    https://github.com/FuelLabs/fuel-core/issues/1548.
    
    The `KeyValueStore` trait has some duplicated logic. This PR removes it,
    minimizing the number of methods that we need to implement. Also I
    applied the original ordering of the method as in the trait.

--- crates/fuel-core/src/state/in_memory/memory_store.rs
+++ crates/fuel-core/src/state/in_memory/memory_store.rs
@@ -1,7 +1,6 @@
 use crate::{
     database::{
         Column,
-        Error as DatabaseError,
         Result as DatabaseResult,
     },
     state::{
@@ -100,15 +99,9 @@ impl MemoryStore {
 }
 
 impl KeyValueStore for MemoryStore {
-    fn get(&self, key: &[u8], column: Column) -> DatabaseResult<Option<Value>> {
-        Ok(self.inner[column.as_usize()]
-            .lock()
-            .expect("poisoned")
-            .get(&key.to_vec())
-            .cloned())
-    }
+    type Column = Column;
 
-    fn put(
+    fn replace(
         &self,
         key: &[u8],
         column: Column,
@@ -120,18 +113,32 @@ impl KeyValueStore for MemoryStore {
             .insert(key.to_vec(), value))
     }
 
-    fn delete(&self, key: &[u8], column: Column) -> DatabaseResult<Option<Value>> {
+    fn write(&self, key: &[u8], column: Column, buf: &[u8]) -> DatabaseResult<usize> {
+        let len = buf.len();
+        self.inner[column.as_usize()]
+            .lock()
+            .expect("poisoned")
+            .insert(key.to_vec(), Arc::new(buf.to_vec()));
+        Ok(len)
+    }
+
+    fn take(&self, key: &[u8], column: Column) -> DatabaseResult<Option<Value>> {
         Ok(self.inner[column.as_usize()]
             .lock()
             .expect("poisoned")
             .remove(&key.to_vec()))
     }
 
-    fn exists(&self, key: &[u8], column: Column) -> DatabaseResult<bool> {
+    fn delete(&self, key: &[u8], column: Column) -> DatabaseResult<()> {
+        self.take(key, column).map(|_| ())
+    }
+
+    fn get(&self, key: &[u8], column: Column) -> DatabaseResult<Option<Value>> {
         Ok(self.inner[column.as_usize()]
             .lock()
             .expect("poisoned")
-            .contains_key(&key.to_vec()))
+            .get(&key.to_vec())
+            .cloned())
     }
 
     fn iter_all(
@@ -142,71 +149,6 @@ impl KeyValueStore for MemoryStore {
         direction: IterDirection,
     ) -> BoxedIter<KVItem> {
         self.iter_all(column, prefix, start, direction).into_boxed()
-    }
-
-    fn size_of_value(&self, key: &[u8], column: Column) -> DatabaseResult<Option<usize>> {
-        Ok(self.inner[column.as_usize()]
-            .lock()
-            .expect("poisoned")
-            .get(&key.to_vec())
-            .map(|v| v.len()))
-    }
-
-    fn read(
-        &self,
-        key: &[u8],
-        column: Column,
-        mut buf: &mut [u8],
-    ) -> DatabaseResult<Option<usize>> {
-        self.inner[column.as_usize()]
-            .lock()
-            .expect("poisoned")
-            .get(&key.to_vec())
-            .map(|value| {
-                let read = value.len();
-                std::io::Write::write_all(&mut buf, value.as_ref())
-                    .map_err(|e| DatabaseError::Other(anyhow::anyhow!(e)))?;
-                DatabaseResult::Ok(read)
-            })
-            .transpose()
-    }
-
-    fn read_alloc(&self, key: &[u8], column: Column) -> DatabaseResult<Option<Value>> {
-        Ok(self.inner[column.as_usize()]
-            .lock()
-            .expect("poisoned")
-            .get(&key.to_vec())
-            .cloned())
-    }
-
-    fn write(&self, key: &[u8], column: Column, buf: &[u8]) -> DatabaseResult<usize> {
-        let len = buf.len();
-        self.inner[column.as_usize()]
-            .lock()
-            .expect("poisoned")
-            .insert(key.to_vec(), Arc::new(buf.to_vec()));
-        Ok(len)
-    }
-
-    fn replace(
-        &self,
-        key: &[u8],
-        column: Column,
-        buf: &[u8],
-    ) -> DatabaseResult<(usize, Option<Value>)> {
-        let len = buf.len();
-        let existing = self.inner[column.as_usize()]
-            .lock()
-            .expect("poisoned")
-            .insert(key.to_vec(), Arc::new(buf.to_vec()));
-        Ok((len, existing))
-    }
-
-    fn take(&self, key: &[u8], column: Column) -> DatabaseResult<Option<Value>> {
-        Ok(self.inner[column.as_usize()]
-            .lock()
-            .expect("poisoned")
-            .remove(&key.to_vec()))
     }
 }
 
@@ -246,10 +188,7 @@ mod tests {
             vec![(key.clone(), expected.clone())]
         );
 
-        assert_eq!(
-            db.delete(&key, Column::Metadata).unwrap().unwrap(),
-            expected
-        );
+        assert_eq!(db.take(&key, Column::Metadata).unwrap().unwrap(), expected);
 
         assert!(!db.exists(&key, Column::Metadata).unwrap());
     }
@@ -273,10 +212,7 @@ mod tests {
             vec![(key.clone(), expected.clone())]
         );
 
-        assert_eq!(
-            db.delete(&key, Column::Metadata).unwrap().unwrap(),
-            expected
-        );
+        assert_eq!(db.take(&key, Column::Metadata).unwrap().unwrap(), expected);
 
         assert!(!db.exists(&key, Column::Metadata).unwrap());
     }
@@ -300,10 +236,7 @@ mod tests {
             vec![(key.clone(), expected.clone())]
         );
 
-        assert_eq!(
-            db.delete(&key, Column::Metadata).unwrap().unwrap(),
-            expected
-        );
+        assert_eq!(db.take(&key, Column::Metadata).unwrap().unwrap(), expected);
 
         assert!(!db.exists(&key, Column::Metadata).unwrap());
     }
