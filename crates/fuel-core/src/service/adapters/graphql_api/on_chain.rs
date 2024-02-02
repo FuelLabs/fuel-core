16b1dc9cf8cb9f7887873d857d08f5b06b874586
xgreenx <xgreenx9999@gmail.com>
2024-02-02 16:29:46 +0100

    Cix compilation

--- crates/fuel-core/src/service/adapters/graphql_api/on_chain.rs
+++ crates/fuel-core/src/service/adapters/graphql_api/on_chain.rs
@@ -30,11 +30,7 @@ use fuel_core_types::{
         },
     },
     entities::message::Message,
-    fuel_tx::{
-        Address,
-        AssetId,
-        UtxoId,
-    },
+    fuel_tx::AssetId,
     fuel_types::{
         BlockHeight,
         Nonce,
@@ -114,26 +110,4 @@ impl DatabaseChain for Database {
     }
 }
 
-impl OnChainDatabase for Database {
-    fn owned_message_ids(
-        &self,
-        owner: &Address,
-        start_message_id: Option<Nonce>,
-        direction: IterDirection,
-    ) -> BoxedIter<'_, StorageResult<Nonce>> {
-        self.owned_message_ids(owner, start_message_id, Some(direction))
-            .map(|result| result.map_err(StorageError::from))
-            .into_boxed()
-    }
-
-    fn owned_coins_ids(
-        &self,
-        owner: &Address,
-        start_coin: Option<UtxoId>,
-        direction: IterDirection,
-    ) -> BoxedIter<'_, StorageResult<UtxoId>> {
-        self.owned_coins_ids(owner, start_coin, Some(direction))
-            .map(|res| res.map_err(StorageError::from))
-            .into_boxed()
-    }
-}
+impl OnChainDatabase for Database {}
