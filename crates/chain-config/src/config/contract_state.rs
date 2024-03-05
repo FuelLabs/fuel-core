caded52cec8fd5e74b8a9a5151bff90f720c2d51
segfault_magnet <ahmed.sagdati.ets@gmail.com>
2024-03-05 11:04:13 +0100

    human readable storage slot value

--- crates/chain-config/src/config/contract_state.rs
+++ crates/chain-config/src/config/contract_state.rs
@@ -1,3 +1,4 @@
+use crate::serialization::HexIfHumanReadable;
 use fuel_core_types::fuel_types::{
     Bytes32,
     ContractId,
@@ -6,11 +7,14 @@ use serde::{
     Deserialize,
     Serialize,
 };
+use serde_with::serde_as;
 
+#[serde_as]
 #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize, PartialOrd, Ord)]
 pub struct ContractStateConfig {
     pub contract_id: ContractId,
     pub key: Bytes32,
+    #[serde_as(as = "HexIfHumanReadable")]
     pub value: Vec<u8>,
 }
 
