a3382ae49422b1fdf77466e8993d7807dc796296
MujkicA <ahmedmujkic2@gmail.com>
2024-03-06 13:43:05 +0100

    remove temp tables

--- crates/fuel-core/src/database/genesis_progress.rs
+++ crates/fuel-core/src/database/genesis_progress.rs
@@ -1,36 +1,28 @@
-use crate::state::DataSource;
-
 use super::Database;
+use fuel_core_chain_config::GenesisCommitment;
+use fuel_core_executor::refs::ContractRef;
 use fuel_core_storage::{
-    blueprint::{
-        plain::Plain,
-        Blueprint,
-    },
-    codec::{
-        postcard::Postcard,
-        raw::Raw,
-    },
+    blueprint::plain::Plain,
+    codec::postcard::Postcard,
     column::Column,
     structured_storage::TableWithBlueprint,
-    tables::ContractsLatestUtxo,
+    tables::{
+        Coins,
+        ContractsLatestUtxo,
+        Messages,
+    },
+    transactional::Transactional,
     Mappable,
     MerkleRoot,
     Result,
     StorageAsMut,
     StorageInspect,
-    StorageMutate,
 };
 use fuel_core_types::{
-    fuel_merkle::sparse::{
-        in_memory::MerkleTree,
-        MerkleTreeKey,
-    },
+    fuel_merkle::binary::root_calculator::MerkleRootCalculator,
     fuel_types::ContractId,
 };
-use itertools::{
-    process_results,
-    Itertools,
-};
+use itertools::process_results;
 use serde::{
     Deserialize,
     Serialize,
@@ -63,60 +55,6 @@ impl TableWithBlueprint for GenesisMetadata {
     }
 }
 
-// TODO: Remove as part of the https://github.com/FuelLabs/fuel-core/issues/1734
-pub struct GenesisCoinRoots;
-
-impl Mappable for GenesisCoinRoots {
-    type Key = Self::OwnedKey;
-    type OwnedKey = MerkleRoot;
-    type Value = Self::OwnedValue;
-    type OwnedValue = ();
-}
-
-impl TableWithBlueprint for GenesisCoinRoots {
-    type Blueprint = Plain<Raw, Postcard>;
-    type Column = Column;
-    fn column() -> Self::Column {
-        Column::GenesisCoinRoots
-    }
-}
-
-// TODO: Remove as part of the https://github.com/FuelLabs/fuel-core/issues/1734
-pub struct GenesisMessageRoots;
-
-impl Mappable for GenesisMessageRoots {
-    type Key = Self::OwnedKey;
-    type OwnedKey = MerkleRoot;
-    type Value = Self::OwnedValue;
-    type OwnedValue = ();
-}
-
-impl TableWithBlueprint for GenesisMessageRoots {
-    type Blueprint = Plain<Raw, Postcard>;
-    type Column = Column;
-    fn column() -> Self::Column {
-        Column::GenesisMessageRoots
-    }
-}
-
-// TODO: Remove as part of the https://github.com/FuelLabs/fuel-core/issues/1734
-pub struct GenesisContractRoots;
-
-impl Mappable for GenesisContractRoots {
-    type Key = Self::OwnedKey;
-    type OwnedKey = MerkleRoot;
-    type Value = Self::OwnedValue;
-    type OwnedValue = ();
-}
-
-impl TableWithBlueprint for GenesisContractRoots {
-    type Blueprint = Plain<Raw, Postcard>;
-    type Column = Column;
-    fn column() -> Self::Column {
-        Column::GenesisContractRoots
-    }
-}
-
 pub type GenesisImportedContractId = ContractId;
 
 impl Database {
@@ -139,66 +77,49 @@ impl Database {
         Ok(())
     }
 
-    pub fn add_coin_root(&mut self, root: MerkleRoot) -> Result<()> {
-        StorageMutate::<GenesisCoinRoots>::insert(self, &root, &())?;
-        Ok(())
-    }
-
-    pub fn add_message_root(&mut self, root: MerkleRoot) -> Result<()> {
-        StorageMutate::<GenesisMessageRoots>::insert(self, &root, &())?;
-        Ok(())
-    }
-
-    pub fn add_contract_root(&mut self, root: MerkleRoot) -> Result<()> {
-        StorageMutate::<GenesisContractRoots>::insert(self, &root, &())?;
-        Ok(())
-    }
-
-    pub(crate) fn genesis_roots<M>(
-        &self,
-    ) -> Result<impl Iterator<Item = (MerkleTreeKey, [u8; 32])>>
-    where
-        M: Mappable<OwnedKey = [u8; 32]> + TableWithBlueprint<Column = Column>,
-        M::Blueprint: Blueprint<M, DataSource>,
-    {
-        let roots_iter = self.iter_all::<M>(None);
+    pub fn genesis_coin_root(&self) -> Result<MerkleRoot> {
+        let roots_iter = self.iter_all::<Coins>(None);
 
         let roots = process_results(roots_iter, |roots| {
-            roots.map(|(root, _)| root).collect::<Vec<MerkleRoot>>()
+            roots
+                .map(|(_, coin)| coin.root().unwrap())
+                .collect::<Vec<MerkleRoot>>()
         })?
-        .into_iter()
-        .enumerate()
-        .map(|(idx, root)| (MerkleTreeKey::new(idx.to_be_bytes()), root));
+        .into_iter();
 
-        Ok(roots)
-    }
-
-    fn compute_genesis_root<M>(&self) -> Result<MerkleRoot>
-    where
-        M: Mappable<OwnedKey = [u8; 32]> + TableWithBlueprint<Column = Column>,
-        M::Blueprint: Blueprint<M, DataSource>,
-    {
-        let roots = self.genesis_roots::<M>()?;
-        Ok(MerkleTree::root_from_set(roots.into_iter()))
-    }
-
-    pub fn genesis_coin_root(&self) -> Result<MerkleRoot> {
-        self.compute_genesis_root::<GenesisCoinRoots>()
+        Ok(MerkleRootCalculator::new().root_from_iterator(roots))
     }
 
     pub fn genesis_messages_root(&self) -> Result<MerkleRoot> {
-        self.compute_genesis_root::<GenesisMessageRoots>()
+        let roots_iter = self.iter_all::<Messages>(None);
+
+        let roots = process_results(roots_iter, |roots| {
+            roots
+                .map(|(_, message)| message.root().unwrap())
+                .collect::<Vec<MerkleRoot>>()
+        })?
+        .into_iter();
+
+        Ok(MerkleRootCalculator::new().root_from_iterator(roots))
     }
 
     pub fn genesis_contracts_root(&self) -> Result<MerkleRoot> {
-        self.compute_genesis_root::<GenesisContractRoots>()
-    }
+        let mut database_transaction = Transactional::transaction(self);
 
-    pub fn genesis_loaded_contracts(
-        &self,
-    ) -> impl Iterator<Item = Result<GenesisImportedContractId>> + '_ {
-        self.iter_all::<ContractsLatestUtxo>(None)
-            .map_ok(|(contract_id, _)| contract_id)
-            .map(|res| res.map_err(Into::into))
+        let database = database_transaction.as_mut();
+        let contracts_iter = self.iter_all::<ContractsLatestUtxo>(None);
+
+        let roots = process_results(contracts_iter, |contracts| {
+            contracts
+                .map(|(contract_id, _)| {
+                    ContractRef::new(&mut *database, contract_id)
+                        .root()
+                        .unwrap()
+                })
+                .collect::<Vec<MerkleRoot>>()
+        })?
+        .into_iter();
+
+        Ok(MerkleRootCalculator::new().root_from_iterator(roots))
     }
 }
