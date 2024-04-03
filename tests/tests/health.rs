5d4d13db27b7bc13abe2f510570e35040ae36739
Brandon Vrooman <brandon.vrooman@gmail.com>
2024-04-03 15:09:33 -0400

    can restart node with data test

--- tests/tests/health.rs
+++ tests/tests/health.rs
@@ -6,6 +6,11 @@ use fuel_core::{
     },
 };
 use fuel_core_client::client::FuelClient;
+// use fuel_core_types::fuel_tx::Transaction;
+
+use fuel_core::combined_database::CombinedDatabase;
+use fuel_core_types::fuel_tx::Transaction;
+use tracing_test::traced_test;
 
 #[tokio::test]
 async fn health() {
@@ -39,5 +44,43 @@ async fn can_restart_node() {
         let _second_startup = FuelService::from_database(database, Config::local_node())
             .await
             .unwrap();
+    }
+}
+
+#[traced_test]
+#[tokio::test]
+async fn can_restart_node_with_data() {
+    use fuel_core::service::ServiceTrait;
+    use tempfile::TempDir;
+
+    let tmp_dir = TempDir::new().unwrap();
+
+    {
+        let database = CombinedDatabase::open(tmp_dir.path(), 10 * 1024 * 1024).unwrap();
+        let service = FuelService::from_combined_database(database, Config::local_node())
+            .await
+            .unwrap();
+        let client = FuelClient::from(service.bound_address);
+        client.health().await.unwrap();
+
+        let tx = Transaction::default_test_tx();
+        client.submit_and_await_commit(&tx).await.unwrap();
+        let tx = Transaction::default_test_tx();
+        client.submit_and_await_commit(&tx).await.unwrap();
+        let tx = Transaction::default_test_tx();
+        client.submit_and_await_commit(&tx).await.unwrap();
+        let tx = Transaction::default_test_tx();
+        client.submit_and_await_commit(&tx).await.unwrap();
+
+        service.stop_and_await().await.unwrap();
+    }
+
+    {
+        let database = CombinedDatabase::open(tmp_dir.path(), 10 * 1024 * 1024).unwrap();
+        let service = FuelService::from_combined_database(database, Config::local_node())
+            .await
+            .unwrap();
+        let client = FuelClient::from(service.bound_address);
+        client.health().await.unwrap();
     }
 }
