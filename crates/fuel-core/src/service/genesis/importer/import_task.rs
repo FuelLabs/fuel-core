5541a6248571ef954357fa2a24fa80a12f05e33b
segfault_magnet <ahmed.sagdati.ets@gmail.com>
2024-04-12 12:46:56 +0200

    wip

--- crates/fuel-core/src/service/genesis/importer/import_task.rs
+++ crates/fuel-core/src/service/genesis/importer/import_task.rs
@@ -1,30 +1,17 @@
-use fuel_core_chain_config::{
-    Groups,
-    TableEntry,
-};
+use fuel_core_chain_config::{Groups, TableEntry};
 use fuel_core_storage::{
     kv_store::StorageColumn,
     structured_storage::TableWithBlueprint,
-    transactional::{
-        StorageTransaction,
-        WriteTransaction,
-    },
-    Mappable,
-    StorageAsRef,
-    StorageInspect,
+    transactional::{StorageTransaction, WriteTransaction},
+    Mappable, StorageAsRef, StorageInspect,
 };
 
 use crate::{
     database::{
         database_description::{
-            off_chain::OffChain,
-            on_chain::OnChain,
-            DatabaseDescription,
+            off_chain::OffChain, on_chain::OnChain, DatabaseDescription,
         },
-        genesis_progress::{
-            GenesisMetadata,
-            GenesisProgressMutate,
-        },
+        genesis_progress::{GenesisMetadata, GenesisProgressMutate},
         Database,
     },
     service::genesis::task_manager::CancellationToken,
@@ -130,82 +117,49 @@ where
 mod tests {
     use crate::{
         database::{
-            database_description::off_chain::OffChain,
+            database_description::{off_chain::OffChain, DatabaseDescription},
             genesis_progress::GenesisProgressInspect,
         },
+        graphql_api::storage::{
+            blocks::FuelBlockIdsToHeights,
+            coins::{OwnedCoinKey, OwnedCoins},
+        },
         service::genesis::{
-            importer::{
-                import_task::import_entries,
-                progress::ProgressReporter,
-            },
+            importer::{import_task::import_entries, progress::ProgressReporter},
             task_manager::CancellationToken,
         },
     };
     use std::{
-        sync::{
-            Arc,
-            Mutex,
-        },
+        marker::PhantomData,
+        sync::{Arc, Mutex},
         time::Duration,
     };
 
-    use anyhow::{
-        anyhow,
-        bail,
-    };
-    use fuel_core_chain_config::{
-        Groups,
-        Randomize,
-        TableEntry,
-    };
+    use anyhow::{anyhow, bail};
+    use fuel_core_chain_config::{Groups, Randomize, TableEntry};
     use fuel_core_storage::{
         column::Column,
-        iter::{
-            BoxedIter,
-            IterDirection,
-            IterableStore,
-        },
-        kv_store::{
-            KVItem,
-            KeyValueInspect,
-            StorageColumn,
-            Value,
-        },
+        iter::{BoxedIter, IterDirection, IterableStore},
+        kv_store::{KVItem, KeyValueInspect, StorageColumn, Value},
         structured_storage::TableWithBlueprint,
         tables::Coins,
-        transactional::{
-            Changes,
-            StorageTransaction,
-        },
-        Result as StorageResult,
-        StorageAsMut,
-        StorageAsRef,
-        StorageInspect,
+        transactional::{Changes, StorageTransaction},
+        Result as StorageResult, StorageAsMut, StorageAsRef, StorageInspect,
     };
     use fuel_core_types::{
-        entities::coins::coin::{
-            CompressedCoin,
-            CompressedCoinV1,
-        },
+        entities::coins::coin::{CompressedCoin, CompressedCoinV1},
         fuel_tx::UtxoId,
         fuel_types::BlockHeight,
     };
-    use rand::{
-        rngs::StdRng,
-        SeedableRng,
-    };
+    use rand::{rngs::StdRng, SeedableRng};
 
     use crate::{
         combined_database::CombinedDatabase,
         database::{
             database_description::on_chain::OnChain,
-            genesis_progress::GenesisProgressMutate,
-            Database,
+            genesis_progress::GenesisProgressMutate, Database,
         },
-        state::{
-            in_memory::memory_store::MemoryStore,
-            TransactableStorage,
-        },
+        state::{in_memory::memory_store::MemoryStore, TransactableStorage},
     };
 
     use super::ImportTable;
@@ -222,6 +176,14 @@ mod tests {
                 on_chain_called_with: Default::default(),
                 off_chain_called_with: Default::default(),
             }
+        }
+
+        pub fn on_chain_called_with(&self) -> Vec<Vec<TableEntry<Coins>>> {
+            self.on_chain_called_with.lock().unwrap().clone()
+        }
+
+        pub fn off_chain_called_with(&self) -> Vec<Vec<TableEntry<Coins>>> {
+            self.off_chain_called_with.lock().unwrap().clone()
         }
 
         fn default_importer(
@@ -247,7 +209,17 @@ mod tests {
             &self,
             on_chain: OnChainCallback,
             off_chain: OffChainCallback,
-        ) -> TestTableImporter<OnChainCallback, OffChainCallback> {
+        ) -> TestTableImporter<OnChainCallback, OffChainCallback>
+        where
+            OnChainCallback: FnMut(
+                Vec<TableEntry<Coins>>,
+                &mut StorageTransaction<&mut Database<OnChain>>,
+            ) -> anyhow::Result<()>,
+            OffChainCallback: FnMut(
+                Vec<TableEntry<Coins>>,
+                &mut StorageTransaction<&mut Database<OffChain>>,
+            ) -> anyhow::Result<()>,
+        {
             TestTableImporter {
                 on_chain,
                 off_chain,
@@ -283,7 +255,7 @@ mod tests {
                 .on_chain_called_with
                 .lock()
                 .unwrap()
-                .push(group);
+                .push(group.clone());
             (self.on_chain)(group, tx)?;
             Ok(())
         }
@@ -296,7 +268,7 @@ mod tests {
                 .off_chain_called_with
                 .lock()
                 .unwrap()
-                .push(group);
+                .push(group.clone());
             (self.off_chain)(group, tx)?;
             Ok(())
         }
@@ -347,18 +319,12 @@ mod tests {
         // given
         let data = TestData::new(3);
 
-        let mut called_with = vec![];
+        let spy = Spy::default();
 
         // when
         import_entries(
             CancellationToken::default(),
-            Spy::custom(
-                |group, _| {
-                    called_with.push(group);
-                    Ok(())
-                },
-                |_, _| Ok(()),
-            ),
+            spy.default_importer(),
             data.as_ok_groups(0),
             Database::default(),
             Database::default(),
@@ -367,7 +333,8 @@ mod tests {
         .unwrap();
 
         // then
-        assert_eq!(called_with, data.as_unwrapped_groups(0));
+        assert_eq!(spy.on_chain_called_with(), data.as_unwrapped_groups(0));
+        assert_eq!(spy.off_chain_called_with(), data.as_unwrapped_groups(0));
     }
 
     #[test]
@@ -375,7 +342,6 @@ mod tests {
         // given
         let data = TestData::new(2);
 
-        let mut called_with = vec![];
         let mut db = CombinedDatabase::default();
         let spy = Spy::new();
         GenesisProgressMutate::<OnChain>::update_genesis_progress(
@@ -397,7 +363,8 @@ mod tests {
         .unwrap();
 
         // then
-        assert_eq!(called_with, data.as_unwrapped_groups(1));
+        assert_eq!(spy.on_chain_called_with(), data.as_unwrapped_groups(1));
+        assert_eq!(spy.off_chain_called_with(), data.as_unwrapped_groups(1));
     }
 
     fn insert_a_coin(tx: &mut StorageTransaction<&mut Database>, utxo_id: &UtxoId) {
@@ -406,19 +373,25 @@ mod tests {
         tx.storage_as_mut::<Coins>().insert(utxo_id, &coin).unwrap();
     }
 
+    fn insert_an_owned_coin(
+        tx: &mut StorageTransaction<&mut Database<OffChain>>,
+        key: OwnedCoinKey,
+    ) {
+        tx.storage_as_mut::<OwnedCoins>().insert(&key, &()).unwrap();
+    }
+
     #[test]
-    fn tx_reverted_if_handler_fails() {
+    fn tx_reverted_if_on_chain_handler_fails() {
         // given
         let groups = TestData::new(1);
         let on_chain_db = Database::default();
-        let off_chain_db = Database::default();
         let utxo_id = UtxoId::new(Default::default(), 0);
 
         // when
         let _ = import_entries(
             CancellationToken::default(),
-            Spy::custom(
-                |_, tx| {
+            Spy::default().custom_importer(
+                move |_, tx| {
                     insert_a_coin(tx, &utxo_id);
                     bail!("Some error")
                 },
@@ -426,7 +399,7 @@ mod tests {
             ),
             groups.as_ok_groups(0),
             on_chain_db.clone(),
-            off_chain_db.clone(),
+            Database::default(),
             ProgressReporter::default(),
         )
         .expect_err("should fail");
@@ -436,22 +409,33 @@ mod tests {
     }
 
     #[test]
-    fn handler_failure_is_propagated() {
+    fn tx_reverted_if_off_chain_handler_fails() {
         // given
         let groups = TestData::new(1);
+        let off_chain_db = Database::default();
+        let key = [0; std::mem::size_of::<OwnedCoinKey>()];
 
         // when
-        let result = import_entries(
+        let _ = import_entries(
             CancellationToken::default(),
-            Spy::custom(|_, _| bail!("Some error"), |_, _| Ok(())),
+            Spy::default().custom_importer(
+                |_, _| Ok(()),
+                move |_, tx| {
+                    insert_an_owned_coin(tx, key);
+                    bail!("Some error")
+                },
+            ),
             groups.as_ok_groups(0),
             Database::default(),
-            Database::default(),
+            off_chain_db.clone(),
             ProgressReporter::default(),
-        );
+        )
+        .expect_err("should fail");
 
         // then
-        assert!(result.is_err());
+        assert!(
+            !StorageInspect::<OwnedCoins>::contains_key(&off_chain_db, &key).unwrap()
+        );
     }
 
     #[test]
@@ -462,7 +446,7 @@ mod tests {
         // when
         let result = import_entries(
             CancellationToken::default(),
-            Spy::custom(|_, _| Ok(()), |_, _| Ok(())),
+            Spy::default().default_importer(),
             groups,
             Database::default(),
             Database::default(),
@@ -483,7 +467,7 @@ mod tests {
         // when
         import_entries(
             CancellationToken::default(),
-            Spy::custom(|_, _| Ok(()), |_, _| Ok(())),
+            Spy::default().default_importer(),
             data.as_ok_groups(0),
             on_chain_db.clone(),
             off_chain_db.clone(),
@@ -499,6 +483,13 @@ mod tests {
             ),
             Some(1)
         );
+        assert_eq!(
+            GenesisProgressInspect::<OffChain>::genesis_progress(
+                &off_chain_db,
+                Coins::column().name(),
+            ),
+            Some(1)
+        );
     }
 
     #[tokio::test]
@@ -506,21 +497,15 @@ mod tests {
         // given
         let (tx, rx) = std::sync::mpsc::channel();
 
-        let read_groups = Arc::new(Mutex::new(vec![]));
         let cancel_token = tokio_util::sync::CancellationToken::new();
+        let spy = Spy::default();
         let runner_handle = {
-            let read_groups = Arc::clone(&read_groups);
             let cancel_token = cancel_token.clone().into();
+            let importer = spy.default_importer();
             std::thread::spawn(move || {
                 import_entries(
                     cancel_token,
-                    Spy::custom(
-                        move |group, _| {
-                            read_groups.lock().unwrap().push(group);
-                            Ok(())
-                        },
-                        |_, _| Ok(()),
-                    ),
+                    importer,
                     rx,
                     Database::default(),
                     Database::default(),
@@ -535,7 +520,7 @@ mod tests {
             tx.send(group).unwrap();
         }
 
-        while read_groups.lock().unwrap().len() < take {
+        while spy.on_chain_called_with().len() < take {
             std::thread::sleep(std::time::Duration::from_millis(1));
         }
 
@@ -554,33 +539,44 @@ mod tests {
         );
 
         // group after signal is not read
-        let read_entries = read_groups.lock().unwrap().clone();
         let inserted_groups = &data.as_unwrapped_groups(0)[..take];
-        assert_eq!(read_entries, inserted_groups);
+        assert_eq!(spy.on_chain_called_with(), inserted_groups);
     }
 
     #[derive(Debug)]
-    struct BrokenTransactions {
-        store: MemoryStore,
+    struct BrokenTransactions<DbDesc>
+    where
+        DbDesc: DatabaseDescription,
+    {
+        store: MemoryStore<DbDesc>,
     }
 
-    impl BrokenTransactions {
-        fn new() -> Self {
+    impl<DbDesc> Default for BrokenTransactions<DbDesc>
+    where
+        DbDesc: DatabaseDescription,
+    {
+        fn default() -> Self {
             Self {
                 store: MemoryStore::default(),
             }
         }
     }
 
-    impl KeyValueInspect for BrokenTransactions {
-        type Column = Column;
+    impl<DbDesc> KeyValueInspect for BrokenTransactions<DbDesc>
+    where
+        DbDesc: DatabaseDescription,
+    {
+        type Column = DbDesc::Column;
 
-        fn get(&self, key: &[u8], column: Column) -> StorageResult<Option<Value>> {
+        fn get(&self, key: &[u8], column: Self::Column) -> StorageResult<Option<Value>> {
             self.store.get(key, column)
         }
     }
 
-    impl IterableStore for BrokenTransactions {
+    impl<DbDesc> IterableStore for BrokenTransactions<DbDesc>
+    where
+        DbDesc: DatabaseDescription,
+    {
         fn iter_store(
             &self,
             _: Self::Column,
@@ -592,18 +588,31 @@ mod tests {
         }
     }
 
-    impl TransactableStorage<BlockHeight> for BrokenTransactions {
+    impl<DbDesc> TransactableStorage<DbDesc::Height> for BrokenTransactions<DbDesc>
+    where
+        DbDesc: DatabaseDescription,
+    {
         fn commit_changes(
             &self,
-            _: Option<BlockHeight>,
+            _: Option<DbDesc::Height>,
             _: Changes,
         ) -> StorageResult<()> {
             Err(anyhow::anyhow!("I refuse to work!").into())
         }
     }
 
-    #[test]
-    fn tx_commit_failure_is_propagated() {
+    #[test_case::test_case(
+        Database::new(Arc::new(BrokenTransactions::<OnChain>::default())),
+        Database::default()
+    ; "broken on chain db")]
+    #[test_case::test_case(
+        Database::default(),
+        Database::new(Arc::new(BrokenTransactions::<OffChain>::default()))
+    ; "broken off chain db")]
+    fn on_chain_tx_commit_failure_is_propagated(
+        on_chain: Database<OnChain>,
+        off_chain: Database<OffChain>,
+    ) {
         // given
         let groups = TestData::new(1);
 
@@ -611,10 +620,10 @@ mod tests {
         // when
         let result = import_entries(
             CancellationToken::default(),
-            Spy::custom(|_, _| Ok(()), |_, _| Ok(())),
+            Spy::default().default_importer(),
             groups.as_ok_groups(0),
-            Database::new(Arc::new(BrokenTransactions::new())),
-            Database::default(),
+            on_chain,
+            off_chain,
             ProgressReporter::default(),
         );
 
