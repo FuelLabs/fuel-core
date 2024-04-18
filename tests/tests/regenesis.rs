59a458624d352a7f190da07bad841a8952c2b875
xgreenx <xgreenx9999@gmail.com>
2024-04-18 17:33:43 +0200

    Added tests for spent messages and processed transactions

--- tests/tests/regenesis.rs
+++ tests/tests/regenesis.rs
@@ -1,13 +1,23 @@
 use clap::Parser;
-use fuel_core::service::{
-    FuelService,
-    ServiceTrait,
+use fuel_core::{
+    chain_config::{
+        ChainConfig,
+        SnapshotWriter,
+    },
+    service::{
+        FuelService,
+        ServiceTrait,
+    },
 };
 use fuel_core_bin::cli::snapshot;
 use fuel_core_client::client::{
     pagination::{
         PageDirection,
         PaginationRequest,
+    },
+    types::{
+        message::MessageStatus,
+        TransactionStatus,
     },
     FuelClient,
 };
@@ -36,7 +46,13 @@ impl FuelCoreDriver {
         // Generate temp params
         let db_dir = tempdir()?;
 
-        let mut args = vec!["_IGNORED_", "--db-path", db_dir.path().to_str().unwrap()];
+        let mut args = vec![
+            "_IGNORED_",
+            "--db-path",
+            db_dir.path().to_str().unwrap(),
+            "--port",
+            "0",
+        ];
         args.extend(extra_args);
 
         let node = fuel_core_bin::cli::run::get_service(
@@ -197,6 +213,160 @@ async fn test_regenesis_old_blocks_are_preserved() -> anyhow::Result<()> {
     assert_eq!(original_blocks[0], regenesis_blocks[0]);
     assert_eq!(original_blocks[1], regenesis_blocks[1]);
     assert_eq!(original_blocks[2], regenesis_blocks[2]);
+
+    Ok(())
+}
+
+#[tokio::test(flavor = "multi_thread")]
+async fn test_regenesis_spent_messages_are_preserved() -> anyhow::Result<()> {
+    let mut rng = StdRng::seed_from_u64(1234);
+    let state_config_dir = tempdir().expect("Failed to create temp dir");
+    let nonce = [123; 32].into();
+    let state_config = fuel_core::chain_config::StateConfig {
+        messages: vec![fuel_core::chain_config::MessageConfig {
+            sender: rng.gen(),
+            recipient: rng.gen(),
+            nonce,
+            amount: 123,
+            data: vec![],
+            da_height: Default::default(),
+        }],
+        ..Default::default()
+    };
+    let writer = SnapshotWriter::json(state_config_dir.path());
+    writer
+        .write_state_config(state_config, &ChainConfig::local_testnet())
+        .unwrap();
+
+    let core = FuelCoreDriver::spawn(&[
+        "--debug",
+        "--poa-instant",
+        "true",
+        "--snapshot",
+        state_config_dir.path().to_str().unwrap(),
+    ])
+    .await?;
+
+    // Add some blocks
+    let secret = SecretKey::random(&mut rng);
+    let tx_with_message = TransactionBuilder::script(vec![], vec![])
+        .add_unsigned_message_input(
+            secret,
+            rng.gen(),
+            nonce,
+            Default::default(),
+            Default::default(),
+        )
+        .add_output(Output::change(
+            Default::default(),
+            Default::default(),
+            Default::default(),
+        ))
+        .finalize_as_transaction();
+    core.client
+        .submit_and_await_commit(&tx_with_message)
+        .await
+        .unwrap();
+
+    let status = core
+        .client
+        .message_status(&nonce)
+        .await
+        .expect("Failed to get message status");
+    assert_eq!(status, MessageStatus::Spent);
+
+    // Stop the node, keep the db
+    let db_dir = core.kill().await;
+
+    // ------------------------- The genesis node is stopped -------------------------
+
+    // Take a snapshot
+    let snapshot_dir = tempdir().expect("Failed to create temp dir");
+    take_snapshot(&db_dir, &snapshot_dir)
+        .await
+        .expect("Failed to take first snapshot");
+
+    // ------------------------- Start a node with the regenesis -------------------------
+
+    // Start a new node with the snapshot
+    let core = FuelCoreDriver::spawn(&[
+        "--debug",
+        "--poa-instant",
+        "true",
+        "--snapshot",
+        snapshot_dir.path().to_str().unwrap(),
+    ])
+    .await?;
+
+    let status = core
+        .client
+        .message_status(&nonce)
+        .await
+        .expect("Failed to get message status");
+    assert_eq!(status, MessageStatus::Spent);
+
+    Ok(())
+}
+
+#[tokio::test(flavor = "multi_thread")]
+async fn test_regenesis_processed_transactions_are_preserved() -> anyhow::Result<()> {
+    let mut rng = StdRng::seed_from_u64(1234);
+    let core = FuelCoreDriver::spawn(&["--debug", "--poa-instant", "true"]).await?;
+
+    // Add some blocks
+    let secret = SecretKey::random(&mut rng);
+    let tx = TransactionBuilder::script(vec![], vec![])
+        .add_unsigned_coin_input(
+            secret,
+            rng.gen(),
+            1234,
+            Default::default(),
+            Default::default(),
+        )
+        .add_output(Output::change(
+            Default::default(),
+            Default::default(),
+            Default::default(),
+        ))
+        .finalize_as_transaction();
+    core.client.submit_and_await_commit(&tx).await.unwrap();
+
+    let TransactionStatus::SqueezedOut { reason } =
+        core.client.submit_and_await_commit(&tx).await.unwrap()
+    else {
+        panic!("Expected transaction to be squeezed out")
+    };
+    assert!(reason.contains("Transaction id was already used"));
+
+    // Stop the node, keep the db
+    let db_dir = core.kill().await;
+
+    // ------------------------- The genesis node is stopped -------------------------
+
+    // Take a snapshot
+    let snapshot_dir = tempdir().expect("Failed to create temp dir");
+    take_snapshot(&db_dir, &snapshot_dir)
+        .await
+        .expect("Failed to take first snapshot");
+
+    // ------------------------- Start a node with the regenesis -------------------------
+
+    // Start a new node with the snapshot
+    let core = FuelCoreDriver::spawn(&[
+        "--debug",
+        "--poa-instant",
+        "true",
+        "--snapshot",
+        snapshot_dir.path().to_str().unwrap(),
+    ])
+    .await?;
+
+    let TransactionStatus::SqueezedOut { reason } =
+        core.client.submit_and_await_commit(&tx).await.unwrap()
+    else {
+        panic!("Expected transaction to be squeezed out")
+    };
+    assert!(reason.contains("Transaction id was already used"));
 
     Ok(())
 }
