6f5c4fc62dcf6ad52413d8e5de3c84c0516208cd
Brandon Vrooman <brandon.vrooman@fuel.sh>
2024-04-29 14:13:09 -0400

    test: restart with relayer data (#1866)
    The relayer service fails to start if the relayer database is already
    populated with a `DaHeightTable` entry. When the relayer starts, it
    attempts to set the DA height to the config's `da_deploy_height`. If the
    `da_deploy_height` defined by the config (where the default is 0) is not
    a monotonically increasing value compared to the database entry, the
    service panics.
    
    Instead of setting we DA height at relayer start up, we can simply
    continue from where the relayer has previously left off. We do not need
    to make this invalid database commit at relayer startup. This PR removes
    the invalid commit.
    
    ## Checklist
    - [x] Breaking changes are clearly marked as such in the PR description
    and changelog
    - [x] New behavior is reflected in tests
    - [ ] [The specification](https://github.com/FuelLabs/fuel-specs/)
    matches the implemented behavior (link update PR if changes are needed)
    
    ### Before requesting review
    - [x] I have reviewed the code myself
    - [ ] I have created follow-up issues caused by this PR and linked them
    here
    
    ### After merging, notify other teams
    
    [Add or remove entries as needed]
    
    - [ ] [Rust SDK](https://github.com/FuelLabs/fuels-rs/)
    - [ ] [Sway compiler](https://github.com/FuelLabs/sway/)
    - [ ] [Platform
    documentation](https://github.com/FuelLabs/devrel-requests/issues/new?assignees=&labels=new+request&projects=&template=NEW-REQUEST.yml&title=%5BRequest%5D%3A+)
    (for out-of-organization contributors, the person merging the PR will do
    this)
    - [ ] Someone else?
    
    ---------
    
    Co-authored-by: xgreenx <xgreenx9999@gmail.com>

--- crates/services/relayer/src/ports.rs
+++ crates/services/relayer/src/ports.rs
@@ -21,16 +21,9 @@ pub trait RelayerDb: Send + Sync {
         events: &[Event],
     ) -> StorageResult<()>;
 
-    /// Set finalized da height that represent last block from da layer that got finalized.
-    /// This will only set the value if it is greater than the current.
-    fn set_finalized_da_height_to_at_least(
-        &mut self,
-        block: &DaBlockHeight,
-    ) -> StorageResult<()>;
-
     /// Get finalized da height that represent last block from da layer that got finalized.
     /// Panics if height is not set as of initialization of database.
-    fn get_finalized_da_height(&self) -> StorageResult<DaBlockHeight>;
+    fn get_finalized_da_height(&self) -> Option<DaBlockHeight>;
 }
 
 /// The trait that should be implemented by the database transaction returned by the database.
@@ -49,4 +42,7 @@ pub trait Transactional {
 
     /// Returns the storage transaction.
     fn transaction(&mut self) -> Self::Transaction<'_>;
+
+    /// Returns the latest da block height.
+    fn latest_da_height(&self) -> Option<DaBlockHeight>;
 }
