c51e15ece80372bb8b2d9f14c48ed09ec5dee8ba
mitchell <james.mitchell.turner@gmail.com>
2024-05-14 15:33:53 -0700

    Simplify algo to ignore block fullness and add d value

--- crates/gas_price_testground/src/main.rs
+++ crates/gas_price_testground/src/main.rs
@@ -4,16 +4,19 @@ use rand::{
     Rng,
     SeedableRng,
 };
-use rand_distr::{
-    Distribution,
-    Normal,
-};
+use std::cell::RefCell;
+
 use std::cmp::max;
 
 struct Algorithm {
     amount: u64,
     min: u64,
-    p_value_factor: u64,
+    p_value_factor: i32,
+    d_value_factor: i32,
+    moving_average_profit: RefCell<i32>,
+    last_profit: RefCell<i32>,
+    profit_slope: RefCell<i32>,
+    moving_average_window: i32,
 }
 
 impl Algorithm {
@@ -25,21 +28,48 @@ impl Algorithm {
         used: u64,
         capacity: u64,
     ) -> u64 {
-        if total_da_recording_cost > total_production_reward {
-            let add =
-                (total_da_recording_cost - total_production_reward) / self.p_value_factor;
-            old_gas_price + add
-        } else {
-            if used > capacity / 2 {
-                todo!();
-            } else if total_production_reward > total_da_recording_cost {
-                let sub = (total_production_reward - total_da_recording_cost)
-                    / self.p_value_factor;
-                max(old_gas_price.saturating_sub(sub), self.min)
-            } else {
-                old_gas_price
-            }
-        }
+        let new_profit = total_production_reward as i32 - total_da_recording_cost as i32;
+        self.calculate_new_moving_average(new_profit);
+        self.calculate_profit_slope(new_profit);
+        let avg_profit = *self.moving_average_profit.borrow();
+        let slope = *self.profit_slope.borrow();
+        // let maybe_new_gas_price = if avg_profit.is_negative() {
+        //     let p_comp = avg_profit.abs() / self.p_value_factor;
+        //     let d_comp = *self.profit_slope.borrow() / self.d_value_factor;
+        //     let add = p_comp - d_comp;
+        //     old_gas_price + add as u64
+        // } else {
+        //     if used > capacity / 2 {
+        //         todo!();
+        //     } else if avg_profit.is_positive() {
+        //         let p_comp = avg_profit / self.p_value_factor;
+        //         let d_comp = *self.profit_slope.borrow() / self.d_value_factor;
+        //         let sub = p_comp - d_comp;
+        //         max(old_gas_price.saturating_sub(sub as u64), self.min)
+        //     } else {
+        //         old_gas_price
+        //     }
+        // };
+        // max(maybe_new_gas_price, self.min)
+        let p_comp = avg_profit / self.p_value_factor;
+        let d_comp = slope / self.d_value_factor;
+        let add = p_comp + d_comp;
+        let new_gas_price = old_gas_price as i32 - add;
+        max(new_gas_price as u64, self.min)
+    }
+
+    fn calculate_new_moving_average(&self, new_profit: i32) {
+        let old = *self.moving_average_profit.borrow();
+        *self.moving_average_profit.borrow_mut() = (old
+            * (self.moving_average_window - 1))
+            .saturating_add(new_profit)
+            .saturating_div(self.moving_average_window);
+    }
+
+    fn calculate_profit_slope(&self, new_profit: i32) {
+        let old_profit = *self.last_profit.borrow();
+        *self.profit_slope.borrow_mut() = new_profit - old_profit;
+        *self.last_profit.borrow_mut() = new_profit;
     }
 }
 
@@ -54,15 +84,15 @@ where
         .fold(0f64, |acc, &c| acc + f64::sin(input / c))
 }
 
-fn arb_cost_signal() -> Vec<u64> {
+fn arb_cost_signal(size: u32) -> Vec<u64> {
     let mut rng = StdRng::seed_from_u64(999);
-    (0u32..1_000)
+    (0u32..size)
         .map(noisy)
+        .map(|x| x * 10_000. + 20_000.)
         .map(|x| {
-            let val = rng.gen_range(-0.5..0.5);
+            let val = rng.gen_range(-19_000.0..10_000.0);
             x + val
         })
-        .map(|x| x * 10_000. + 20_000.)
         .map(|x| x as u64)
         .collect()
 }
@@ -70,18 +100,25 @@ fn arb_cost_signal() -> Vec<u64> {
 fn main() {
     let amount = 1;
     let min = 10;
-    let p_value_factor = 300;
+    let p_value_factor = 1_000;
+    let d_value_factor = 500;
+    let moving_average_window = 1;
     let algo = Algorithm {
         amount,
         min,
         p_value_factor,
+        d_value_factor,
+        moving_average_profit: RefCell::new(0),
+        last_profit: RefCell::new(0),
+        profit_slope: RefCell::new(0),
+        moving_average_window,
     };
 
     let gas_spent = 200;
-    let simulation_size = 1_000;
+    let simulation_size = 1000;
 
     // Run simulation
-    let da_recording_cost = arb_cost_signal();
+    let da_recording_cost = arb_cost_signal(simulation_size);
     let mut gas_price = 100;
     let mut gas_prices = vec![gas_price as i32];
     let mut total_profits = vec![0i32];
