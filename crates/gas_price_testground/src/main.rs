a8cf8e10b11a423ca223de5057e23edf5265f1ff
mitchell <james.mitchell.turner@gmail.com>
2024-05-14 13:57:41 -0700

    Introduce "more realistic" noise to cost signal

--- crates/gas_price_testground/src/main.rs
+++ crates/gas_price_testground/src/main.rs
@@ -1,5 +1,15 @@
 use plotters::prelude::*;
+use rand::{
+    rngs::StdRng,
+    Rng,
+    SeedableRng,
+};
+use rand_distr::{
+    Distribution,
+    Normal,
+};
 use std::cmp::max;
+
 struct Algorithm {
     amount: u64,
     min: u64,
@@ -33,6 +43,30 @@ impl Algorithm {
     }
 }
 
+fn noisy<T: TryInto<f64>>(input: T) -> f64
+where
+    <T as TryInto<f64>>::Error: core::fmt::Debug,
+{
+    let input = input.try_into().unwrap();
+    let components: &[f64] = &[50.0, 100.0, 300.0, 1000.0, 500.0];
+    components
+        .iter()
+        .fold(0f64, |acc, &c| acc + f64::sin(input / c))
+}
+
+fn arb_cost_signal() -> Vec<u64> {
+    let mut rng = StdRng::seed_from_u64(999);
+    (0u32..1_000)
+        .map(noisy)
+        .map(|x| {
+            let val = rng.gen_range(-0.5..0.5);
+            x + val
+        })
+        .map(|x| x * 10_000. + 20_000.)
+        .map(|x| x as u64)
+        .collect()
+}
+
 fn main() {
     let amount = 1;
     let min = 10;
@@ -47,11 +81,7 @@ fn main() {
     let simulation_size = 1_000;
 
     // Run simulation
-    let da_recording_cost = (0u32..simulation_size)
-        .map(|val| f64::try_from(val).unwrap() / 100.)
-        .map(f64::sin)
-        .map(|x| x * 10_000. + 20_000.)
-        .map(|x| x as u64);
+    let da_recording_cost = arb_cost_signal();
     let mut gas_price = 100;
     let mut gas_prices = vec![gas_price as i32];
     let mut total_profits = vec![0i32];
@@ -61,7 +91,7 @@ fn main() {
     // 50% capacity
     let used = gas_spent;
     let capacity = gas_spent * 2;
-    for cost in da_recording_cost.clone() {
+    for cost in &da_recording_cost {
         total_cost += cost;
         let reward = gas_price * gas_spent;
         rewards.push(reward);
@@ -77,10 +107,10 @@ fn main() {
     let root = BitMapBackend::new("gas_prices.png", (640, 480)).into_drawing_area();
     root.fill(&WHITE).unwrap();
 
-    // let min = *total_profits.iter().min().unwrap();
-    // let max = *total_profits.iter().max().unwrap();
-    let min = -10_000;
-    let max = 40_000;
+    let min = *total_profits.iter().min().unwrap() - 10_000;
+    let max = *rewards.iter().max().unwrap() as i32 + 10_000;
+    // let min = -10_000;
+    // let max = 40_000;
 
     let mut chart = ChartBuilder::on(&root)
         .caption("Gas Prices Over Time", ("sans-serif", 50).into_font())
@@ -121,7 +151,10 @@ fn main() {
 
     chart
         .draw_series(LineSeries::new(
-            da_recording_cost.enumerate().map(|(x, y)| (x, y as i32)),
+            da_recording_cost
+                .into_iter()
+                .enumerate()
+                .map(|(x, y)| (x, y as i32)),
             &COST_COLOR,
         ))
         .unwrap()
