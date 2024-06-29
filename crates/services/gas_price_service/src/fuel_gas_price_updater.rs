5a4ee34f8ed11d1555717d9c8deab81569ef71f8
Mitch Turner <james.mitchell.turner@gmail.com>
2024-06-29 12:10:43 -0700

    Try to move adapter into service

--- crates/services/gas_price_service/src/fuel_gas_price_updater.rs
+++ crates/services/gas_price_service/src/fuel_gas_price_updater.rs
@@ -9,6 +9,8 @@ pub use fuel_gas_price_algorithm::{
 #[cfg(test)]
 mod tests;
 
+pub mod fuel_core_storage_adapter;
+
 pub struct FuelGasPriceUpdater<L2, Metadata> {
     inner: AlgorithmUpdaterV1,
     l2_block_source: L2,
---
+++ crates/services/gas_price_service/src/fuel_gas_price_updater/fuel_core_storage_adapter.rs
@@ -0,0 +1,176 @@
+use crate::fuel_gas_price_updater::{
+    fuel_core_storage_adapter::database::GasPriceMetadata,
+    Error,
+    MetadataStorage,
+    UpdaterMetadata,
+};
+use fuel_core_storage::{
+    transactional::{
+        AtomicView,
+        Modifiable,
+        StorageTransaction,
+        WriteTransaction,
+    },
+    StorageAsMut,
+    StorageAsRef,
+};
+use fuel_core_types::{
+    fuel_merkle::storage::{
+        StorageInspect,
+        StorageMutate,
+    },
+    fuel_types::BlockHeight,
+};
+
+#[cfg(test)]
+mod tests {
+    #![allow(non_snake_case)]
+    use super::*;
+
+    use crate::fuel_gas_price_updater::fuel_core_storage_adapter::database::GasPriceColumn;
+    use fuel_core_storage::{
+        structured_storage::test::InMemoryStorage,
+        StorageAsMut,
+    };
+    use fuel_gas_price_algorithm::AlgorithmUpdaterV1;
+
+    fn arb_metadata() -> UpdaterMetadata {
+        let height = 111231u32.into();
+        arb_metadata_with_l2_height(height)
+    }
+
+    fn arb_metadata_with_l2_height(l2_height: BlockHeight) -> UpdaterMetadata {
+        AlgorithmUpdaterV1 {
+            new_exec_price: 100,
+            last_da_gas_price: 34,
+            min_exec_gas_price: 12,
+            exec_gas_price_change_percent: 2,
+            l2_block_height: l2_height.into(),
+            l2_block_fullness_threshold_percent: 0,
+            min_da_gas_price: 0,
+            max_da_gas_price_change_percent: 0,
+            total_da_rewards: 0,
+            da_recorded_block_height: 0,
+            latest_known_total_da_cost: 0,
+            projected_total_da_cost: 0,
+            da_p_component: 0,
+            da_d_component: 0,
+            profit_avg: 0,
+            avg_window: 0,
+            latest_da_cost_per_byte: 0,
+            unrecorded_blocks: vec![],
+        }
+        .into()
+    }
+
+    fn database() -> InMemoryStorage<GasPriceColumn> {
+        InMemoryStorage::default()
+    }
+
+    #[tokio::test]
+    async fn get_metadata__can_get_most_recent_version() {
+        // given
+        let mut database = database();
+        let block_height: BlockHeight = 1u32.into();
+        let metadata = arb_metadata();
+        database
+            .storage_as_mut::<GasPriceMetadata>()
+            .insert(&block_height, &metadata)
+            .unwrap();
+
+        // when
+        let actual = database.get_metadata(&block_height).await.unwrap();
+
+        // then
+        let expected = Some(metadata);
+        assert_eq!(expected, actual);
+    }
+
+    #[tokio::test]
+    async fn get_metadata__returns_none_if_does_not_exist() {
+        // given
+        let database = database();
+        let block_height: BlockHeight = 1u32.into();
+
+        // when
+        let actual = database.get_metadata(&block_height).await.unwrap();
+
+        // then
+        let expected = None;
+        assert_eq!(expected, actual);
+    }
+
+    #[tokio::test]
+    async fn set_metadata__can_set_metadata() {
+        // given
+        let mut database = database();
+        let block_height: BlockHeight = 1u32.into();
+        let metadata = arb_metadata_with_l2_height(block_height);
+
+        // when
+        let actual = database.get_metadata(&block_height).await.unwrap();
+        assert_eq!(None, actual);
+        database.set_metadata(metadata.clone()).await.unwrap();
+        let actual = database.get_metadata(&block_height).await.unwrap();
+
+        // then
+        let expected = Some(metadata);
+        assert_eq!(expected, actual);
+    }
+}
+
+pub mod database;
+
+#[async_trait::async_trait]
+impl<Database> MetadataStorage for Database
+where
+    Database: AtomicView,
+    Database::LatestView: StorageAsRef,
+    Database::LatestView: StorageInspect<GasPriceMetadata>,
+    <Database::LatestView as StorageInspect<GasPriceMetadata>>::Error:
+        Into<anyhow::Error>,
+    Database: Modifiable,
+    Database: WriteTransaction,
+    for<'a> StorageTransaction<&'a mut Database>: StorageMutate<GasPriceMetadata>,
+    for<'a> <StorageTransaction<&'a mut Database> as StorageInspect<GasPriceMetadata>>::Error:
+        Into<anyhow::Error>,
+{
+    async fn get_metadata(
+        &self,
+        block_height: &BlockHeight,
+    ) -> crate::fuel_gas_price_updater::Result<Option<UpdaterMetadata>> {
+        let view = self
+            .latest_view()
+            .map_err(|err| Error::CouldNotFetchMetadata {
+                block_height: *block_height,
+                source_error: err.into(),
+            })?;
+        let metadata = view
+            .storage::<GasPriceMetadata>()
+            .get(block_height)
+            .map_err(|err| Error::CouldNotFetchMetadata {
+                block_height: *block_height,
+                source_error: err.into(),
+            })?;
+        Ok(metadata.map(|inner| inner.into_owned()))
+    }
+
+    async fn set_metadata(
+        &mut self,
+        metadata: UpdaterMetadata,
+    ) -> crate::fuel_gas_price_updater::Result<()> {
+        let block_height = metadata.l2_block_height();
+        let mut tx = self.write_transaction();
+        tx.storage_as_mut::<GasPriceMetadata>()
+            .insert(&block_height, &metadata)
+            .map_err(|err| Error::CouldNotSetMetadata {
+                block_height,
+                source_error: err.into(),
+            })?;
+        tx.commit().map_err(|err| Error::CouldNotSetMetadata {
+            block_height,
+            source_error: err.into(),
+        })?;
+        Ok(())
+    }
+}
