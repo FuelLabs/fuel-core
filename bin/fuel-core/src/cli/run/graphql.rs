279ac4ca3c4cb9bbc36649ef10d831565fd4f1de
green <xgreenx9999@gmail.com>
2024-07-02 10:23:43 +0200

    Use 1 MB as a default value

--- bin/fuel-core/src/cli/run/graphql.rs
+++ bin/fuel-core/src/cli/run/graphql.rs
@@ -25,7 +25,11 @@ pub struct GraphQLArgs {
     pub graphql_max_recursive_depth: usize,
 
     /// The max body limit of the GraphQL query.
-    #[clap(long = "graphql-max-queries-body-limit", default_value = "262144", env)]
+    #[clap(
+        long = "graphql-max-queries-body-limit",
+        default_value = "1048576",
+        env
+    )]
     pub graphql_max_queries_body_limit: usize,
 
     /// Time to wait after submitting a query before debug info will be logged about query.
