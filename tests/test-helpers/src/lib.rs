2aebec92b82a79a3f35477e6ed69156baef13281
Green Baneling <XgreenX9999@gmail.com>
2024-07-02 09:25:31 +0200

    Minimize the number of panics in the codebase (#1999)
    Removed the hard requirement from panic to abort the process to allow
    graceful shutdown. Any panic in any service will trigger the graceful
    shutdown by default.
    
    Also, minimized the number of panics in the codebase by replacing them
    with errors.
    
    ### Before requesting review
    - [x] I have reviewed the code myself

--- tests/test-helpers/src/lib.rs
+++ tests/test-helpers/src/lib.rs
@@ -1,2 +1,11 @@
 pub mod builder;
 pub mod fuel_core_driver;
+
+pub async fn send_graph_ql_query(url: &str, query: &str) -> String {
+    let client = reqwest::Client::new();
+    let mut map = std::collections::HashMap::new();
+    map.insert("query", query);
+    let response = client.post(url).json(&map).send().await.unwrap();
+
+    response.text().await.unwrap()
+}
