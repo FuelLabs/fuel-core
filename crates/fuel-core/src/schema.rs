c51742dce43c6f78ef248a2c93e172a3243696bf
Money Bund <dk_bill@qq.com>
2024-07-16 08:21:19 +0800

    Add files via upload

--- crates/fuel-core/src/schema.rs
+++ crates/fuel-core/src/schema.rs
@@ -127,24 +127,17 @@ where
         first,
         last,
         |after: Option<SchemaKey>, before: Option<SchemaKey>, first, last| async move {
-            let (count, direction) = if let Some(first) = first {
-                (first, IterDirection::Forward)
-            } else if let Some(last) = last {
-                (last, IterDirection::Reverse)
-            } else {
-                return Err(anyhow!("Either `first` or `last` should be provided"))
+            let (count, direction) = match (first, last) {
+                (Some(first), _) => (first, IterDirection::Forward),
+                (_, Some(last)) => (last, IterDirection::Reverse),
+                _ => return Err(anyhow!("Either first or last should be provided")),
             };
 
-            let start;
-            let end;
-
-            if direction == IterDirection::Forward {
-                start = after;
-                end = before;
+            let (start, end) = if direction == IterDirection::Forward {
+                (after, before)
             } else {
-                start = before;
-                end = after;
-            }
+                (before, after)
+            };
 
             let entries = entries(&start, direction)?;
             let mut has_previous_page = false;
