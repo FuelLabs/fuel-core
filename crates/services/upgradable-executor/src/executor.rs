b1f4b45c27d5076bc05c84f9ddd559c8ce627818
Mitch Turner <james.mitchell.turner@gmail.com>
2024-08-09 16:03:44 -0600

    Bump to new version

--- crates/services/upgradable-executor/src/executor.rs
+++ crates/services/upgradable-executor/src/executor.rs
@@ -137,7 +137,8 @@ impl<S, R> Executor<S, R> {
         ("0-29-0", 3),
         ("0-30-0", 4),
         ("0-31-0", 5),
-        ("0-32-0", LATEST_STATE_TRANSITION_VERSION),
+        ("0-32-0", 6),
+        ("0-32-1", LATEST_STATE_TRANSITION_VERSION),
     ];
 
     pub fn new(
