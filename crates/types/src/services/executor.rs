49afa9aa7e88ba24228d169aaccde99bf52f846e
Hannes Karppila <hannes.karppila@gmail.com>
2024-08-15 13:10:06 +0200

    Cleanup

--- crates/types/src/services/executor.rs
+++ crates/types/src/services/executor.rs
@@ -360,20 +360,3 @@ impl From<ValidityError> for TransactionValidityError {
         Self::Validation(CheckError::Validity(e))
     }
 }
-
-/// Error type for the upgradable (wasm) executor.
-#[cfg(feature = "wasm-executor")]
-#[allow(missing_docs)]
-#[derive(Debug, Clone, PartialEq, derive_more::Display, derive_more::From)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
-pub enum UpgradableError {
-    #[display(fmt = "Invalid WASM bytecode: {_0} (version: {_1:?})")]
-    #[cfg(feature = "wasm-executor")]
-    InvalidWasm(
-        String,
-        Option<crate::blockchain::header::StateTransitionBytecodeVersion>,
-    ),
-    /// Normal errors from the executor
-    #[display(fmt = "Eexecutor error: {_0}")]
-    ExecutorError(Error),
-}
