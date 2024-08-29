c619b937b8762501375f34ea9a62241b0b1d4958
rymnc <43716372+rymnc@users.noreply.github.com>
2024-08-29 20:15:24 +0530

    fix: dont use dyn Iterator

--- benches/benches/db_lookup_times_utils/matrix.rs
+++ benches/benches/db_lookup_times_utils/matrix.rs
@@ -2,12 +2,10 @@ pub const BLOCK_COUNT_MATRIX: [u32; 2] = [10, 100];
 pub const TX_COUNT_MATRIX: [u32; 2] = [100, 1000];
 
 // todo: we can make this lazy loaded
-pub fn matrix() -> Box<dyn Iterator<Item = (u32, u32)>> {
-    let iter = BLOCK_COUNT_MATRIX.iter().flat_map(|&block_count| {
+pub fn matrix() -> impl Iterator<Item = (u32, u32)> {
+    BLOCK_COUNT_MATRIX.iter().flat_map(|&block_count| {
         TX_COUNT_MATRIX
             .iter()
             .map(move |&tx_count| (block_count, tx_count))
-    });
-
-    Box::new(iter)
+    })
 }
