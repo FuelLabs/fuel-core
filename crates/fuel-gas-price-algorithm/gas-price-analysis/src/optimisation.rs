dfa76d7851adf73a5788ae7f615208f0ff8bc571
Mitchell Turner <james.mitchell.turner@gmail.com>
2024-09-02 17:30:20 +0200

    Debug V1 algorightm and use more realistic values in gas price analysis (#2129)
    ## Linked Issues
    https://github.com/FuelLabs/fuel-core/issues/2148
    
    ## Description
    
    Tuning and improving the V1 gas price algorithm is still and ongoing
    process, but this has drastically improved the accuracy of the algorithm
    when trying to track the costs of the DA block posting.
    
    Some terms I'll use here:
    
    **"exec gas price"**: The portion of the L2 gas price that goes toward
    executing transations on L2
    
    **"da gas price"**: The portion of the L2 gas price that goes toward
    paying for committing block to DA
    
    **"cost"**: The amount in base asset to commit a block to the DA
    
    **"reward"**: The amount in base asset received by block producer from
    the "da gas price"
    
    **"profit"**: The difference between "reward" and "cost". Negative
    "profit" means it has costed more in total to commit blocks than has
    been rewarded in total to the block producer
    
    **"accuracy"**: The sum of all profit errors over the sample (either
    positive or negative). The optimization is trying to minimize the error
    for the highest "accuracy"
    
    As you can see, however, there is room to improve:
    <img width="444" alt="image"
    src="https://github.com/user-attachments/assets/9b5715ed-234d-466f-9da0-8bf77a2ad539">
    
    During a relatively stable period where the posting costs barely change,
    the profit is still oscillating. We can dampen this by increasing the
    `D` value, but this doesn't result in a better accuracy:
    <img width="444" alt="image"
    src="https://github.com/user-attachments/assets/e272f433-4664-451d-b877-5df38d932b25">
    
    The max error is much higher in this case. i.e. the profit gets much
    higher/lower. Maybe this is okay?
    
    We can continue to improve the algorithm and do more tuning. A followup
    issue to this is to use real values instead of simulated DA gas prices
    https://github.com/FuelLabs/fuel-core/issues/2138. Perhaps that will
    have less noise :) Another possibility is that we choose more inaccuracy
    in favor of a smoother algorithm.
    
    This PR includes some of the following changes:
    
    - Modify the way we generate DA costs to be in a more realistic range
    - Just use the previous two profits to calculate the slope for the `D`
    component (ignore bytes for now)
    - Introduce DA gas price factor so that we can deal with smaller values
    while still increasing/decreasing. i.e. (similar to what we do with Fuel
    gas price factor or instead of using a float)
    - Add bytes per block chart 
    - ...
    
    ## Checklist
    - [ ] Breaking changes are clearly marked as such in the PR description
    and changelog
    - [ ] New behavior is reflected in tests
    - [ ] [The specification](https://github.com/FuelLabs/fuel-specs/)
    matches the implemented behavior (link update PR if changes are needed)
    
    ### Before requesting review
    - [ ] I have reviewed the code myself
    - [ ] I have created follow-up issues caused by this PR and linked them
    here
    
    ### After merging, notify other teams
    
    [Add or remove entries as needed]
    
    - [ ] [Rust SDK](https://github.com/FuelLabs/fuels-rs/)
    - [ ] [Sway compiler](https://github.com/FuelLabs/sway/)
    - [ ] [Platform
    documentation](https://github.com/FuelLabs/devrel-requests/issues/new?assignees=&labels=new+request&projects=&template=NEW-REQUEST.yml&title=%5BRequest%5D%3A+)
    (for out-of-organization contributors, the person merging the PR will do
    this)
    - [ ] Someone else?
    
    ---------
    
    Co-authored-by: Mårten Blankfors <nilssonmartenx@gmail.com>

--- crates/fuel-gas-price-algorithm/gas-price-analysis/src/optimisation.rs
+++ crates/fuel-gas-price-algorithm/gas-price-analysis/src/optimisation.rs
@@ -1,26 +1,26 @@
 use super::*;
 
-fn da_pid_factors(size: usize) -> Vec<(i64, i64)> {
-    let mut rng = StdRng::seed_from_u64(10902);
-    (0usize..size)
-        .map(|_| {
-            let p = rng.gen_range(1_000..100_000_000_000);
-            let d = rng.gen_range(1_000..100_000_000_000);
-            (p, d)
-        })
-        .collect()
+fn da_pid_factors(_size: usize) -> Vec<(i64, i64)> {
+    // let mut rng = StdRng::seed_from_u64(10902);
+    // (0usize..size)
+    //     .map(|_| {
+    //         let p = rng.gen_range(100_000..5_000_000);
+    //         let d = rng.gen_range(100_000..5_000_000);
+    //         (p, d)
+    //     })
+    //     .collect()
+    // vec![(1_700_000, 50_000)] // Better on short term
+    vec![(1_700_000, 650_000)] // Safe
 }
 
-pub fn naive_optimisation(
-    iterations: usize,
-    avg_window: u32,
-) -> (SimulationResults, (i64, i64, u32)) {
+pub fn naive_optimisation(iterations: usize) -> (SimulationResults, (i64, i64)) {
     da_pid_factors(iterations)
         .iter()
-        .map(|(p, d)| (run_simulation(*p, *d, avg_window), (*p, *d, avg_window)))
+        .map(|(p, d)| (run_simulation(*p, *d), (*p, *d)))
         .min_by_key(|(results, _)| {
             let SimulationResults { actual_profit, .. } = results;
-            actual_profit.iter().map(|p| p.abs()).sum::<i64>()
+            let err = actual_profit.iter().map(|p| p.abs()).sum::<i64>();
+            err
         })
         .unwrap()
 }
