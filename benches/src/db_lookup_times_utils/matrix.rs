4202d9e10dc8a23f1e7c7782542c968949a75a6a
rymnc <43716372+rymnc@users.noreply.github.com>
2024-09-06 12:38:46 +0530

    fix: matrix

--- benches/src/db_lookup_times_utils/matrix.rs
+++ benches/src/db_lookup_times_utils/matrix.rs
@@ -1,7 +1,7 @@
 use fuel_core_types::fuel_types::BlockHeight;
 
-pub const BLOCK_COUNT_MATRIX: [u32; 1] = [4];
-pub const TX_COUNT_MATRIX: [u32; 1] = [1];
+pub const BLOCK_COUNT_MATRIX: [u32; 5] = [10, 100, 1000, 5000, 10_000];
+pub const TX_COUNT_MATRIX: [u32; 3] = [500, 1000, 2000];
 
 pub fn matrix() -> impl Iterator<Item = (BlockHeight, u32)> {
     BLOCK_COUNT_MATRIX.iter().flat_map(|&block_count| {
