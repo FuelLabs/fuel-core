8ac1599e179877e647bc401809bae082e3912f9d
Green Baneling <XgreenX9999@gmail.com>
2024-09-12 19:33:08 +0200

    Update benches/src/lib.rs
    Co-authored-by: Mårten Blankfors <nilssonmartenx@gmail.com>

--- benches/src/lib.rs
+++ benches/src/lib.rs
@@ -455,8 +455,7 @@ impl TryFrom<VmBench> for VmBenchPrepared {
 
         if let Some(BlobCode { code, id }) = blob {
             db.storage_as_mut::<BlobData>()
-                .insert(&id, &code.0)
-                .unwrap();
+                .insert(&id, &code.0)?;
         }
 
         for contract_id in empty_contracts {
