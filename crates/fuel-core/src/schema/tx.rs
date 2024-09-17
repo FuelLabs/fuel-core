5bb588e966f09521885f273dbd64d494ee83fa42
Mårten Blankfors <marten@blankfors.se>
2024-09-17 10:22:27 +0200

    chore: Reference #1749 instead of duplicate issue

--- crates/fuel-core/src/schema/tx.rs
+++ crates/fuel-core/src/schema/tx.rs
@@ -299,8 +299,8 @@ impl TxMutation {
         let tx_statuses = block_producer
             .dry_run_txs(
                 transactions,
-                None, // TODO(#2207): Pass parameter from API
-                None, // TODO(#2207): Pass parameter from API
+                None, // TODO(#1749): Pass parameter from API
+                None, // TODO(#1749): Pass parameter from API
                 utxo_validation,
                 gas_price.map(|x| x.into()),
             )
