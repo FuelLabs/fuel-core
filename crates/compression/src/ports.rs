7e8424ecd23573ce38288f00b323d3c367a3ad60
Hannes Karppila <hannes.karppila@gmail.com>
2024-09-17 12:14:09 +0200

    Remove some commented-out code

--- crates/compression/src/ports.rs
+++ crates/compression/src/ports.rs
@@ -66,19 +66,3 @@ pub struct MessageInfo {
     pub amount: Word,
     pub data: Vec<u8>,
 }
-
-// Exposed interfaces: where should these live?
-
-// use fuel_core_types::blockchain::block::Block;
-
-// #[async_trait::async_trait]
-// pub trait CompressPort {
-//     /// Compress the next block.
-//     async fn compress_next(&mut self, block: Block) -> anyhow::Result<Vec<u8>>;
-// }
-
-// #[async_trait::async_trait]
-// pub trait DecompressPort {
-//     /// Decompress the next block.
-//     async fn decompress_next(&mut self, block: Vec<u8>) -> anyhow::Result<Block>;
-// }
