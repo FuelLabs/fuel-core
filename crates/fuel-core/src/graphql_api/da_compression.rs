99f875646fee35e1537471cc3efb7bf32be82e9f
Hannes Karppila <hannes.karppila@gmail.com>
2024-09-18 21:04:23 +0300

    Clean up

--- crates/fuel-core/src/graphql_api/da_compression.rs
+++ crates/fuel-core/src/graphql_api/da_compression.rs
@@ -36,91 +36,7 @@ where
     T: OffChainDatabaseTransaction,
     T: StorageInspect<DaCompressionTemporalRegistry>,
 {
-    struct DbTx<'a, Tx>(&'a mut Tx, &'a [Event]);
-
-    impl<'a, Tx> TemporalRegistry for DbTx<'a, Tx>
-    where
-        Tx: OffChainDatabaseTransaction,
-        Tx: StorageInspect<DaCompressionTemporalRegistry>,
-    {
-        fn read_registry(
-            &self,
-            keyspace: RegistryKeyspace,
-            key: fuel_core_types::fuel_compression::RegistryKey,
-        ) -> anyhow::Result<Vec<u8>> {
-            Ok(self
-                .0
-                .storage_as_ref::<DaCompressionTemporalRegistry>()
-                .get(&(keyspace, key))?
-                .ok_or(not_found!(DaCompressionTemporalRegistry))?
-                .into_owned())
-        }
-
-        fn write_registry(
-            &mut self,
-            keyspace: RegistryKeyspace,
-            key: fuel_core_types::fuel_compression::RegistryKey,
-            value: Vec<u8>,
-        ) -> anyhow::Result<()> {
-            // Write the actual value
-            self.0
-                .storage_as_mut::<DaCompressionTemporalRegistry>()
-                .insert(&(keyspace, key), &value)?;
-
-            // Remove the overwritten value from index, if any
-            self.0
-                .storage_as_mut::<DaCompressionTemporalRegistryIndex>()
-                .remove(&(keyspace, value.clone()))?;
-
-            // Add the new value to the index
-            self.0
-                .storage_as_mut::<DaCompressionTemporalRegistryIndex>()
-                .insert(&(keyspace, value), &key)?;
-
-            Ok(())
-        }
-
-        fn registry_index_lookup(
-            &self,
-            keyspace: RegistryKeyspace,
-            value: Vec<u8>,
-        ) -> anyhow::Result<Option<fuel_core_types::fuel_compression::RegistryKey>>
-        {
-            Ok(self
-                .0
-                .storage_as_ref::<DaCompressionTemporalRegistryIndex>()
-                .get(&(keyspace, value))?
-                .map(|v| v.into_owned()))
-        }
-    }
-
-    impl<'a, Tx> UtxoIdToPointer for DbTx<'a, Tx>
-    where
-        Tx: OffChainDatabaseTransaction,
-    {
-        fn lookup(
-            &self,
-            utxo_id: fuel_core_types::fuel_tx::UtxoId,
-        ) -> anyhow::Result<fuel_core_types::fuel_tx::CompressedUtxoId> {
-            for event in self.1 {
-                match event {
-                    Event::CoinCreated(coin) | Event::CoinConsumed(coin)
-                        if coin.utxo_id == utxo_id =>
-                    {
-                        let output_index = coin.utxo_id.output_index();
-                        return Ok(fuel_core_types::fuel_tx::CompressedUtxoId {
-                            tx_pointer: coin.tx_pointer,
-                            output_index,
-                        });
-                    }
-                    _ => {}
-                }
-            }
-            panic!("UtxoId not found in the block events");
-        }
-    }
-
-    let compressed = compress(DbTx(transaction, events), block)
+    let compressed = compress(CompressTx(transaction, events), block)
         .now_or_never()
         .expect("The current implementation resolved all futures instantly")?;
 
@@ -129,4 +45,88 @@ where
         .insert(&block.header().consensus().height, &compressed)?;
 
     Ok(())
+}
+
+struct CompressTx<'a, Tx>(&'a mut Tx, &'a [Event]);
+
+impl<'a, Tx> TemporalRegistry for CompressTx<'a, Tx>
+where
+    Tx: OffChainDatabaseTransaction,
+    Tx: StorageInspect<DaCompressionTemporalRegistry>,
+{
+    fn read_registry(
+        &self,
+        keyspace: RegistryKeyspace,
+        key: fuel_core_types::fuel_compression::RegistryKey,
+    ) -> anyhow::Result<Vec<u8>> {
+        Ok(self
+            .0
+            .storage_as_ref::<DaCompressionTemporalRegistry>()
+            .get(&(keyspace, key))?
+            .ok_or(not_found!(DaCompressionTemporalRegistry))?
+            .into_owned())
+    }
+
+    fn write_registry(
+        &mut self,
+        keyspace: RegistryKeyspace,
+        key: fuel_core_types::fuel_compression::RegistryKey,
+        value: Vec<u8>,
+    ) -> anyhow::Result<()> {
+        // Write the actual value
+        self.0
+            .storage_as_mut::<DaCompressionTemporalRegistry>()
+            .insert(&(keyspace, key), &value)?;
+
+        // Remove the overwritten value from index, if any
+        self.0
+            .storage_as_mut::<DaCompressionTemporalRegistryIndex>()
+            .remove(&(keyspace, value.clone()))?;
+
+        // Add the new value to the index
+        self.0
+            .storage_as_mut::<DaCompressionTemporalRegistryIndex>()
+            .insert(&(keyspace, value), &key)?;
+
+        Ok(())
+    }
+
+    fn registry_index_lookup(
+        &self,
+        keyspace: RegistryKeyspace,
+        value: Vec<u8>,
+    ) -> anyhow::Result<Option<fuel_core_types::fuel_compression::RegistryKey>>
+    {
+        Ok(self
+            .0
+            .storage_as_ref::<DaCompressionTemporalRegistryIndex>()
+            .get(&(keyspace, value))?
+            .map(|v| v.into_owned()))
+    }
+}
+
+impl<'a, Tx> UtxoIdToPointer for CompressTx<'a, Tx>
+where
+    Tx: OffChainDatabaseTransaction,
+{
+    fn lookup(
+        &self,
+        utxo_id: fuel_core_types::fuel_tx::UtxoId,
+    ) -> anyhow::Result<fuel_core_types::fuel_tx::CompressedUtxoId> {
+        for event in self.1 {
+            match event {
+                Event::CoinCreated(coin) | Event::CoinConsumed(coin)
+                    if coin.utxo_id == utxo_id =>
+                {
+                    let output_index = coin.utxo_id.output_index();
+                    return Ok(fuel_core_types::fuel_tx::CompressedUtxoId {
+                        tx_pointer: coin.tx_pointer,
+                        output_index,
+                    });
+                }
+                _ => {}
+            }
+        }
+        panic!("UtxoId not found in the block events");
+    }
 }
