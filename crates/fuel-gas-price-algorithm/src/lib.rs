4337b14502861275c52aabedc4d9396b6f9e5147
Rafał Chabowski <rafal.chabowski@fuel.sh>
2024-09-19 12:10:53 +0200

    Use more clear variable names in `cumulative_percentage_change`

--- crates/fuel-gas-price-algorithm/src/lib.rs
+++ crates/fuel-gas-price-algorithm/src/lib.rs
@@ -17,10 +17,10 @@ pub(crate) fn cumulative_percentage_change(
     let multiple = (1.0f64 + percentage_as_decimal).powf(blocks);
     let mut approx = new_exec_price as f64 * multiple;
     // account for rounding errors and take a slightly higher value
-    const ARB_CUTOFF: f64 = 16948547188989277.0;
-    if approx > ARB_CUTOFF {
-        const ARB_ADDITION: f64 = 2000.0;
-        approx += ARB_ADDITION;
+    const ROUNDING_ERROR_CUTOFF: f64 = 16948547188989277.0;
+    if approx > ROUNDING_ERROR_CUTOFF {
+        const ROUNDING_ERROR_COMPENSATION: f64 = 2000.0;
+        approx += ROUNDING_ERROR_COMPENSATION;
     }
     // `f64` over `u64::MAX` are cast to `u64::MAX`
     approx.ceil() as u64
