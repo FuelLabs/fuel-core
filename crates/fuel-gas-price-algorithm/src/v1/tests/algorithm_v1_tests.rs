7359bd7a850e6a48732c4bc887526dd39093f196
Rafał Chabowski <rafal.chabowski@fuel.sh>
2024-09-25 10:06:35 +0200

    Remove the redundant test as `da_change` no longer panics

--- crates/fuel-gas-price-algorithm/src/v1/tests/algorithm_v1_tests.rs
+++ crates/fuel-gas-price-algorithm/src/v1/tests/algorithm_v1_tests.rs
@@ -1,10 +1,6 @@
-use std::num::NonZeroU64;
-
 use crate::v1::{
     tests::UpdaterBuilder,
-    AlgorithmUpdaterV1,
     AlgorithmV1,
-    ClampedPercentage,
 };
 use proptest::prelude::*;
 
@@ -146,41 +142,4 @@ fn worst_case__same_block_gives_the_same_value_as_calculate() {
     // then
     let expected = algorithm.calculate();
     assert_eq!(expected, actual);
-}
-
-#[test]
-fn da_change_should_not_panic() {
-    const HEIGHT: u32 = 0;
-
-    // The following values are chosen to make the `p.saturating_add(d)` result in
-    // i128::MIN, which would cause a panic if the calculation is not using `saturating_abs()`.
-    const LAST_PROFIT: i128 = i128::MIN / 2;
-    const TOTAL_DA_REWARDS_EXCESS: i128 = i128::MAX / 2;
-    const P: i64 = 1;
-    const D: i64 = 1;
-
-    let mut updater = AlgorithmUpdaterV1 {
-        new_scaled_exec_price: 0,
-        min_exec_gas_price: 0,
-        exec_gas_price_change_percent: 0,
-        l2_block_height: HEIGHT,
-        l2_block_fullness_threshold_percent: ClampedPercentage::new(0),
-        new_scaled_da_gas_price: 0,
-        gas_price_factor: NonZeroU64::new(1).unwrap(),
-        min_da_gas_price: 0,
-        max_da_gas_price_change_percent: 0,
-        total_da_rewards_excess: TOTAL_DA_REWARDS_EXCESS as u128,
-        da_recorded_block_height: 0,
-        latest_known_total_da_cost_excess: 0,
-        projected_total_da_cost: 0,
-        da_p_component: P,
-        da_d_component: D,
-        last_profit: LAST_PROFIT,
-        second_to_last_profit: 0,
-        latest_da_cost_per_byte: 0,
-        unrecorded_blocks: vec![],
-    };
-
-    let _ =
-        updater.update_l2_block_data(HEIGHT + 1, 0, NonZeroU64::new(1).unwrap(), 0, 0);
 }
