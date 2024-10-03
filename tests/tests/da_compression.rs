cf03b7fa86e80ff6bdd730db841de9623dd778c9
Hannes Karppila <hannes.karppila@gmail.com>
2024-10-03 16:33:05 +0300

    Make integration tests sligtly more accurate

--- tests/tests/da_compression.rs
+++ tests/tests/da_compression.rs
@@ -12,12 +12,18 @@ use fuel_core_client::client::{
     types::TransactionStatus,
     FuelClient,
 };
+use fuel_core_compression::VersionedCompressedBlock;
 use fuel_core_poa::signer::SignMode;
 use fuel_core_types::{
+    fuel_asm::{
+        op,
+        RegId,
+    },
     fuel_crypto::SecretKey,
     fuel_tx::{
+        GasCosts,
         Input,
-        Transaction,
+        TransactionBuilder,
     },
     secrecy::Secret,
 };
@@ -34,28 +40,35 @@ async fn can_fetch_da_compressed_block_from_graphql() {
     let db = CombinedDatabase::default();
     let mut config = Config::local_node();
     config.consensus_signer = SignMode::Key(Secret::new(poa_secret.into()));
-    config.da_compression = DaCompressionConfig::Enabled(fuel_core_compression::Config {
+    let compression_config = fuel_core_compression::Config {
         temporal_registry_retention: Duration::from_secs(3600),
-    });
+    };
+    config.da_compression = DaCompressionConfig::Enabled(compression_config);
     let srv = FuelService::from_combined_database(db.clone(), config)
         .await
         .unwrap();
     let client = FuelClient::from(srv.bound_address);
 
-    let status = client
-        .submit_and_await_commit(&Transaction::default_test_tx())
-        .await
-        .unwrap();
+    let tx =
+        TransactionBuilder::script([op::ret(RegId::ONE)].into_iter().collect(), vec![])
+            .max_fee_limit(0)
+            .script_gas_limit(1_000_000)
+            .with_gas_costs(GasCosts::free())
+            .add_random_fee_input()
+            .finalize_as_transaction();
+
+    let status = client.submit_and_await_commit(&tx).await.unwrap();
 
     let block_height = match status {
         TransactionStatus::Success { block_height, .. } => block_height,
-        _ => {
-            panic!("unexpected result")
+        other => {
+            panic!("unexpected result {other:?}")
         }
     };
 
     let block = client.da_compressed_block(block_height).await.unwrap();
-    assert!(block.is_some());
+    let block = block.expect("Unable to get compressed block");
+    let _: VersionedCompressedBlock = postcard::from_bytes(&block).unwrap();
 }
 
 #[tokio::test(flavor = "multi_thread")]
@@ -97,9 +110,10 @@ async fn da_compressed_blocks_are_available_from_non_block_producing_nodes() {
 
     let block_height = 1u32.into();
 
-    let _ = v_client
+    let block = v_client
         .da_compressed_block(block_height)
         .await
         .unwrap()
         .expect("Compressed block not available from validator");
+    let _: VersionedCompressedBlock = postcard::from_bytes(&block).unwrap();
 }
