27fb0fd6ac7f541acebf61e20634f5919fc5a3b6
Rafał Chabowski <rafal.chabowski@fuel.sh>
2024-10-07 18:12:53 +0200

    Update bucket values for new metrics

--- crates/metrics/src/lib.rs
+++ crates/metrics/src/lib.rs
@@ -40,37 +40,38 @@ pub fn timing_buckets() -> &'static Vec<f64> {
 static GAS_USED_BUCKETS: OnceLock<Vec<f64>> = OnceLock::new();
 pub fn gas_used_buckets() -> &'static Vec<f64> {
     GAS_USED_BUCKETS.get_or_init(|| {
-        // TODO[RC]: Figure out proper values.
         vec![
-            0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1.0, 2.5, 5.0, 10.0,
+            10000.0, 25000.0, 50000.0, 100000.0, 500000.0, 1000000.0, 1875000.0,
+            3750000.0, 7500000.0, 15000000.0, 30000000.0,
         ]
     })
 }
 static TRANSACTIONS_COUNT_BUCKETS: OnceLock<Vec<f64>> = OnceLock::new();
 pub fn transactions_count_buckets() -> &'static Vec<f64> {
     TRANSACTIONS_COUNT_BUCKETS.get_or_init(|| {
-        // TODO[RC]: Figure out proper values.
         vec![
-            0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1.0, 2.5, 5.0, 10.0,
+            5.0, 10.0, 25.0, 50.0, 100.0, 1000.0, 5000.0, 10000.0, 15000.0, 30000.0,
+            65535.0,
         ]
     })
 }
 static FEE_BUCKETS: OnceLock<Vec<f64>> = OnceLock::new();
 pub fn fee_buckets() -> &'static Vec<f64> {
     FEE_BUCKETS.get_or_init(|| {
-        // TODO[RC]: Figure out proper values.
+        // TODO[RC]: Uses the same values as gas_used_buckets for now. We should probably change this.
         vec![
-            0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1.0, 2.5, 5.0, 10.0,
+            10000.0, 25000.0, 50000.0, 100000.0, 500000.0, 1000000.0, 1875000.0,
+            3750000.0, 7500000.0, 15000000.0, 30000000.0,
         ]
     })
 }
-
 static GAS_PRICE_BUCKETS: OnceLock<Vec<f64>> = OnceLock::new();
 pub fn gas_price_buckets() -> &'static Vec<f64> {
     GAS_PRICE_BUCKETS.get_or_init(|| {
-        // TODO[RC]: Figure out proper values.
+        // TODO[RC]: Uses the same values as gas_used_buckets for now. We should probably change this.
         vec![
-            0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1.0, 2.5, 5.0, 10.0,
+            10000.0, 25000.0, 50000.0, 100000.0, 500000.0, 1000000.0, 1875000.0,
+            3750000.0, 7500000.0, 15000000.0, 30000000.0,
         ]
     })
 }
