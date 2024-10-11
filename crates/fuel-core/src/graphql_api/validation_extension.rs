1372d79c35b15ca61ee658006996ca431aeba857
Green Baneling <XgreenX9999@gmail.com>
2024-10-11 17:20:53 +0200

    Reject queries that are recursive during the resolution (#2330)
    If during resolution of the query we hit the same type, reject further
    execution. The code to do this check uses `Visitor` logic from the
    `async-graphql` library, but this logic is private, so I duplicated it
    in our branch, Later it would be nice to create PR to them in make it
    public. Or add a way to pass our own query validators into the
    `async-graphql`(which will be more performant).
    
    ## Checklist
    - [x] Breaking changes are clearly marked as such in the PR description
    and changelog
    - [x] New behavior is reflected in tests
    
    ### Before requesting review
    - [x] I have reviewed the code myself
    
    ---------
    
    Co-authored-by: AurelienFT <aurelien.foucault@epitech.eu>
    Co-authored-by: Mårten Blankfors <marten@blankfors.se>
    Co-authored-by: AurelienFT <32803821+AurelienFT@users.noreply.github.com>
    Co-authored-by: AurelienFT <aurefook@gmail.com>

---
+++ crates/fuel-core/src/graphql_api/validation_extension.rs
@@ -0,0 +1,110 @@
+use crate::{
+    fuel_core_graphql_api::validation_extension::visitor::{
+        visit,
+        RuleError,
+        VisitorContext,
+    },
+    graphql_api::validation_extension::recursion_finder::RecursionFinder,
+};
+use async_graphql::{
+    extensions::{
+        Extension,
+        ExtensionContext,
+        ExtensionFactory,
+        NextParseQuery,
+        NextValidation,
+    },
+    parser::types::ExecutableDocument,
+    ServerError,
+    ServerResult,
+    ValidationResult,
+    Variables,
+};
+use std::sync::{
+    Arc,
+    Mutex,
+};
+
+mod recursion_finder;
+mod visitor;
+
+/// The extension validates the queries.
+pub(crate) struct ValidationExtension {
+    recursion_limit: usize,
+}
+
+impl ValidationExtension {
+    pub fn new(recursion_limit: usize) -> Self {
+        Self { recursion_limit }
+    }
+}
+
+impl ExtensionFactory for ValidationExtension {
+    fn create(&self) -> Arc<dyn Extension> {
+        Arc::new(ValidationInner::new(self.recursion_limit))
+    }
+}
+
+struct ValidationInner {
+    recursion_limit: usize,
+    errors: Mutex<Vec<RuleError>>,
+}
+
+impl ValidationInner {
+    fn new(recursion_limit: usize) -> Self {
+        Self {
+            recursion_limit,
+            errors: Default::default(),
+        }
+    }
+}
+
+#[async_trait::async_trait]
+impl Extension for ValidationInner {
+    async fn parse_query(
+        &self,
+        ctx: &ExtensionContext<'_>,
+        query: &str,
+        variables: &Variables,
+        next: NextParseQuery<'_>,
+    ) -> ServerResult<ExecutableDocument> {
+        let result = next.run(ctx, query, variables).await?;
+        let registry = &ctx.schema_env.registry;
+        let mut visitor = VisitorContext::new(registry, &result, Some(variables));
+        visit(
+            &mut RecursionFinder::new(self.recursion_limit),
+            &mut visitor,
+            &result,
+        );
+
+        let errors = visitor.errors;
+
+        if !errors.is_empty() {
+            let mut store = self
+                .errors
+                .lock()
+                .expect("Only one instance owns `ValidationInner`; qed");
+            store.extend(errors);
+        }
+
+        Ok(result)
+    }
+
+    async fn validation(
+        &self,
+        ctx: &ExtensionContext<'_>,
+        next: NextValidation<'_>,
+    ) -> async_graphql::Result<ValidationResult, Vec<ServerError>> {
+        {
+            let mut errors = self
+                .errors
+                .lock()
+                .expect("Only one instance owns `ValidationInner`; qed");
+            if !errors.is_empty() {
+                return Err(errors.drain(..).map(Into::into).collect())
+            }
+        }
+
+        next.run(ctx).await
+    }
+}
