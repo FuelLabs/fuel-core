c4f6f0107e795b17a0164324b3acd7103aa6022e
Mårten Blankfors <marten@blankfors.se>
2024-11-09 19:08:16 +0700

    wip: Pre-populate database

--- benches/benches/end_to_end_query_times.rs
+++ benches/benches/end_to_end_query_times.rs
@@ -5,12 +5,36 @@ use fuel_core::{
         FuelService,
     },
 };
+use fuel_core_storage::{
+    tables::FuelBlocks,
+    transactional::WriteTransaction,
+    StorageAsMut,
+};
+
+use fuel_core_types::{
+    blockchain::block::CompressedBlock,
+    fuel_types::BlockHeight,
+};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let config = Config::local_node();
-    let db: Database = Database::default();
+    let mut db: Database = Database::default();
     let node = FuelService::from_database(db.clone(), config).await?;
+
+    let height = BlockHeight::from(1);
+    let block = CompressedBlock::default();
+
+    let mut transaction = db.write_transaction();
+    transaction
+        .storage::<FuelBlocks>()
+        .insert(&height, &block)
+        .unwrap();
+    // transaction
+    //    .storage::<SealedBlockConsensus>()
+    //    .insert(&height, &Consensus::PoA(Default::default()))
+    //    .unwrap();
+    transaction.commit().unwrap();
 
     let url = format!("http://{}/v1/graphql", node.bound_address);
     let response = test_helpers::send_graph_ql_query(&url, BLOCK_QUERY).await;
