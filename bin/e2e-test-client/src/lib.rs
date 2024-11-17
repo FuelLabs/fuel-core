0c93efefd5ddb57bc33e73d5e5a65a3e09664599
green <xgreenx9999@gmail.com>
2024-11-16 23:38:17 -0500

    Clean up small things

--- bin/e2e-test-client/src/lib.rs
+++ bin/e2e-test-client/src/lib.rs
@@ -15,7 +15,7 @@ use std::{
 };
 
 pub const CONFIG_FILE_KEY: &str = "FUEL_CORE_E2E_CONFIG";
-pub const SYNC_TIMEOUT: Duration = Duration::from_secs(10);
+pub const SYNC_TIMEOUT: Duration = Duration::from_secs(20);
 
 pub mod config;
 pub mod test_context;
