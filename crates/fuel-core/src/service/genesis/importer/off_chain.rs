53631e7143e04c12b9e81b958624e8173519d080
Rafał Chabowski <rafal.chabowski@fuel.sh>
2024-11-21 12:04:36 +0100

    Extract and reuse `BALANCES_INDEXATION_ENABLED` flag

--- crates/fuel-core/src/service/genesis/importer/off_chain.rs
+++ crates/fuel-core/src/service/genesis/importer/off_chain.rs
@@ -43,6 +43,9 @@ use super::{
     Handler,
 };
 
+// We always want to enable balances indexation if we're starting at genesis.
+const BALANCES_INDEXATION_ENABLED: bool = true;
+
 impl ImportTable for Handler<TransactionStatuses, TransactionStatuses> {
     type TableInSnapshot = TransactionStatuses;
     type TableBeingWritten = TransactionStatuses;
@@ -107,9 +110,6 @@ impl ImportTable for Handler<OwnedMessageIds, Messages> {
         group: Vec<TableEntry<Self::TableInSnapshot>>,
         tx: &mut StorageTransaction<&mut GenesisDatabase<Self::DbDesc>>,
     ) -> anyhow::Result<()> {
-        // We always want to enable balances indexation if we're starting at genesis.
-        const BALANCES_INDEXATION_ENABLED: bool = true;
-
         let events = group
             .into_iter()
             .map(|TableEntry { value, .. }| Cow::Owned(Event::MessageImported(value)));
@@ -128,9 +128,6 @@ impl ImportTable for Handler<OwnedCoins, Coins> {
         group: Vec<TableEntry<Self::TableInSnapshot>>,
         tx: &mut StorageTransaction<&mut GenesisDatabase<Self::DbDesc>>,
     ) -> anyhow::Result<()> {
-        // We always want to enable balances indexation if we're starting at genesis.
-        const BALANCES_INDEXATION_ENABLED: bool = true;
-
         let events = group.into_iter().map(|TableEntry { value, key }| {
             Cow::Owned(Event::CoinCreated(value.uncompress(key)))
         });
