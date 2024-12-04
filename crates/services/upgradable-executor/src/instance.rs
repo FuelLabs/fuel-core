11e8257acd31e0287ce155af326cf2fb82fdcabc
green <xgreenx9999@gmail.com>
2024-12-03 20:52:42 -0500

    Allow blocks with more transactions

--- crates/services/upgradable-executor/src/instance.rs
+++ crates/services/upgradable-executor/src/instance.rs
@@ -63,7 +63,7 @@ trait CallerHelper {
         &mut self,
         source: Arc<Source>,
         gas_limit: u64,
-        tx_number_limit: u16,
+        tx_number_limit: u32,
         block_transaction_size_limit: u32,
     ) -> anyhow::Result<u32>
     where
@@ -83,7 +83,7 @@ impl<'a> CallerHelper for Caller<'a, ExecutionState> {
         &mut self,
         source: Arc<Source>,
         gas_limit: u64,
-        tx_number_limit: u16,
+        tx_number_limit: u32,
         block_transaction_size_limit: u32,
     ) -> anyhow::Result<u32>
     where
@@ -241,7 +241,7 @@ impl Instance<Created> {
                 return Ok(0);
             };
 
-            caller.peek_next_txs_bytes(source, gas_limit, u16::MAX, u32::MAX)
+            caller.peek_next_txs_bytes(source, gas_limit, u32::MAX, u32::MAX)
         };
 
         Func::wrap(&mut self.store, closure)
@@ -259,10 +259,6 @@ impl Instance<Created> {
             let Some(source) = source.clone() else {
                 return Ok(0);
             };
-
-            let tx_number_limit = u16::try_from(tx_number_limit).map_err(|e| {
-                anyhow::anyhow!("The number of transactions is more than `u16::MAX`: {e}")
-            })?;
 
             caller.peek_next_txs_bytes(
                 source,
