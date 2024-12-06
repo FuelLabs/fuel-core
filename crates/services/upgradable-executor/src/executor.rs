ab12fb6772ca8a01b39203de8736933d2e17d3f8
AurelienFT <32803821+AurelienFT@users.noreply.github.com>
2024-12-06 10:27:25 +0100

    Release v0.40.2 (#2477)
    ## Version 0.40.2
    
    ### Fixed
    
    - [2476](https://github.com/FuelLabs/fuel-core/pull/2476): Hardcode the
    timestamp of the genesis block.
    
    ## What's Changed
    
    - Make genesis block timestamp hardcoded. by @AurelienFT in #2476 
    
    **Full Changelog**:
    https://github.com/FuelLabs/fuel-core/compare/v0.40.1...v0.40.2

--- crates/services/upgradable-executor/src/executor.rs
+++ crates/services/upgradable-executor/src/executor.rs
@@ -156,7 +156,8 @@ impl<S, R> Executor<S, R> {
         ("0-38-0", 14),
         ("0-39-0", 15),
         ("0-40-0", 16),
-        ("0-40-1", LATEST_STATE_TRANSITION_VERSION),
+        ("0-40-1", 17),
+        ("0-40-2", LATEST_STATE_TRANSITION_VERSION),
     ];
 
     pub fn new(
