553a14c480def16af6c606eb9fba7b6b75d024d7
Hannes Karppila <hannes.karppila@gmail.com>
2024-12-09 17:11:25 +0200

    Remove unnecessary and broken import

--- tests/tests/da_compression.rs
+++ tests/tests/da_compression.rs
@@ -23,7 +23,6 @@ use fuel_core_compression::{
     decompress::decompress,
     VersionedCompressedBlock,
 };
-use fuel_core_poa::signer::SignMode;
 use fuel_core_storage::transactional::{
     HistoricalView,
     IntoTransaction,
