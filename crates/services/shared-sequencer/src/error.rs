1d01f3cfe5efd64b86d0f135c9cdb58e1f8912b6
Hannes Karppila <hannes.karppila@gmail.com>
2024-12-09 16:32:16 +0200

    Re-add error types that somehow got removed

--- crates/services/shared-sequencer/src/error.rs
+++ crates/services/shared-sequencer/src/error.rs
@@ -1,5 +1,24 @@
 use core::fmt;
 
+use cosmrs::ErrorReport;
+
+#[derive(Debug)]
+pub struct CosmosError(pub ErrorReport);
+
+impl fmt::Display for CosmosError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "CosmosError: {:?}", self.0)
+    }
+}
+
+impl std::error::Error for CosmosError {}
+
+impl From<ErrorReport> for CosmosError {
+    fn from(e: ErrorReport) -> Self {
+        Self(e)
+    }
+}
+
 #[derive(Debug)]
 pub struct PostBlobError {
     pub message: String,
