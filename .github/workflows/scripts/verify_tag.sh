9f96dec21195fc155f8359ebd58eda8217178491
Jack Drogon <jack.xsuperman@gmail.com>
2024-12-27 21:42:44 +0800

    fix typo
    Signed-off-by: Jack Drogon <jack.xsuperman@gmail.com>

--- .github/workflows/scripts/verify_tag.sh
+++ .github/workflows/scripts/verify_tag.sh
@@ -23,7 +23,7 @@ if [ -z "$MANIFEST" ]; then
     exit 1
 fi
 
-# strip preceeding 'v' if it exists on tag
+# strip preceding 'v' if it exists on tag
 REF=${REF/#v}
 TOML_VERSION=$(cat $MANIFEST | dasel -r toml 'workspace.package.version')
 
