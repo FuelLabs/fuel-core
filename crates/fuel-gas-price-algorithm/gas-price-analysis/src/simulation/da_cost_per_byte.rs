c909cb400a049faa17b89c9f0aa1487c546c068e
Rafał Chabowski <rafal.chabowski@fuel.sh>
2025-01-02 10:50:26 +0100

    Add some clarifying comments

--- crates/fuel-gas-price-algorithm/gas-price-analysis/src/simulation/da_cost_per_byte.rs
+++ crates/fuel-gas-price-algorithm/gas-price-analysis/src/simulation/da_cost_per_byte.rs
@@ -21,7 +21,7 @@ pub fn get_da_cost_per_byte_from_source(
         Source::Predefined {
             file_path,
             sample_size,
-        } => get_costs_from_csv_file::<PredefinedRecord>(
+        } => get_l1_costs_from_csv_file::<PredefinedRecord>(
             &file_path,
             sample_size,
             PREDEFINED_L2_BLOCKS_PER_L1_BLOCK,
@@ -29,7 +29,7 @@ pub fn get_da_cost_per_byte_from_source(
         Source::Predefined2 {
             file_path,
             l2_blocks_per_blob,
-        } => get_costs_from_csv_file::<Predefined2Record>(
+        } => get_l1_costs_from_csv_file::<Predefined2Record>(
             &file_path,
             None,
             l2_blocks_per_blob,
@@ -94,7 +94,24 @@ where
     fields.iter().map(|f| f.name.clone()).collect()
 }
 
-fn get_costs_from_csv_file<T>(
+// This function is used to read the CSV file and extract the blob fee from it.
+// The `sample_size` and `l2_blocks_per_blob` parameters are used to tailor the
+// behavior to the slightly different approaches used in the two different CSV files.
+//
+// "Predefined" CSV file format (aka legacy):
+// 1. `sample_size` represents the number of L1 blocks we want to simulate.
+// 2. we assume that there are 12 L2 blocks per L1 block.
+// 3. because each row in the file is a different L1 block.
+// 4. we need to pull `sample_size` / 12 records from the CSV file
+// 5. for example, for sample size 1200, algorithm assumes 1200 L1 blocks and pulls 100 L1 blocks from the file
+//
+// "Predefined2" CSV file format:
+// 1. `sample_size` is not used - we always pull all data from the input file
+// 2. `l2_blocks_per_blob` is used to determine how many L2 blocks are in a single L1 blob (no assumptions)
+// 3. each row in the file is a different L2 block, hence L1 entries are repeated
+// 4. so we need to skip `l2_blocks_per_blob` records to get to the next L1 block
+// 5. for example, for l2_blocks_per_blob = 12, algorithm takes 1 value, skips 11, takes another value, etc.
+fn get_l1_costs_from_csv_file<T>(
     file_path: &str,
     sample_size: Option<usize>,
     l2_blocks_per_blob: usize,
