3522efc84111c93b93ccffda6718b697554371d5
Rafał Chabowski <rafal.chabowski@fuel.sh>
2025-01-13 19:00:50 +0100

    Update the complexity spec for `balances` endpoint

--- crates/fuel-core/src/schema/balance.rs
+++ crates/fuel-core/src/schema/balance.rs
@@ -81,8 +81,8 @@ impl BalanceQuery {
 
     #[graphql(
         complexity = "query_costs().balance_query + query_costs().storage_iterator \
-        + (query_costs().storage_read + first.unwrap_or_default() as usize) * child_complexity \
-        + (query_costs().storage_read + last.unwrap_or_default() as usize) * child_complexity"
+        + (query_costs().storage_read * first.unwrap_or_default() as usize) * child_complexity \
+        + (query_costs().storage_read * last.unwrap_or_default() as usize) * child_complexity"
     )]
     async fn balances(
         &self,
