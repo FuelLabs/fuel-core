44d6d792f14cbfdffde3fdbfacae000fc7e5b6f0
Mårten Blankfors <marten@blankfors.se>
2025-01-16 16:39:16 +0100

    chore: Remove unused crate dependencies

--- crates/fraud_proofs/global_merkle_root/storage/src/lib.rs
+++ crates/fraud_proofs/global_merkle_root/storage/src/lib.rs
@@ -1,9 +1,8 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 #![deny(clippy::arithmetic_side_effects)]
 #![deny(clippy::cast_possible_truncation)]
-// #![deny(unused_crate_dependencies)]
-// #![deny(missing_docs)]
-// #![deny(warnings)]
+#![deny(unused_crate_dependencies)]
+#![deny(warnings)]
 
 #[cfg(feature = "alloc")]
 extern crate alloc;
