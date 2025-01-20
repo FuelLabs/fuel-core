dbc5cdc9ab10715817017f2a50fe46f3c1d371dc
acerone85 <andrea.cerone@gmail.com>
2025-01-20 22:21:01 +0000

    Fix other tests

--- tests/tests/required_fuel_block_height_header.rs
+++ tests/tests/required_fuel_block_height_header.rs
@@ -52,7 +52,8 @@ async fn request_with_required_block_height_extension_field_works() {
 }
 
 #[tokio::test]
-async fn current_fuel_block_height_header_is_present_on_successful_request() {
+async fn current_fuel_block_height_header_is_present_on_failed_request() {
+    // TODO: Figure out a way to get the current fuel block height from FuelClient queries
     let query = r#"{ "query": "{ contract(id:\"0x7e2becd64cd598da59b4d1064b711661898656c6b1f4918a787156b8965dc83c\") { id bytecode } }", "extensions": {"required_fuel_block_height": 100} }"#;
 
     // setup config
@@ -77,12 +78,72 @@ async fn current_fuel_block_height_header_is_present_on_successful_request() {
     let response = client.execute(request).await.unwrap();
 
     assert!(response.status() == StatusCode::OK);
-    let response_body = response.text().await.unwrap();
-    println!("{response_body:?}");
+    let response_body: serde_json::Value = response.json().await.unwrap();
+
+    let is_failed_request = response_body.as_object().unwrap().get("errors").is_some();
+    assert!(is_failed_request);
+
+    let current_fuel_block_height = response_body
+        .as_object()
+        .unwrap()
+        .get("extensions")
+        .unwrap()
+        .as_object()
+        .unwrap()
+        .get("current_fuel_block_height")
+        .unwrap()
+        .as_u64()
+        .unwrap();
+    assert_eq!(current_fuel_block_height, 0);
 }
 
 #[tokio::test]
-async fn current_fuel_block_height_header_is_present_on_failed_request() {
+async fn current_fuel_block_height_header_is_present_on_successful_request() {
+    // TODO: Figure out a way to get the current fuel block height from FuelClient queries
+    let query = r#"{ "query": "{ contract(id:\"0x7e2becd64cd598da59b4d1064b711661898656c6b1f4918a787156b8965dc83c\") { id bytecode } }", "extensions": {"required_fuel_block_height": 0} }"#;
+
+    // setup config
+    let state_config = StateConfig::default();
+    let config = Config::local_node_with_state_config(state_config);
+
+    // setup server & client
+    let srv = FuelService::new_node(config).await.unwrap();
+    let url: reqwest::Url = format!("http://{}/v1/graphql", srv.bound_address)
+        .as_str()
+        .parse()
+        .unwrap();
+
+    let client = reqwest::Client::new();
+
+    let request = client
+        .post(url)
+        .body(query)
+        .header(CONTENT_TYPE, "application/json")
+        .build()
+        .unwrap();
+    let response = client.execute(request).await.unwrap();
+
+    assert!(response.status() == StatusCode::OK);
+    let response_body: serde_json::Value = response.json().await.unwrap();
+    let is_successful = response_body.as_object().unwrap().get("errors").is_none();
+    assert!(is_successful);
+    let current_fuel_block_height = response_body
+        .as_object()
+        .unwrap()
+        .get("extensions")
+        .unwrap()
+        .as_object()
+        .unwrap()
+        .get("current_fuel_block_height")
+        .unwrap()
+        .as_u64()
+        .unwrap();
+    assert_eq!(current_fuel_block_height, 0);
+}
+
+#[tokio::test]
+async fn current_fuel_block_height_header_is_present_on_no_required_fuel_block_height() {
+    // TODO: Figure out a way to get the current fuel block height from FuelClient queries
     let query = r#"{ "query": "{ contract(id:\"0x7e2becd64cd598da59b4d1064b711661898656c6b1f4918a787156b8965dc83c\") { id bytecode } }" }"#;
 
     // setup config
@@ -101,12 +162,23 @@ async fn current_fuel_block_height_header_is_present_on_failed_request() {
     let request = client
         .post(url)
         .body(query)
-        .header("REQUIRED_FUEL_BLOCK_HEIGHT", "100")
         .header(CONTENT_TYPE, "application/json")
         .build()
         .unwrap();
     let response = client.execute(request).await.unwrap();
 
-    assert!(response.status() == StatusCode::PRECONDITION_FAILED);
-    assert!(response.headers().contains_key("CURRENT_FUEL_BLOCK_HEIGHT"));
+    assert!(response.status() == StatusCode::OK);
+    let response_body: serde_json::Value = response.json().await.unwrap();
+    let current_fuel_block_height = response_body
+        .as_object()
+        .unwrap()
+        .get("extensions")
+        .unwrap()
+        .as_object()
+        .unwrap()
+        .get("current_fuel_block_height")
+        .unwrap()
+        .as_u64()
+        .unwrap();
+    assert_eq!(current_fuel_block_height, 0);
 }
