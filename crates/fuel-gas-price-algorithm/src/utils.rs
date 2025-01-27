d7e48d1823021dd3cd570c0f6410d1fc992da066
Aaryamann Challani <43716372+rymnc@users.noreply.github.com>
2025-01-28 05:10:10 +0530

    fix: 22% speedup on cumulative pct change

--- crates/fuel-gas-price-algorithm/src/utils.rs
+++ crates/fuel-gas-price-algorithm/src/utils.rs
@@ -1,4 +1,5 @@
 #[allow(clippy::cast_possible_truncation)]
+#[inline]
 pub(crate) fn cumulative_percentage_change(
     new_exec_price: u64,
     for_height: u32,
@@ -7,7 +8,10 @@ pub(crate) fn cumulative_percentage_change(
 ) -> u64 {
     let blocks = height.saturating_sub(for_height) as f64;
     let percentage_as_decimal = percentage as f64 / 100.0;
-    let multiple = (1.0f64 + percentage_as_decimal).powf(blocks);
+    // optimized version of `f64::powf(1.0 + percentage_as_decimal, blocks)`
+    // x^y = e^(y*ln(x))
+    // we gain around 22%
+    let multiple = f64::exp(blocks * f64::ln(1.0 + percentage_as_decimal));
     let mut approx = new_exec_price as f64 * multiple;
     // account for rounding errors and take a slightly higher value
     const ROUNDING_ERROR_CUTOFF: f64 = 16948547188989277.0;
