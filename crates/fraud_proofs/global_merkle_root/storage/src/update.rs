a05d94c245f3728ced345c9c8f64739e7cfda101
Andrea Cerone <22031682+acerone85@users.noreply.github.com>
2025-02-03 19:41:09 +0100

    Update crates/fraud_proofs/global_merkle_root/storage/src/update.rs
    Co-authored-by: Mårten Blankfors <marten@blankfors.se>

--- crates/fraud_proofs/global_merkle_root/storage/src/update.rs
+++ crates/fraud_proofs/global_merkle_root/storage/src/update.rs
@@ -193,9 +193,7 @@ where
             .outputs()
             .iter()
             .filter(|output| matches!(output, Output::ContractCreated { .. }))
-            .take(1)
-            .collect::<Vec<&Output>>()
-            .first()
+            .next()
         else {
             anyhow::bail!("Create transaction does not have contract created output")
         };
