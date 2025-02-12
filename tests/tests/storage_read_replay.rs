684e8cc5bb8d5b9dfac7f74ec27f313920f51c3e
Hannes Karppila <hannes.karppila@gmail.com>
2025-02-12 15:16:34 +0200

    Change the test to use column number

--- tests/tests/storage_read_replay.rs
+++ tests/tests/storage_read_replay.rs
@@ -44,7 +44,7 @@ async fn storage_read_replay__returns_counter_state() {
 
         let storage_bytes = replay
             .iter()
-            .find(|item| item.column == "ContractsState" && item.key == storage_slot_key)
+            .find(|item| item.column == Column::ContractsState && item.key == storage_slot_key)
             .expect("No storage read found")
             .value
             .clone()
