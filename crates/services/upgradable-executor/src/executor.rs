a41cdf1290d6973f31f71687a845ba2206ad5155
AurelienFT <aurelien.foucault@epitech.eu>
2025-02-13 12:04:05 +0100

    Port the update of versions

--- crates/services/upgradable-executor/src/executor.rs
+++ crates/services/upgradable-executor/src/executor.rs
@@ -171,7 +171,8 @@ impl<S, R> Executor<S, R> {
         ("0-40-2", 18),
         ("0-41-4", 19),
         ("0-41-5", 20),
-        ("0-41-6", LATEST_STATE_TRANSITION_VERSION),
+        ("0-41-6", 21),
+        ("0-41-7", LATEST_STATE_TRANSITION_VERSION),
     ];
 
     pub fn new(
