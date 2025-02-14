a15f9e3763c2cb5217ee86c20f40600b7413bbea
rymnc <43716372+rymnc@users.noreply.github.com>
2025-02-14 15:54:11 +0530

    fault_proving(merklized_registry): add merklized registry tables

---
+++ crates/fuel-core/src/graphql_api/storage/da_compression/v2.rs
@@ -0,0 +1,11 @@
+pub mod address;
+pub mod asset_id;
+pub mod contract_id;
+pub mod predicate_code;
+pub mod script_code;
+
+pub use address::*;
+pub use asset_id::*;
+pub use contract_id::*;
+pub use predicate_code::*;
+pub use script_code::*;
