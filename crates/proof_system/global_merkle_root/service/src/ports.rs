22d417322dd985cf6d9fea0d176d4fc4d95c1d80
Mårten Blankfors <marten@blankfors.se>
2025-02-19 11:38:08 +0100

    fault_proving(global_roots): service definition and initial tests (#2722)

---
+++ crates/proof_system/global_merkle_root/service/src/ports.rs
@@ -0,0 +1,22 @@
+use std::future::Future;
+
+use fuel_core_global_merkle_root_storage::column::Column;
+use fuel_core_storage::{
+    kv_store::KeyValueInspect,
+    transactional::Modifiable,
+};
+use fuel_core_types::blockchain::block::Block;
+
+/// A stream of blocks
+pub trait BlockStream {
+    /// Error type
+    type Error;
+
+    /// Get the next block
+    fn next(&mut self) -> impl Future<Output = Result<Block, Self::Error>> + Send;
+}
+
+/// The storage requirements for the merkle root service
+pub trait ServiceStorage: KeyValueInspect<Column = Column> + Modifiable {}
+
+impl<T> ServiceStorage for T where T: KeyValueInspect<Column = Column> + Modifiable {}
