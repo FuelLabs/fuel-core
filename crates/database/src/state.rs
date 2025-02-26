6ef2074d3216def37bb96d0600e12ae519eb7fb2
Mårten Blankfors <marten@blankfors.se>
2025-02-26 14:19:35 +0100

    wip: Added iterable key value view and key value view modules

--- crates/database/src/state.rs
+++ crates/database/src/state.rs
@@ -3,10 +3,10 @@ use crate::database_description::DatabaseDescription;
 // pub mod data_source;
 // pub mod generic_database;
 //#[cfg(feature = "rocksdb")]
-pub mod historical_rocksdb;
+// pub mod historical_rocksdb;
 // pub mod in_memory;
-// pub mod iterable_key_value_view;
-// pub mod key_value_view;
+pub mod iterable_key_value_view;
+pub mod key_value_view;
 //#[cfg(feature = "rocksdb")]
 pub mod rocks_db;
 //#[cfg(feature = "rocksdb")]
