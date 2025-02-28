0440743f681930dc97156588812262db6546587b
Mitch Turner <james.mitchell.turner@gmail.com>
2025-02-27 17:52:02 -0700

    Add another test, refactor

--- crates/fuel-core/src/service/adapters/consensus_module/poa/pre_confirmation_signature.rs
+++ crates/fuel-core/src/service/adapters/consensus_module/poa/pre_confirmation_signature.rs
@@ -9,51 +9,10 @@ use fuel_core_types::tai64::Tai64;
 use std::time::Duration;
 
 #[cfg(test)]
-mod trigger {
-    #![allow(non_snake_case)]
-    use super::*;
-
-    struct FakeTime {
-        now: Tai64,
-    }
-
-    impl FakeTime {
-        fn new(now: u64) -> Self {
-            let now = Tai64(now);
-            Self { now }
-        }
-    }
-
-    impl GetTime for FakeTime {
-        fn now(&self) -> Tai64 {
-            self.now
-        }
-    }
-
-    #[tokio::test]
-    async fn next_rotation__triggers_at_expected_time() {
-        tokio::time::pause();
-        // given
-        let rotation_interval = 10;
-        let rotation_interval_duration = Duration::from_secs(rotation_interval);
-        let fake_time = FakeTime::new(50);
-
-        let mut trigger = TimeBasedTrigger::new(fake_time, rotation_interval_duration);
-
-        // when
-        let mut fut = tokio_test::task::spawn(trigger.next_rotation());
-        tokio_test::assert_pending!(fut.poll());
-
-        // then
-        let advance_time = Duration::from_secs(rotation_interval + 1);
-        tokio::time::advance(advance_time).await;
-        tokio_test::assert_ready!(fut.poll()).expect("should trigger");
-    }
-}
+mod trigger;
 
 pub struct TimeBasedTrigger<Time> {
     time: Time,
-    // next_rotation: Mutex<Tai64>,
     next_rotation: Tai64,
     rotation_interval: Duration,
 }
@@ -92,12 +51,16 @@ impl<Time: GetTime + Send> KeyRotationTrigger for TimeBasedTrigger<Time> {
         let next_rotation = self.get_next_rotation()?;
         let time_to_wait = duration_between(self.time.now(), next_rotation);
         tokio::time::sleep(time_to_wait).await;
+        tracing::debug!(
+            "next rotation triggered after waiting {:?}",
+            time_to_wait.as_secs()
+        );
         let new_next_rotation = self
-            .time
-            .now()
+            .next_rotation
             .0
             .saturating_add(self.rotation_interval.as_secs());
         self.set_next_rotation(Tai64(new_next_rotation))?;
+        tracing::debug!("next rotation set to {:?}", self.next_rotation);
         Ok(())
     }
 }
---
+++ crates/fuel-core/src/service/adapters/consensus_module/poa/pre_confirmation_signature/trigger.rs
@@ -0,0 +1,69 @@
+#![allow(non_snake_case)]
+use super::*;
+use tokio::time::Instant;
+
+#[derive(Clone)]
+struct TokioTime {
+    start: Instant,
+}
+
+impl TokioTime {
+    pub fn new() -> Self {
+        let start = Instant::now();
+        Self { start }
+    }
+}
+
+impl GetTime for TokioTime {
+    fn now(&self) -> Tai64 {
+        let now = Instant::now();
+        let elapsed = now.duration_since(self.start);
+        Tai64(elapsed.as_secs())
+    }
+}
+
+#[tokio::test]
+async fn next_rotation__triggers_at_expected_time() {
+    tokio::time::pause();
+    // given
+    let rotation_interval = 10;
+    let rotation_interval_duration = Duration::from_secs(rotation_interval);
+    let fake_time = TokioTime::new();
+
+    let mut trigger = TimeBasedTrigger::new(fake_time, rotation_interval_duration);
+
+    // when
+    let mut fut = tokio_test::task::spawn(trigger.next_rotation());
+    tokio_test::assert_pending!(fut.poll());
+
+    // then
+    let advance_time = Duration::from_secs(rotation_interval + 1);
+    tokio::time::advance(advance_time).await;
+    tokio_test::assert_ready!(fut.poll()).expect("should trigger");
+}
+
+#[tokio::test]
+async fn next_rotation__subsequent_triggers_are_same_interval() {
+    tokio::time::pause();
+    // given
+    let rotation_interval = 10;
+    let rotation_interval_duration = Duration::from_secs(rotation_interval);
+    let fake_time = TokioTime::new();
+    let fake_time_2 = TokioTime::new();
+
+    let mut trigger = TimeBasedTrigger::new(fake_time, rotation_interval_duration);
+
+    let advance_time = rotation_interval + 1;
+    let advance_time_duration = Duration::from_secs(advance_time);
+    tokio::time::advance(advance_time_duration).await;
+    trigger.next_rotation().await.unwrap();
+
+    // when
+    let mut fut = tokio_test::task::spawn(trigger.next_rotation());
+    tokio_test::assert_pending!(fut.poll());
+
+    // then
+    let advance_time = Duration::from_secs(rotation_interval);
+    tokio::time::advance(advance_time).await;
+    tokio_test::assert_ready!(fut.poll()).expect("should trigger");
+}
