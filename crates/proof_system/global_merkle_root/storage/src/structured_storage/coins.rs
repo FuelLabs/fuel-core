9833d2d6da257205133f69ebdc9ec14d5bbfe542
Mårten Blankfors <marten@blankfors.se>
2025-03-05 15:12:47 +0100

    refactor: Dedicated table definitions for the state root service

--- crates/proof_system/global_merkle_root/storage/src/structured_storage/coins.rs
+++ crates/proof_system/global_merkle_root/storage/src/structured_storage/coins.rs
@@ -1,8 +1,18 @@
+use fuel_core_storage::{
+    blueprint::plain::Plain,
+    codec::{
+        postcard::Postcard,
+        primitive::Primitive,
+    },
+    merkle::column::MerkleizedColumn,
+    structured_storage::TableWithBlueprint,
+};
+
 use crate::{
     column::TableColumn,
     merkle::MerkleizedTableColumn,
+    tables::Coins,
 };
-use fuel_core_storage::tables::Coins;
 
 impl MerkleizedTableColumn for Coins {
     type TableColumn = TableColumn;
@@ -11,3 +21,19 @@ impl MerkleizedTableColumn for Coins {
         TableColumn::Coins
     }
 }
+
+impl TableWithBlueprint for Coins {
+    type Blueprint = Plain<Primitive<34>, Postcard>;
+    type Column = MerkleizedColumn<TableColumn>;
+
+    fn column() -> Self::Column {
+        MerkleizedColumn::TableColumn(TableColumn::Coins)
+    }
+}
+
+#[cfg(test)]
+fuel_core_storage::basic_storage_tests!(
+    Coins,
+    <Coins as fuel_core_storage::Mappable>::Key::default(),
+    <Coins as fuel_core_storage::Mappable>::Value::default()
+);
