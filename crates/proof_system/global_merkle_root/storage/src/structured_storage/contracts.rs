81e331ca158ec6966229eece43d073037107b77a
Mårten Blankfors <marten@blankfors.se>
2025-03-05 15:12:47 +0100

    refactor: Dedicated table definitions for the state root service

--- crates/proof_system/global_merkle_root/storage/src/structured_storage/contracts.rs
+++ crates/proof_system/global_merkle_root/storage/src/structured_storage/contracts.rs
@@ -1,10 +1,20 @@
+use fuel_core_storage::{
+    blueprint::plain::Plain,
+    codec::{
+        postcard::Postcard,
+        raw::Raw,
+    },
+    merkle::column::MerkleizedColumn,
+    structured_storage::TableWithBlueprint,
+};
+
 use crate::{
     column::TableColumn,
     merkle::MerkleizedTableColumn,
-};
-use fuel_core_storage::tables::{
-    ContractsLatestUtxo,
-    ContractsRawCode,
+    tables::{
+        ContractsLatestUtxo,
+        ContractsRawCode,
+    },
 };
 
 impl MerkleizedTableColumn for ContractsRawCode {
@@ -21,4 +31,40 @@ impl MerkleizedTableColumn for ContractsLatestUtxo {
     fn table_column() -> TableColumn {
         TableColumn::ContractsLatestUtxo
     }
+}
+
+impl TableWithBlueprint for ContractsRawCode {
+    type Blueprint = Plain<Raw, Raw>;
+    type Column = MerkleizedColumn<TableColumn>;
+
+    fn column() -> Self::Column {
+        MerkleizedColumn::TableColumn(TableColumn::ContractsRawCode)
+    }
+}
+
+impl TableWithBlueprint for ContractsLatestUtxo {
+    type Blueprint = Plain<Raw, Postcard>;
+    type Column = MerkleizedColumn<TableColumn>;
+
+    fn column() -> Self::Column {
+        MerkleizedColumn::TableColumn(TableColumn::ContractsLatestUtxo)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    fuel_core_storage::basic_storage_tests!(
+        ContractsRawCode,
+        <ContractsRawCode as fuel_core_storage::Mappable>::Key::from([1u8; 32]),
+        vec![32u8],
+        <ContractsRawCode as fuel_core_storage::Mappable>::OwnedValue::from(vec![32u8])
+    );
+
+    fuel_core_storage::basic_storage_tests!(
+        ContractsLatestUtxo,
+        <ContractsLatestUtxo as fuel_core_storage::Mappable>::Key::from([1u8; 32]),
+        <ContractsLatestUtxo as fuel_core_storage::Mappable>::Value::default()
+    );
 }
