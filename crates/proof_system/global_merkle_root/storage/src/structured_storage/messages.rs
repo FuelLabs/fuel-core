5838e55b51541512ab872b9815d4f4a096ce1462
rymnc <43716372+rymnc@users.noreply.github.com>
2025-03-05 12:12:38 +0530

    fixup

--- crates/proof_system/global_merkle_root/storage/src/structured_storage/messages.rs
+++ crates/proof_system/global_merkle_root/storage/src/structured_storage/messages.rs
@@ -5,6 +5,8 @@ use crate::{
 use fuel_core_storage::tables::Messages;
 
 impl MerkleizedTableColumn for Messages {
+    type TableColumn = TableColumn;
+
     fn table_column() -> TableColumn {
         TableColumn::Messages
     }
