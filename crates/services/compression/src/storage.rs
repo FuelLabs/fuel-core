228b2e828ac5c368cc635bb81c6624f7a4164839
rymnc <43716372+rymnc@users.noreply.github.com>
2025-03-07 19:16:36 +0530

    fault_proving(compression): add table definitions reusing new storage trait impls

--- crates/services/compression/src/storage.rs
+++ crates/services/compression/src/storage.rs
@@ -1,1 +1,45 @@
+use fuel_core_storage::merkle::sparse::Merkleized;
 
+/// Generates a random RegistryKey
+#[cfg(feature = "test-helpers")]
+#[allow(clippy::arithmetic_side_effects)]
+pub fn generate_key(
+    rng: &mut impl rand::Rng,
+) -> fuel_core_types::fuel_compression::RegistryKey {
+    let raw_key: u32 = rng.gen_range(0..2u32.pow(24) - 2);
+    fuel_core_types::fuel_compression::RegistryKey::try_from(raw_key).unwrap()
+}
+
+pub mod address;
+pub mod asset_id;
+pub mod column;
+pub mod compressed_blocks;
+pub mod contract_id;
+pub mod evictor_cache;
+pub mod predicate_code;
+pub mod registry_index;
+pub mod script_code;
+
+/// Merkleized Address table type alias
+pub type Address = Merkleized<address::Address>;
+
+/// Merkleized AssetId table type alias
+pub type AssetId = Merkleized<asset_id::AssetId>;
+
+/// Merkleized ContractId table type alias
+pub type ContractId = Merkleized<contract_id::ContractId>;
+
+/// Merkleized EvictorCache table type alias
+pub type EvictorCache = Merkleized<evictor_cache::EvictorCache>;
+
+/// Merkleized PredicateCode table type alias
+pub type PredicateCode = Merkleized<predicate_code::PredicateCode>;
+
+/// Merkleized ScriptCode table type alias
+pub type ScriptCode = Merkleized<script_code::ScriptCode>;
+
+/// Merkleized RegistryIndex table type alias
+pub type RegistryIndex = Merkleized<registry_index::RegistryIndex>;
+
+/// Re-export to match api
+pub use compressed_blocks::CompressedBlocks;
---
+++ crates/services/compression/src/storage/asset_id.rs
@@ -0,0 +1,62 @@
+//! Asset ID table.
+
+use fuel_core_storage::{
+    blueprint::plain::Plain,
+    codec::{
+        postcard::Postcard,
+        raw::Raw,
+    },
+    merkle::{
+        column::MerkleizedColumn,
+        sparse::MerkleizedTableColumn,
+    },
+    structured_storage::TableWithBlueprint,
+    Mappable,
+};
+use fuel_core_types::fuel_compression::RegistryKey;
+
+use super::column::{
+    CompressionColumn,
+    MerkleizedColumnOf,
+};
+
+/// Table that indexes the asset ids.
+pub struct AssetId;
+
+impl MerkleizedTableColumn for AssetId {
+    type TableColumn = CompressionColumn;
+
+    fn table_column() -> Self::TableColumn {
+        Self::TableColumn::AssetId
+    }
+}
+
+impl Mappable for AssetId {
+    type Key = Self::OwnedKey;
+    type OwnedKey = RegistryKey;
+    type Value = Self::OwnedValue;
+    type OwnedValue = fuel_core_types::fuel_tx::AssetId;
+}
+
+impl TableWithBlueprint for AssetId {
+    type Blueprint = Plain<Postcard, Raw>;
+    type Column = MerkleizedColumnOf<Self>;
+
+    fn column() -> Self::Column {
+        Self::Column::TableColumn(Self::table_column())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use fuel_core_compression_service::storage::generate_key;
+
+    fuel_core_storage::basic_storage_tests!(
+        AssetId,
+        RegistryKey::ZERO,
+        <AssetId as Mappable>::Value::default(),
+        <AssetId as Mappable>::Value::default(),
+        generate_key
+    );
+}
