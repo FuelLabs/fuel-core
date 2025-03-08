74a8b528737c2d07b84a272bd04bc9bdffdb4f1c
rymnc <43716372+rymnc@users.noreply.github.com>
2025-03-09 04:40:50 +0530

    fix: clippy

--- crates/services/src/service.rs
+++ crates/services/src/service.rs
@@ -359,7 +359,7 @@ async fn run<S>(
     let mut task = service
         .into_task(&state, params)
         .await
-        .expect(&format!("The initialization of {} failed", S::NAME));
+        .unwrap_or_else(|_| panic!("The initialization of {} failed", S::NAME));
 
     sender.send_if_modified(|s| {
         if s.starting() {
