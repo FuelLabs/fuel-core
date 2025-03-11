3c70ca14c1cd8675a4c13bfbf8eb34c58ff4ef88
Mårten Blankfors <marten@blankfors.se>
2025-03-11 18:08:09 +0100

    fix: Re-enable lints

--- crates/proof_system/global_merkle_root/app/src/lib.rs
+++ crates/proof_system/global_merkle_root/app/src/lib.rs
@@ -1,10 +1,10 @@
 //! The CLI and adapters for the state root service
 
-// #![deny(clippy::arithmetic_side_effects)]
-// #![deny(clippy::cast_possible_truncation)]
-// #![deny(unused_crate_dependencies)]
-// #![deny(missing_docs)]
-// #![deny(warnings)]
+#![deny(clippy::arithmetic_side_effects)]
+#![deny(clippy::cast_possible_truncation)]
+#![deny(unused_crate_dependencies)]
+#![deny(missing_docs)]
+#![deny(warnings)]
 
 /// Adapters
 pub mod adapters;
