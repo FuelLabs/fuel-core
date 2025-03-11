1e27960cdb4f4c5ac929d0577de3b051eab792bf
Mitch Turner <james.mitchell.turner@gmail.com>
2025-03-11 16:04:21 -0600

    Add basic trait for adding delegate key

--- crates/services/tx_status_manager/src/service.rs
+++ crates/services/tx_status_manager/src/service.rs
@@ -35,6 +35,7 @@ use fuel_core_types::{
     },
 };
 use futures::StreamExt;
+use std::future::Future;
 use tokio::sync::{
     mpsc,
     oneshot,
@@ -96,32 +97,48 @@ impl SharedData {
     }
 }
 
-pub struct Task {
+pub struct Task<T> {
     manager: TxStatusManager,
     subscriptions: Subscriptions,
     read_requests_receiver: mpsc::Receiver<ReadRequest>,
     write_requests_receiver: mpsc::UnboundedReceiver<WriteRequest>,
     shared_data: SharedData,
+    signature_verification: T,
 }
 
-pub trait SignatureVerification {
+#[allow(dead_code)]
+/// Interface for signature verification of preconfirmations
+pub trait SignatureVerification: Send {
+    /// Adds a new delegate signature to verify the preconfirmations
     fn add_new_delegate(
         &mut self,
         delegate: DelegatePublicKey,
         protocol_signature: ProtocolSignature,
-    ) -> bool;
+    ) -> impl Future<Output = bool> + Send;
 }
 
-impl Task {
+impl<T: SignatureVerification> Task<T> {
     // TODO: Implement signatures verifications logic for preconfirmation logic.
     //  https://github.com/FuelLabs/fuel-core/issues/2823
-    fn new_preconfirmations_from_p2p(
+    async fn new_preconfirmations_from_p2p(
         &mut self,
         preconfirmations: P2PPreConfirmationMessage,
     ) {
         match preconfirmations {
-            PreConfirmationMessage::Delegate(_) => {}
+            PreConfirmationMessage::Delegate(sealed) => {
+                tracing::debug!(
+                    "Received new delegate signature from peer: {:?}",
+                    sealed.entity.public_key
+                );
+                let Sealed { signature, entity } = sealed;
+                let delegate_key = entity.public_key;
+                let _ = self
+                    .signature_verification
+                    .add_new_delegate(delegate_key, signature)
+                    .await;
+            }
             PreConfirmationMessage::Preconfirmations(sealed) => {
+                tracing::debug!("Received new preconfirmations from peer");
                 let Sealed {
                     signature: _,
                     entity,
@@ -138,7 +155,7 @@ impl Task {
 }
 
 #[async_trait::async_trait]
-impl RunnableService for Task {
+impl<T: SignatureVerification> RunnableService for Task<T> {
     const NAME: &'static str = "TxStatusManagerTask";
     type SharedData = SharedData;
     type Task = Self;
@@ -157,7 +174,7 @@ impl RunnableService for Task {
     }
 }
 
-impl RunnableTask for Task {
+impl<T: SignatureVerification> RunnableTask for Task<T> {
     async fn run(&mut self, watcher: &mut StateWatcher) -> TaskNextAction {
         tokio::select! {
             biased;
@@ -169,7 +186,7 @@ impl RunnableTask for Task {
             tx_status_from_p2p = self.subscriptions.new_tx_status.next() => {
                 if let Some(GossipData { data, .. }) = tx_status_from_p2p {
                     if let Some(msg) = data {
-                        self.new_preconfirmations_from_p2p(msg);
+                        self.new_preconfirmations_from_p2p(msg).await;
                     }
                     TaskNextAction::Continue
                 } else {
@@ -214,7 +231,11 @@ impl RunnableTask for Task {
     }
 }
 
-pub fn new_service<P2P>(p2p: P2P, config: Config) -> ServiceRunner<Task>
+pub fn new_service<P2P, Sign: SignatureVerification>(
+    p2p: P2P,
+    signature_verification: Sign,
+    config: Config,
+) -> ServiceRunner<Task<Sign>>
 where
     P2P: P2PSubscriptions<GossipedStatuses = P2PPreConfirmationGossipData>,
 {
@@ -243,15 +264,103 @@ where
         read_requests_receiver,
         write_requests_receiver,
         shared_data,
+        signature_verification,
     })
 }
 
 #[cfg(test)]
 mod tests {
+    #![allow(non_snake_case)]
+
     use super::*;
+    use crate::tests::FakeSignatureVerification;
+    use fuel_core_types::services::p2p::{
+        DelegatePreConfirmationKey,
+        Tai64,
+    };
+    use std::time::Duration;
+    use tokio_stream::wrappers::ReceiverStream;
+
+    const TTL: Duration = Duration::from_secs(4);
+
+    fn new_task_with_signature_verification<T: SignatureVerification>(
+        signature_verification: T,
+    ) -> (Task<T>, mpsc::Sender<P2PPreConfirmationGossipData>) {
+        let (read_requests_sender, read_requests_receiver) = mpsc::channel(1);
+        let (write_requests_sender, write_requests_receiver) = mpsc::unbounded_channel();
+        let shared_data = SharedData {
+            read_requests_sender,
+            write_requests_sender,
+        };
+        let (sender, receiver) = mpsc::channel(1_000);
+        let new_tx_status = Box::pin(ReceiverStream::new(receiver));
+        let subscriptions = Subscriptions { new_tx_status };
+
+        let tx_status_change = TxStatusChange::new(100, Duration::from_secs(360));
+        let tx_status_manager = TxStatusManager::new(tx_status_change, TTL);
+        let task = Task {
+            manager: tx_status_manager,
+            subscriptions,
+            read_requests_receiver,
+            write_requests_receiver,
+            shared_data,
+            signature_verification,
+        };
+        (task, sender)
+    }
+
+    fn arbitrary_delegate_signatures_message() -> P2PPreConfirmationGossipData {
+        let signature = ProtocolSignature::from_bytes([1u8; 64]);
+        let delegate_key = DelegatePublicKey::default();
+        let entity = DelegatePreConfirmationKey {
+            public_key: delegate_key,
+            expiration: Tai64(1234u64),
+        };
+        let sealed = Sealed { signature, entity };
+        let inner = P2PPreConfirmationMessage::Delegate(sealed);
+        GossipData {
+            data: Some(inner),
+            peer_id: Default::default(),
+            message_id: vec![],
+        }
+    }
 
     #[tokio::test]
     async fn run__when_receive_pre_confirmation_delegations_message_updates_delegate() {
-        todo!()
+        let _ = tracing_subscriber::fmt()
+            .with_max_level(tracing::Level::DEBUG)
+            .try_init();
+
+        // given
+        let (signature_verification, mut new_delegate_handle) =
+            FakeSignatureVerification::new_with_handles(true);
+        let (mut task, sender) =
+            new_task_with_signature_verification(signature_verification);
+        let delegate_signature_message = arbitrary_delegate_signatures_message();
+        let mut state_watcher = StateWatcher::started();
+
+        // when
+        tokio::task::spawn(async move {
+            let _ = task.run(&mut state_watcher).await;
+        });
+        sender
+            .send(delegate_signature_message.clone())
+            .await
+            .unwrap();
+        tokio::time::sleep(Duration::from_millis(100)).await;
+
+        // then
+        let (actual_delegate_key, actual_protocol_signature) =
+            new_delegate_handle.recv().await.unwrap();
+        let (expected_delegate_key, expected_protocol_signature) =
+            if let P2PPreConfirmationMessage::Delegate(sealed) =
+                delegate_signature_message.data.unwrap()
+            {
+                (sealed.entity.public_key, sealed.signature)
+            } else {
+                panic!("Expected Delegate message");
+            };
+        assert_eq!(actual_delegate_key, expected_delegate_key);
+        assert_eq!(actual_protocol_signature, expected_protocol_signature);
     }
 }
--- crates/services/tx_status_manager/src/tests/mod.rs
+++ crates/services/tx_status_manager/src/tests/mod.rs
@@ -1,5 +1,12 @@
 #![allow(non_snake_case)]
 
+use crate::service::SignatureVerification;
+use fuel_core_types::services::p2p::{
+    DelegatePublicKey,
+    ProtocolSignature,
+};
+use tokio::sync::mpsc;
+
 mod mocks;
 mod tests_e2e;
 mod tests_permits;
@@ -9,3 +16,36 @@ mod tests_subscribe;
 mod tests_update_stream_state;
 mod universe;
 mod utils;
+
+pub(crate) struct FakeSignatureVerification {
+    new_delegate_sender: mpsc::Sender<(DelegatePublicKey, ProtocolSignature)>,
+    new_delegate_response: bool,
+}
+
+impl FakeSignatureVerification {
+    pub fn new_with_handles(
+        new_delegate_response: bool,
+    ) -> (Self, mpsc::Receiver<(DelegatePublicKey, ProtocolSignature)>) {
+        let (new_delegate_sender, new_delegate_receiver) = mpsc::channel(1_000);
+        let adapter = Self {
+            new_delegate_sender,
+            new_delegate_response,
+        };
+        (adapter, new_delegate_receiver)
+    }
+}
+
+impl SignatureVerification for FakeSignatureVerification {
+    async fn add_new_delegate(
+        &mut self,
+        _delegate: DelegatePublicKey,
+        _protocol_signature: ProtocolSignature,
+    ) -> bool {
+        tracing::debug!("FakeSignatureVerification::add_new_delegate");
+        self.new_delegate_sender
+            .send((_delegate, _protocol_signature))
+            .await
+            .unwrap();
+        self.new_delegate_response
+    }
+}
