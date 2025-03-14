e09788c97ed3a1bedade4fd23bef8c9b8cc5cd55
AurelienFT <aurelien.foucault@epitech.eu>
2025-03-14 12:45:03 +0100

    Add args for preconfirmation signature service

--- bin/fuel-core/src/cli/run.rs
+++ bin/fuel-core/src/cli/run.rs
@@ -108,6 +108,8 @@ mod shared_sequencer;
 mod consensus;
 mod gas_price;
 mod graphql;
+#[cfg(feature = "p2p")]
+mod preconfirmation_signature_service;
 mod profiling;
 #[cfg(feature = "relayer")]
 mod relayer;
@@ -272,6 +274,10 @@ pub struct Command {
     #[cfg(feature = "p2p")]
     pub sync_args: p2p::SyncArgs,
 
+    #[cfg_attr(feature = "p2p", clap(flatten))]
+    #[cfg(feature = "p2p")]
+    pub pre_confirmation_signature_service_args: preconfirmation_signature_service::Args,
+
     #[cfg_attr(feature = "shared-sequencer", clap(flatten))]
     #[cfg(feature = "shared-sequencer")]
     pub shared_sequencer_args: shared_sequencer::Args,
@@ -341,6 +347,8 @@ impl Command {
             p2p_args,
             #[cfg(feature = "p2p")]
             sync_args,
+            #[cfg(feature = "p2p")]
+            pre_confirmation_signature_service_args,
             #[cfg(feature = "shared-sequencer")]
             shared_sequencer_args,
             disabled_metrics,
@@ -403,6 +411,17 @@ impl Command {
             chain_config.chain_name.clone(),
             disabled_metrics.is_enabled(Module::P2P),
         )?;
+
+        #[cfg(feature = "p2p")]
+        let preconfirmation_signature_service_config =
+            fuel_core_poa::pre_confirmation_signature_service::config::Config {
+                key_rotation_interval: *pre_confirmation_signature_service_args
+                    .key_rotation_interval,
+                key_expiration_interval: *pre_confirmation_signature_service_args
+                    .key_expiration_interval,
+                echo_delegation_interval: *pre_confirmation_signature_service_args
+                    .echo_delegation_interval,
+            };
 
         let trigger: Trigger = poa_trigger.into();
 
@@ -702,12 +721,7 @@ impl Command {
             #[cfg(feature = "p2p")]
             sync: sync_args.into(),
             #[cfg(feature = "p2p")]
-            pre_confirmation_signature_service:
-                fuel_core_poa::pre_confirmation_signature_service::config::Config {
-                    key_rotation_interval: Duration::from_secs(10),
-                    key_expiration_interval: Duration::from_secs(30),
-                    echo_delegation_interval: Duration::from_secs(1),
-                },
+            pre_confirmation_signature_service: preconfirmation_signature_service_config,
             #[cfg(feature = "shared-sequencer")]
             shared_sequencer: shared_sequencer_args.try_into()?,
             consensus_signer,
