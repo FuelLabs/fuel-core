90e63a559c721c91c3805b4e9be9838829106c39
AurelienFT <aurelien.foucault@epitech.eu>
2025-03-14 15:16:35 +0100

    format and fix test compilation

--- bin/fuel-core/src/cli/run.rs
+++ bin/fuel-core/src/cli/run.rs
@@ -276,7 +276,8 @@ pub struct Command {
 
     #[cfg_attr(feature = "p2p", clap(flatten))]
     #[cfg(feature = "p2p")]
-    pub pre_confirmation_signature_service_args: preconfirmation_signature_service::PreconfirmationArgs,
+    pub pre_confirmation_signature_service_args:
+        preconfirmation_signature_service::PreconfirmationArgs,
 
     #[cfg_attr(feature = "shared-sequencer", clap(flatten))]
     #[cfg(feature = "shared-sequencer")]
