f9f728eedca3b2f8f4b63f37119a2b95a6458525
green <xgreenx9999@gmail.com>
2025-03-21 13:00:29 -0400

    Add another test

--- crates/services/consensus_module/poa/src/service_test/trigger_tests.rs
+++ crates/services/consensus_module/poa/src/service_test/trigger_tests.rs
@@ -511,6 +511,7 @@ async fn interval_trigger_even_if_queued_tx_events() {
 async fn open_trigger__produce_blocks_in_time() {
     // Given
     let open_time = Duration::from_secs(10);
+    let quarter_of_open_time = open_time / 4;
     let offset = Duration::from_secs(1);
     let mut ctx = DefaultContext::new(Config {
         trigger: Trigger::Open { period: open_time },
@@ -519,19 +520,66 @@ async fn open_trigger__produce_blocks_in_time() {
         ..Default::default()
     })
     .await;
-    let expected_block_time = ctx.now().0.checked_add(open_time.as_secs()).unwrap();
+    time::sleep(offset).await;
+
+    for _ in 0..10 {
+        // When
+        ctx.advance_time_with_tokio();
+        time::sleep(quarter_of_open_time).await;
+        let first_quarter = ctx.block_import.try_recv();
+
+        ctx.advance_time_with_tokio();
+        time::sleep(quarter_of_open_time).await;
+        let second_quarter = ctx.block_import.try_recv();
+
+        ctx.advance_time_with_tokio();
+        time::sleep(quarter_of_open_time).await;
+        let third_quarter = ctx.block_import.try_recv();
+
+        ctx.advance_time_with_tokio();
+        time::sleep(quarter_of_open_time).await;
+        let forth_quarter = ctx.block_import.try_recv();
+
+        // Then
+        assert!(first_quarter.is_err());
+        assert!(second_quarter.is_err());
+        assert!(third_quarter.is_err());
+        assert!(forth_quarter.is_ok());
+    }
+}
+
+#[tokio::test]
+async fn open_trigger__produce_blocks_with_correct_time() {
+    // Given
+    let open_time = Duration::from_secs(10);
+    let offset = Duration::from_secs(1);
+    let mut ctx = DefaultContext::new(Config {
+        trigger: Trigger::Open { period: open_time },
+        signer: SignMode::Key(test_signing_key()),
+        metrics: false,
+        ..Default::default()
+    })
+    .await;
+    let expected_first_block_time = ctx.now().0.checked_add(open_time.as_secs()).unwrap();
+    let expected_second_block_time = expected_first_block_time
+        .checked_add(open_time.as_secs())
+        .unwrap();
 
     // When
     time::sleep(offset).await;
     ctx.advance_time_with_tokio();
-    let receive_before_closed = ctx.block_import.try_recv();
-
     time::sleep(open_time).await;
-    let receive_after_open = ctx.block_import.try_recv();
+    let first_block = ctx.block_import.try_recv();
+
+    ctx.advance_time_with_tokio();
+    time::sleep(open_time).await;
+    let second_block = ctx.block_import.try_recv();
 
     // Then
-    assert!(receive_before_closed.is_err());
-    assert!(receive_after_open.is_ok());
-    let block_time = receive_after_open.unwrap().entity.header().time();
-    assert_eq!(block_time.0, expected_block_time);
+    assert!(first_block.is_ok());
+    assert!(second_block.is_ok());
+    let first_block_time = first_block.unwrap().entity.header().time();
+    let second_block_time = second_block.unwrap().entity.header().time();
+    assert_eq!(first_block_time.0, expected_first_block_time);
+    assert_eq!(second_block_time.0, expected_second_block_time);
 }
