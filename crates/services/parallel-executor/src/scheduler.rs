312cf7fee2a4fdd458e30997a7f1af9f8b13f381
AurelienFT <aurelien.foucault@epitech.eu>
2025-04-25 15:43:45 +0200

    Define all structure and initialisation of the scheduler

--- crates/services/parallel-executor/src/scheduler.rs
+++ crates/services/parallel-executor/src/scheduler.rs
@@ -1,11 +1,14 @@
-use std::collections::{
-    HashMap,
-    HashSet,
+use std::{
+    collections::HashSet,
+    time::Duration,
 };
 
+use ::futures::{
+    StreamExt,
+    stream::FuturesUnordered,
+};
 use fuel_core_storage::transactional::StorageChanges;
 use fuel_core_types::fuel_tx::ContractId;
-use fuel_core_upgradable_executor::executor::Executor as UpgradableExecutor;
 use tokio::runtime::Runtime;
 
 use crate::ports::{
@@ -34,9 +37,16 @@ use crate::ports::{
 /// If we have a transaction that end up being skipped (only possible cause if consensus parameters changes) then we will have to
 /// fallback a sequential execution of the transaction that used the skipped one as a dependency.
 
-type WorkerID = usize;
+pub struct Config {
+    block_gas_limit: u64,
+    max_execution_time: Duration,
+    block_transaction_size_limit: u32,
+    block_transaction_count_limit: u16,
+}
 
 pub struct Scheduler<TxSource> {
+    /// Config
+    config: Config,
     /// The state of each worker
     workers_state: Vec<WorkerState>,
     /// The list of contracts that are currently being executed for each worker (useful to filter them when asking to transaction source)
@@ -45,10 +55,12 @@ pub struct Scheduler<TxSource> {
     transaction_source: TxSource,
     /// Runtime to run the workers
     runtime: Option<Runtime>,
-    /// Total execution time
-    total_execution_time: u64,
-    pub tx_left: u16,
-    pub tx_size_left: u32,
+    /// Total execution time left (used to determine gas_left)
+    execution_time_left: u64,
+    /// Total maximum of transactions left
+    tx_left: u16,
+    /// Total maximum of byte size left
+    tx_size_left: u32,
 }
 
 pub struct WorkerState {
@@ -79,7 +91,11 @@ impl<TxSource> Scheduler<TxSource>
 where
     TxSource: TransactionsSource,
 {
-    pub fn new(number_of_worker: usize, transaction_source: TxSource) -> Self {
+    pub fn new(
+        config: Config,
+        number_of_worker: usize,
+        transaction_source: TxSource,
+    ) -> Self {
         let mut workers_state = Vec::with_capacity(number_of_worker);
         for _ in 0..number_of_worker {
             workers_state.push(WorkerState {
@@ -98,27 +114,61 @@ where
             executing_contracts: vec![Vec::new(); number_of_worker],
             transaction_source,
             runtime: Some(runtime),
-            // TODO
-            total_execution_time: 0,
-            tx_left: 0,
-            tx_size_left: 0,
+            // TODO: change AS
+            execution_time_left: config.max_execution_time.as_millis() as u64,
+            tx_left: config.block_transaction_count_limit,
+            tx_size_left: config.block_transaction_size_limit,
+            config,
         }
     }
 
-    pub async fn run(&mut self) {
+    // TODO: Error type
+    pub async fn run(&mut self) -> Result<(), String> {
         let runtime = self.runtime.as_ref().unwrap();
+        let mut spent_time = Duration::ZERO;
+        // Store the futures of all the workers to be triggered when one of them finish
+        let mut futures = FuturesUnordered::new();
+
         // All workers starts empty, so we fetch the first batch
-        // for state in self.workers_state.iter_mut() {
-        //     let (batch, _) = self.transaction_source.get_executable_transactions(
-        //         state.gas_left,
-        //         state.tx_left,
-        //         state.tx_size_left,
-        //         Filter {
-        //             excluded_contract_ids: HashSet::new(),
-        //         },
-        //     );
-        //     runtime.spawn(async move {})
-        // }
+        let number_of_workers = self.workers_state.len();
+        let initial_gas = self
+            .config
+            .block_gas_limit
+            .checked_div(number_of_workers as u64)
+            .ok_or("Invalid block gas limit")?;
+        for (i, state) in self.workers_state.iter_mut().enumerate() {
+            let (batch, _) = self.transaction_source.get_executable_transactions(
+                initial_gas,
+                self.tx_left,
+                self.tx_size_left,
+                Filter {
+                    excluded_contract_ids: HashSet::new(),
+                },
+            );
+            futures.push(runtime.spawn({
+                let worker_id = i;
+                async move {
+                    // TODO: Execute the batch of transactions
+                    (worker_id, StorageChanges::default())
+                }
+            }));
+        }
         // Waiting for the workers to notify us
+        while !futures.is_empty() {
+            // Wait for the first worker to finish
+            let result = futures.next().await;
+            match result {
+                Some(Ok((worker_id, changes))) => {
+                    // Update the state of the worker
+                    self.workers_state[worker_id].status = WorkerStatus::Idle;
+                    self.workers_state[worker_id].state = changes;
+                }
+                _ => {
+                    return Err("Worker failed".to_string());
+                }
+            }
+            // Ask for new transactions to the transaction source
+        }
+        Ok(())
     }
 }
