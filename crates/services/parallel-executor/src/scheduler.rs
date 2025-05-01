e95fb8e17a1a9893f6fd62d80b8df8ff7d3fad98
AurelienFT <aurelien.foucault@epitech.eu>
2025-05-01 12:45:49 +0200

    All todos/implementation are here (except blobs)

--- crates/services/parallel-executor/src/scheduler.rs
+++ crates/services/parallel-executor/src/scheduler.rs
@@ -1,5 +1,29 @@
+//! The scheduler is responsible for managing the state of all the execution workers.
+//! His goal is to gather transactions for the transaction source and organize their execution
+//! through the different workers.
+//!
+//! There is few rules that need to be followed in order to produce a valid execution result:
+//! - The dependency chain of the input and output must be maintained across the block.
+//! - The constraints of the block (maximum number of transactions, maximum size, maximum gas, etc.) must be respected.
+//!
+//! Current design:
+//!
+//! The scheduler creates multiple workers. For each of this workers, the scheduler will ask to the transaction source
+//! to provide a batch of transactions that can be executed.
+//!
+//! When a thread has finished his execution then it will notify the scheduler that will re-ask for a new batch to the transaction source.
+//! This new batch mustn't contain any transaction that use a contract used in a batch of any other worker.
+//!
+//! For transactions without contracts, they are treat them as independent transactions. A verification is done at the end for the coin dependency chain.
+//! This can be done because we assume that the transaction pool is sending us transactions that are already correctly verified.
+//! If we have a transaction that end up being skipped (only possible cause if consensus parameters changes) then we will have to
+//! fallback a sequential execution of the transaction that used the skipped one as a dependency.
+
 use std::{
-    collections::HashSet,
+    collections::{
+        HashMap,
+        HashSet,
+    },
     time::Duration,
 };
 
@@ -7,9 +31,16 @@ use ::futures::{
     StreamExt,
     stream::FuturesUnordered,
 };
-use fuel_core_storage::transactional::{
-    Changes,
-    StorageChanges,
+use fuel_core_storage::{
+    StorageAsRef,
+    column::Column,
+    kv_store::KeyValueInspect,
+    tables::Coins,
+    transactional::{
+        Changes,
+        StorageChanges,
+        StorageTransaction,
+    },
 };
 use fuel_core_types::{
     blockchain::transaction::TransactionExt,
@@ -27,27 +58,7 @@ use crate::ports::{
     TransactionsSource,
 };
 
-/// The scheduler is responsible for managing the state of all the execution workers.
-/// His goal is to gather transactions for the transaction source and organize their execution
-/// through the different workers.
-///
-/// There is few rules that need to be followed in order to produce a valid execution result:
-/// - The dependency chain of the input and output must be maintained across the block.
-/// - The constraints of the block (maximum number of transactions, maximum size, maximum gas, etc.) must be respected.
-///
-/// Current design:
-///
-/// The scheduler creates multiple workers. For each of this workers, the scheduler will ask to the transaction source
-/// to provide a batch of transactions that can be executed.
-///
-/// When a thread has finished his execution then it will notify the scheduler that will re-ask for a new batch to the transaction source.
-/// This new batch mustn't contain any transaction that use a contract used in a batch of any other worker.
-///
-/// For transactions without contracts, they are treat them as independent transactions. A verification is done at the end for the coin dependency chain.
-/// This can be done because we assume that the transaction pool is sending us transactions that are already correctly verified.
-/// If we have a transaction that end up being skipped (only possible cause if consensus parameters changes) then we will have to
-/// fallback a sequential execution of the transaction that used the skipped one as a dependency.
-
+/// Config for the scheduler
 pub struct Config {
     block_gas_limit: u64,
     total_execution_time: Duration,
@@ -55,13 +66,15 @@ pub struct Config {
     block_transaction_count_limit: u16,
 }
 
-pub struct Scheduler<TxSource> {
+pub struct Scheduler<TxSource, D> {
     /// Config
     config: Config,
     /// The state of each worker
     workers_state: Vec<WorkerState>,
     /// Transaction source to ask for new transactions
     transaction_source: TxSource,
+    /// Database transaction for the execution
+    storage_transaction: StorageTransaction<D>,
     /// Runtime to run the workers
     runtime: Option<Runtime>,
     /// Total maximum of transactions left
@@ -86,9 +99,24 @@ pub enum WorkerStatus {
 
 type SkippedTransactions = Vec<MaybeCheckedTransaction>;
 
+struct WorkerExecutionResult {
+    /// The id of the worker
+    worker_id: usize,
+    /// The id of the batch of transactions
+    batch_id: usize,
+    /// The changes made by the worker used to commit them to the database at the end of execution
+    changes: Changes,
+    /// The coins created by the worker used to verify the coin dependency chain at the end of execution
+    coins_created: Vec<UtxoId>,
+    /// The coins used by the worker used to verify the coin dependency chain at the end of execution
+    coins_used: Vec<UtxoId>,
+    /// The transactions that were skipped by the worker
+    skipped_tx: SkippedTransactions,
+}
+
 // Shutdown the tokio runtime to avoid panic if executor is already
 //   used from another tokio runtime
-impl<TxSource> Drop for Scheduler<TxSource> {
+impl<TxSource, D> Drop for Scheduler<TxSource, D> {
     fn drop(&mut self) {
         if let Some(runtime) = self.runtime.take() {
             runtime.shutdown_background();
@@ -96,14 +124,16 @@ impl<TxSource> Drop for Scheduler<TxSource> {
     }
 }
 
-impl<TxSource> Scheduler<TxSource>
+impl<TxSource, D> Scheduler<TxSource, D>
 where
     TxSource: TransactionsSource,
+    D: KeyValueInspect<Column = Column>,
 {
     pub fn new(
         config: Config,
         number_of_worker: usize,
         transaction_source: TxSource,
+        storage_transaction: StorageTransaction<D>,
     ) -> Self {
         let mut workers_state = Vec::with_capacity(number_of_worker);
         for _ in 0..number_of_worker {
@@ -125,6 +155,7 @@ where
             tx_left: config.block_transaction_count_limit,
             tx_size_left: config.block_transaction_size_limit,
             config,
+            storage_transaction,
         }
     }
 
@@ -135,7 +166,10 @@ where
         let mut transactions_left_to_fetch = true;
         let new_tx_notifier = self.transaction_source.get_new_transactions_notifier();
         let deadline = now + self.config.total_execution_time;
-        let mut storage_changes = vec![];
+        // All executed transactions associated with their batch id
+        let mut execution_results = HashMap::new();
+        let mut nb_batch_created = 0;
+        let mut _current_nb_transactions = 0;
         // Store the futures of all the workers to be triggered when one of them finish
         let mut futures = FuturesUnordered::new();
 
@@ -147,11 +181,12 @@ where
             .checked_div(number_of_workers as u64)
             .ok_or("Invalid block gas limit")?;
         for (i, state) in self.workers_state.iter_mut().enumerate() {
-            let (batch, _) = self.transaction_source.get_executable_transactions(
+            let (batch, filtered) = self.transaction_source.get_executable_transactions(
                 initial_gas,
                 self.tx_left,
                 self.tx_size_left,
                 Filter {
+                    // TODO: take into account transaction of other workers
                     excluded_contract_ids: HashSet::new(),
                 },
             );
@@ -159,26 +194,33 @@ where
             // TODO: Maybe it's the transaction source that should gather these infos and the full size
             let (contracts_used, coins_used) = get_contracts_and_coins_used(&batch);
             self.tx_left -= batch.len() as u16;
+            // Useful to have a range of slots in the block to set tx pointers
+            _current_nb_transactions += batch.len() as u16;
             self.tx_size_left -= batch.iter().map(|tx| tx.size()).sum::<usize>() as u32;
 
-            if batch.is_empty() {
+            if batch.is_empty() && filtered == TransactionFiltered::NotFiltered {
                 // No more transactions to execute
                 // (none can be filtered out because we are the first batch)
+                // TODO: Wait for the notifier to tell us that we have new transactions
                 break;
             }
 
             futures.push(runtime.spawn({
                 let worker_id = i;
+                let batch_id = nb_batch_created;
                 async move {
                     // TODO: Execute the batch of transactions
-                    (
+                    WorkerExecutionResult {
                         worker_id,
-                        Changes::default(),
+                        batch_id,
+                        changes: Changes::default(),
+                        coins_created: vec![],
                         coins_used,
-                        SkippedTransactions::default(),
-                    )
+                        skipped_tx: vec![],
+                    }
                 }
             }));
+            nb_batch_created += 1;
             state.executing_contracts.extend(contracts_used);
 
             state.status = WorkerStatus::Executing;
@@ -189,21 +231,34 @@ where
                 // We have new transactions to execute
                 _ = new_tx_notifier.notified() => {
                     transactions_left_to_fetch = true;
+                    // TODO: If a worker is idle, we can ask him to fetch new transactions
                 }
                 result = futures.next() => {
                     match result {
-                        Some(Ok((worker_id, changes, _, skipped_tx))) => {
+                        Some(Ok(WorkerExecutionResult { worker_id, batch_id, changes, coins_created, coins_used, skipped_tx })) => {
                             if !skipped_tx.is_empty() {
                                 // TODO: Handle the skipped transactions
                                 // Wait for all the workers to finish gather all theirs transactions
                                 // re-execute them in one worker without skipped one.
                                 // Tell the TransactionSource that this transaction is skipped
-                                // to avoid sending new transactions that depend on it
+                                // to avoid sending new transactions that depend on it (using preconfirmation squeeze out)
                             }
                             // Update the state of the worker
                             self.workers_state[worker_id].status = WorkerStatus::Idle;
                             self.workers_state[worker_id].executing_contracts.clear();
-                            storage_changes.push(changes);
+                            // TODO: Save changes with a way to fetch them by contract id so that we get them when we execute and if
+                            // no contract is used we don't need anything and it's faster
+                            execution_results.insert(
+                                batch_id,
+                                WorkerExecutionResult {
+                                    worker_id,
+                                    batch_id,
+                                    changes,
+                                    coins_created,
+                                    coins_used,
+                                    skipped_tx,
+                                }
+                            );
                             if !transactions_left_to_fetch {
                                 // We have no more transactions to fetch
                                 continue 'outer;
@@ -218,6 +273,7 @@ where
                             // Time left in percentage to have the gas percentage left
                             // TODO: maybe try to remove "as"
                             let current_gas = initial_gas * ((1u128 - spent_time.as_millis() / self.config.total_execution_time.as_millis()) as u64);
+
                             let (batch, filtered) = self.transaction_source.get_executable_transactions(
                                 current_gas,
                                 self.tx_left,
@@ -226,8 +282,6 @@ where
                                     excluded_contract_ids: contracts_currently_used,
                                 },
                             );
-
-                            let (contracts_used, coins_used) = get_contracts_and_coins_used(&batch);
 
                             if batch.is_empty() {
                                 if filtered == TransactionFiltered::Filtered {
@@ -241,13 +295,23 @@ where
                                 }
                             }
 
+                            let (contracts_used, coins_used) = get_contracts_and_coins_used(&batch);
+
                             futures.push(runtime.spawn({
-                                let worker_id = worker_id;
+                                let batch_id = nb_batch_created;
                                 async move {
                                     // TODO: Execute the batch of transactions
-                                    (worker_id, Changes::default(), coins_used, SkippedTransactions::default())
+                                    WorkerExecutionResult {
+                                        worker_id,
+                                        batch_id,
+                                        changes: Changes::default(),
+                                        coins_created: vec![],
+                                        coins_used,
+                                        skipped_tx: vec![],
+                                    }
                                 }
                             }));
+                            nb_batch_created += 1;
                             self.workers_state[worker_id].executing_contracts = contracts_used;
                             self.workers_state[worker_id].status = WorkerStatus::Executing;
                         }
@@ -270,9 +334,25 @@ where
         // We need to merge the states of all the workers
         for future in futures {
             match future.await {
-                Ok((_, changes, _, _)) => {
-                    // TODO: Be careful ordering
-                    storage_changes.push(changes);
+                Ok(res) => {
+                    if !res.skipped_tx.is_empty() {
+                        // TODO: Handle the skipped transactions
+                        // Wait for all the workers to finish gather all theirs transactions
+                        // re-execute them in one worker without skipped one.
+                        // Tell the TransactionSource that this transaction is skipped
+                        // to avoid sending new transactions that depend on it
+                    }
+                    execution_results.insert(
+                        res.batch_id,
+                        WorkerExecutionResult {
+                            worker_id: res.worker_id,
+                            batch_id: res.batch_id,
+                            changes: res.changes,
+                            coins_created: res.coins_created,
+                            coins_used: res.coins_used,
+                            skipped_tx: res.skipped_tx,
+                        },
+                    );
                 }
                 Err(_) => {
                     return Err("Worker failed".to_string());
@@ -287,7 +367,57 @@ where
             );
         }
 
-        // TODO: Verify the coin dependency chain
+        // Verify the coin dependency chain
+        let mut storage_changes = vec![];
+        let mut compiled_created_coins = HashMap::new();
+        // TODO: Maybe verify the dependency chain inside of the batch also (which requires a bit of architectural changes)
+        // TODO: Maybe we also want to verify the amount
+        for batch_id in 0..nb_batch_created {
+            if let Some(changes) = execution_results.remove(&batch_id) {
+                // TODO: extend all the changes together
+
+                for coin in changes.coins_created {
+                    compiled_created_coins.insert(coin, batch_id);
+                }
+                for coin in changes.coins_used {
+                    // TODO: Maybe in a port
+                    match self.storage_transaction.storage::<Coins>().get(&coin) {
+                        Ok(Some(_)) => {
+                            // Coin is in the database
+                        }
+                        Ok(None) => {
+                            // Coin is not in the database
+                            match compiled_created_coins.get(&coin) {
+                                Some(coin_creation_batch_id) => {
+                                    // Coin is in the block
+                                    if coin_creation_batch_id <= &batch_id {
+                                        // Coin is created in a batch that is before the current one
+                                    } else {
+                                        // Coin is created in a batch that is after the current one
+                                        return Err(format!(
+                                            "Coin {coin} is created in a batch that is after the current one"
+                                        ));
+                                    }
+                                }
+                                None => {
+                                    return Err(format!(
+                                        "Coin {coin} is not in the database and not created in the block"
+                                    ));
+                                }
+                            }
+                        }
+                        Err(e) => {
+                            return Err(format!("Error while getting coin {coin}: {e}"));
+                        }
+                    }
+                }
+                storage_changes.push(changes.changes);
+            } else {
+                return Err(format!(
+                    "Batch {batch_id} not found in the execution results"
+                ));
+            }
+        }
 
         Ok(StorageChanges::ChangesList(storage_changes))
     }
