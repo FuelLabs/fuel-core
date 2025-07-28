77a38ab7bec2e2967a9779fa710a5b7c6d9da295
Mitch Turner <james.mitchell.turner@gmail.com>
2025-07-28 14:10:49 -0600

    Add test for mint mismatch

--- crates/fuel-core/src/executor.rs
+++ crates/fuel-core/src/executor.rs
@@ -3242,6 +3242,63 @@ mod tests {
         assert_eq!(mint.gas_price(), &gas_price);
     }
 
+    #[tokio::test]
+    async fn validate__will_fail_if_gas_price_does_not_match_expected_value() {
+        use fuel_core_executor::executor::{
+            TimeoutOnlyTxWaiter,
+            TransparentPreconfirmationSender,
+        };
+
+        // Given
+        let mut rng = StdRng::seed_from_u64(2322u64);
+        let base_asset_id = rng.r#gen();
+        let gas_price = 1000;
+
+        let tx = TransactionBuilder::script(vec![], vec![])
+            .add_unsigned_coin_input(
+                SecretKey::random(&mut rng),
+                rng.r#gen(),
+                4321,
+                base_asset_id,
+                Default::default(),
+            )
+            .finalize();
+
+        let config = Config {
+            forbid_fake_coins_default: false,
+            ..Default::default()
+        };
+        let exec = create_executor(Database::default(), config.clone());
+
+        // When
+        let res = exec
+            .produce_without_commit_with_source(
+                Components {
+                    header_to_produce: Default::default(),
+                    transactions_source: OnceTransactionsSource::new(vec![tx.into()]),
+                    gas_price,
+                    coinbase_recipient: [1u8; 32].into(),
+                },
+                TimeoutOnlyTxWaiter,
+                TransparentPreconfirmationSender,
+            )
+            .await
+            .unwrap()
+            .into_result();
+
+        // Then
+        let mut block = res.block;
+        let mint = block.transactions_mut().first_mut().unwrap();
+        let price = mint.as_mint_mut().unwrap().gas_price_mut();
+        *price = gas_price * 2;
+
+        let res = exec.validate(&block);
+        assert!(
+            matches!(res, Err(ExecutorError::BlockMismatch)),
+            "Expected BlockMismatch error, got: {res:?}"
+        );
+    }
+
     #[test]
     #[cfg(not(feature = "wasm-executor"))]
     fn block_producer_never_includes_more_than_max_tx_count_transactions() {
