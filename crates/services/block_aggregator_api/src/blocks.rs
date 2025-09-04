720694aa68bb07e8cacd5a01ab32350c5c072d9d
Mitch Turner <james.mitchell.turner@gmail.com>
2025-09-04 13:59:17 -0600

    Fix Debug impl and only use in test

--- crates/services/block_aggregator_api/src/blocks.rs
+++ crates/services/block_aggregator_api/src/blocks.rs
@@ -1,22 +1,58 @@
 use crate::result::Result;
+use bytes::Bytes;
+use std::fmt::{
+    Debug,
+    Formatter,
+};
 
 pub trait BlockSource: Send + Sync {
     fn next_block(&mut self) -> impl Future<Output = Result<(u64, Block)>> + Send;
 }
 
-#[derive(Clone, Debug, PartialEq, Eq)]
+#[derive(Clone, PartialEq, Eq)]
 pub struct Block {
-    bytes: Vec<u8>,
+    bytes: Bytes,
+}
+
+#[cfg(test)]
+impl Debug for Block {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        const BYTES_DISPLAY: usize = 8;
+        if self.bytes.len() <= BYTES_DISPLAY {
+            let bytes = &self
+                .bytes
+                .iter()
+                .map(|b| format!("{}", b))
+                .collect::<Vec<_>>();
+            let bytes_string = bytes.join(", ");
+            write!(f, "Block {{ bytes: [{}] }}", bytes_string)?;
+        } else {
+            let bytes_string = &self
+                .bytes
+                .iter()
+                .take(BYTES_DISPLAY)
+                .map(|b| format!("{}", b))
+                .collect::<Vec<_>>();
+            let bytes_string = bytes_string.join(", ");
+            let len = self.bytes.len();
+            write!(
+                f,
+                "Block {{ bytes: [{}, ...] (total {} bytes) }}",
+                bytes_string, len
+            )?;
+        }
+        Ok(())
+    }
 }
 
 impl Block {
-    pub fn new(bytes: Vec<u8>) -> Self {
+    pub fn new(bytes: Bytes) -> Self {
         Self { bytes }
     }
 
     #[cfg(test)]
     pub fn arb_size<Rng: rand::Rng + ?Sized>(rng: &mut Rng, size: usize) -> Self {
-        let bytes: Vec<u8> = (0..size).map(|_| rng.r#gen()).collect();
+        let bytes: Bytes = (0..size).map(|_| rng.r#gen()).collect();
         Self::new(bytes)
     }
 
@@ -32,7 +68,8 @@ impl Block {
 }
 
 impl From<Vec<u8>> for Block {
-    fn from(bytes: Vec<u8>) -> Self {
+    fn from(value: Vec<u8>) -> Self {
+        let bytes = Bytes::from(value);
         Self::new(bytes)
     }
 }
