6649ac6acd9e344a6419ed60cf84c4e04d6dbf34
Mitch Turner <james.mitchell.turner@gmail.com>
2025-09-08 20:34:38 -0600

    Add tests for get_current_height indirectly

--- crates/services/block_aggregator_api/src/db/storage_db/tests.rs
+++ crates/services/block_aggregator_api/src/db/storage_db/tests.rs
@@ -32,7 +32,7 @@ async fn store_block__adds_to_storage() {
 
     // then
     let actual = adapter
-        .inner
+        .storage
         .storage_as_ref::<Blocks>()
         .get(&height)
         .unwrap()
@@ -81,4 +81,71 @@ async fn get_block__can_get_expected_range() {
 
     // then
     assert_eq!(actual, vec![expected_2, expected_3]);
+}
+
+#[tokio::test]
+async fn store_block__updates_the_highest_continuous_block_if_contiguous() {
+    let mut rng = StdRng::seed_from_u64(666);
+    // given
+    let db = database();
+    let mut adapter = StorageDB::new_with_height(db, BlockHeight::from(0u32));
+    let height = BlockHeight::from(1u32);
+    let expected = Block::random(&mut rng);
+
+    // when
+    adapter.store_block(height, expected.clone()).await.unwrap();
+
+    // then
+    let expected = height;
+    let actual = adapter.get_current_height().await.unwrap();
+    assert_eq!(expected, actual);
+}
+
+#[tokio::test]
+async fn store_block__does_not_update_the_highest_continuous_block_if_not_contiguous() {
+    let mut rng = StdRng::seed_from_u64(666);
+    // given
+    let db = database();
+    let starting_height = BlockHeight::from(0u32);
+    let mut adapter = StorageDB::new_with_height(db, starting_height);
+    let height = BlockHeight::from(2u32);
+    let expected = Block::random(&mut rng);
+
+    // when
+    adapter.store_block(height, expected.clone()).await.unwrap();
+
+    // then
+    let expected = starting_height;
+    let actual = adapter.get_current_height().await.unwrap();
+    assert_eq!(expected, actual);
+}
+
+#[tokio::test]
+async fn store_block__updates_the_highest_continuous_block_if_filling_a_gap() {
+    let mut rng = StdRng::seed_from_u64(666);
+    // given
+    let db = database();
+    let starting_height = BlockHeight::from(0u32);
+    let mut adapter = StorageDB::new_with_height(db, starting_height);
+
+    let mut orphaned_height = None;
+    for height in 2..=10u32 {
+        let height = BlockHeight::from(height);
+        orphaned_height = Some(height);
+        let block = Block::random(&mut rng);
+        adapter.store_block(height, block).await.unwrap();
+    }
+    let expected = starting_height;
+    let actual = adapter.get_current_height().await.unwrap();
+    assert_eq!(expected, actual);
+
+    // when
+    let height = BlockHeight::from(1u32);
+    let expected = Block::random(&mut rng);
+    adapter.store_block(height, expected.clone()).await.unwrap();
+
+    // then
+    let expected = orphaned_height.unwrap();
+    let actual = adapter.get_current_height().await.unwrap();
+    assert_eq!(expected, actual);
 }
