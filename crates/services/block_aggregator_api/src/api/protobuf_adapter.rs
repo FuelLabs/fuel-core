aad009a593654ac1b81988117060433358b58ef3
Mitch Turner <james.mitchell.turner@gmail.com>
2025-09-16 12:33:10 -0600

    Add failing test

--- crates/services/block_aggregator_api/src/api/protobuf_adapter.rs
+++ crates/services/block_aggregator_api/src/api/protobuf_adapter.rs
@@ -64,7 +64,7 @@ impl BlockAggregator for Server {
 }
 
 pub struct ProtobufAPI {
-    server_task_handle: tokio::task::JoinHandle<()>,
+    _server_task_handle: tokio::task::JoinHandle<()>,
     query_receiver: tokio::sync::mpsc::Receiver<BlockAggregatorQuery<BlockRangeResponse>>,
 }
 
@@ -74,7 +74,7 @@ impl ProtobufAPI {
             tokio::sync::mpsc::channel::<BlockAggregatorQuery<BlockRangeResponse>>(100);
         let server = Server::new(query_sender);
         let addr = url.parse().unwrap();
-        let server_task_handle = tokio::spawn(async move {
+        let _server_task_handle = tokio::spawn(async move {
             tonic::transport::Server::builder()
                 .add_service(block_aggregator_server::BlockAggregatorServer::new(server))
                 .serve(addr)
@@ -82,7 +82,7 @@ impl ProtobufAPI {
                 .unwrap();
         });
         Self {
-            server_task_handle,
+            _server_task_handle,
             query_receiver,
         }
     }
@@ -108,8 +108,14 @@ pub struct ProtobufClient;
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::blocks::Block;
     use block_aggregator_client::BlockAggregatorClient;
+    use bytes::Bytes;
     use fuel_core_types::fuel_types::BlockHeight;
+    use futures::{
+        StreamExt,
+        TryStreamExt,
+    };
 
     #[tokio::test]
     async fn await_query__get_current_height__client_receives_expected_value() {
@@ -153,6 +159,60 @@ mod tests {
 
     #[tokio::test]
     async fn await_query__get_block_range__client_receives_expected_value() {
-        todo!()
+        let _ = tracing_subscriber::fmt()
+            .with_max_level(tracing::Level::DEBUG)
+            .init();
+        // given
+        let path = "[::1]:50051";
+        let mut api = ProtobufAPI::new(path.to_string());
+        tokio::time::sleep(std::time::Duration::from_millis(100)).await;
+
+        // call get current height endpoint with client
+        let url = "http://[::1]:50051";
+        let mut client = BlockAggregatorClient::connect(url.to_string())
+            .await
+            .expect("could not connect to server");
+        let request = BlockRangeRequest { start: 0, end: 1 };
+        let handle = tokio::spawn(async move {
+            tracing::info!("querying with client");
+            client
+                .get_block_range(request)
+                .await
+                .expect("could not get height")
+        });
+
+        // when
+        tracing::info!("awaiting query");
+        let query = api.await_query().await.unwrap();
+
+        // then
+        let block1 = Block::new(Bytes::from(vec![0u8; 100]));
+        let block2 = Block::new(Bytes::from(vec![1u8; 100]));
+        let list = vec![block1, block2];
+        // return response through query's channel
+        if let BlockAggregatorQuery::GetBlockRange {
+            first,
+            last,
+            response,
+        } = query
+        {
+            let stream = tokio_stream::iter(list.clone()).boxed();
+            let range = BlockRangeResponse::Literal(stream);
+            response.send(range).unwrap();
+        } else {
+            panic!("expected GetBlockRange query");
+        }
+        let response = handle.await.unwrap();
+        let expected: Vec<Vec<u8>> = list.iter().map(|b| b.bytes().to_vec()).collect();
+        let actual: Vec<Vec<u8>> = response
+            .into_inner()
+            .try_collect::<Vec<_>>()
+            .await
+            .unwrap()
+            .into_iter()
+            .map(|b| b.data.to_vec())
+            .collect();
+
+        assert_eq!(expected, actual);
     }
 }
--- crates/services/block_aggregator_api/src/block_range_response.rs
+++ crates/services/block_aggregator_api/src/block_range_response.rs
@@ -1,5 +1,8 @@
 use crate::blocks::Block;
-use fuel_core_services::stream::BoxStream;
+use fuel_core_services::stream::Stream;
+use std::fmt;
+
+pub type BoxStream<T> = core::pin::Pin<Box<dyn Stream<Item = T> + Send + 'static>>;
 
 /// The response to a block range query, either as a literal stream of blocks or as a remote URL
 pub enum BlockRangeResponse {
@@ -7,4 +10,16 @@ pub enum BlockRangeResponse {
     Literal(BoxStream<Block>),
     /// A remote URL where the blocks can be fetched
     Remote(String),
+}
+
+#[cfg(test)]
+impl std::fmt::Debug for BlockRangeResponse {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            BlockRangeResponse::Literal(_) => f.debug_struct("Literal").finish(),
+            BlockRangeResponse::Remote(url) => {
+                f.debug_struct("Remote").field("url", url).finish()
+            }
+        }
+    }
 }
