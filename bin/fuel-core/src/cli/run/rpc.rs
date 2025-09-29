dd998811bdfe21cc477a95965a11597e936fb52e
Mitch Turner <james.mitchell.turner@gmail.com>
2025-09-29 10:43:28 -0600

    improve comments on cli args

--- bin/fuel-core/src/cli/run/rpc.rs
+++ bin/fuel-core/src/cli/run/rpc.rs
@@ -3,10 +3,11 @@ use std::net;
 
 #[derive(Debug, Clone, Args)]
 pub struct RpcArgs {
+    /// The IP address to bind the RPC service to
     #[clap(long = "ip", default_value = "127.0.0.1", value_parser, env)]
     pub ip: net::IpAddr,
 
-    /// The port to bind the GraphQL service to.
+    /// The port to bind the RPC service to
     #[clap(long = "port", default_value = "4000", env)]
     pub port: u16,
 }
