a5e11f3c4016c2c2ca316bc0c2dd7e59b137e8fb
Mitch Turner <james.mitchell.turner@gmail.com>
2025-09-30 11:40:28 -0600

    fmt

--- crates/services/block_aggregator_api/src/db/remote_cache.rs
+++ crates/services/block_aggregator_api/src/db/remote_cache.rs
@@ -1,22 +1,35 @@
+use crate::{
+    block_range_response::BlockRangeResponse,
+    blocks::Block,
+    db::BlockStorage,
+};
 use fuel_core_types::fuel_types::BlockHeight;
-use crate::block_range_response::BlockRangeResponse;
-use crate::blocks::Block;
-use crate::db::BlockStorage;
 
 pub struct RemoteCache;
 
 impl BlockStorage for RemoteCache {
     type BlockRangeResponse = BlockRangeResponse;
 
-    fn store_block(&mut self, height: BlockHeight, block: Block) -> impl Future<Output=crate::result::Result<()>> + Send {
+    fn store_block(
+        &mut self,
+        height: BlockHeight,
+        block: Block,
+    ) -> impl Future<Output = crate::result::Result<()>> + Send {
         todo!()
     }
 
-    fn get_block_range(&self, first: BlockHeight, last: BlockHeight) -> impl Future<Output=crate::result::Result<Self::BlockRangeResponse>> + Send {
+    fn get_block_range(
+        &self,
+        first: BlockHeight,
+        last: BlockHeight,
+    ) -> impl Future<Output = crate::result::Result<Self::BlockRangeResponse>> + Send
+    {
         todo!()
     }
 
-    fn get_current_height(&self) -> impl Future<Output=crate::result::Result<BlockHeight>> + Send {
+    fn get_current_height(
+        &self,
+    ) -> impl Future<Output = crate::result::Result<BlockHeight>> + Send {
         todo!()
     }
 }
