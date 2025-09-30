d7e75e169cd71967d1a278a9de8d38672e7d2516
Mitch Turner <james.mitchell.turner@gmail.com>
2025-09-30 11:49:44 -0600

    Make clap args unique

--- bin/fuel-core/src/cli/run/rpc.rs
+++ bin/fuel-core/src/cli/run/rpc.rs
@@ -4,18 +4,18 @@ use std::net;
 #[derive(Debug, Clone, Args)]
 pub struct RpcArgs {
     /// The IP address to bind the RPC service to
-    #[clap(long = "ip", default_value = "127.0.0.1", value_parser, env)]
-    pub ip: net::IpAddr,
+    #[clap(long = "rpc_ip", default_value = "127.0.0.1", value_parser, env)]
+    pub rpc_ip: net::IpAddr,
 
     /// The port to bind the RPC service to
-    #[clap(long = "port", default_value = "4000", env)]
-    pub port: u16,
+    #[clap(long = "rpc_port", default_value = "4001", env)]
+    pub rpc_port: u16,
 }
 
 impl RpcArgs {
     pub fn into_config(self) -> fuel_block_aggregator_api::integration::Config {
         fuel_block_aggregator_api::integration::Config {
-            addr: net::SocketAddr::new(self.ip, self.port),
+            addr: net::SocketAddr::new(self.rpc_ip, self.rpc_port),
         }
     }
 }
