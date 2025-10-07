0bbe62a0b21cc156a91d6cfcfc38c9d773630e71
Mitch Turner <james.mitchell.turner@gmail.com>
2025-10-07 12:41:39 -0600

    WIP add proto types for block w/header & txs

--- crates/services/block_aggregator_api/src/blocks/importer_and_db_source/serializer_adapter.rs
+++ crates/services/block_aggregator_api/src/blocks/importer_and_db_source/serializer_adapter.rs
@@ -1,14 +1,26 @@
 use crate::{
-    blocks::{
-        Block,
-        importer_and_db_source::BlockSerializer,
-    },
+    blocks::importer_and_db_source::BlockSerializer,
     result::Error,
 };
 
-use crate::protobuf_types::Block as ProtoBlock;
+use crate::protobuf_types::{
+    Block as ProtoBlock,
+    Header as ProtoHeader,
+    Transaction as ProtoTransaction,
+    V1Block as ProtoV1Block,
+    block::VersionedBlock as ProtoVersionedBlock,
+};
 use anyhow::anyhow;
-use fuel_core_types::blockchain::block::Block as FuelBlock;
+use fuel_core_types::{
+    blockchain::{
+        block::{
+            Block as FuelBlock,
+            BlockV1,
+        },
+        header::BlockHeader,
+    },
+    fuel_tx::Transaction as FuelTransaction,
+};
 use postcard::to_allocvec;
 
 #[derive(Clone)]
@@ -18,10 +30,27 @@ impl BlockSerializer for SerializerAdapter {
     type Block = ProtoBlock;
 
     fn serialize_block(&self, block: &FuelBlock) -> crate::result::Result<Self::Block> {
-        let data = to_allocvec(block).map_err(|e| {
-            Error::BlockSource(anyhow!("failed to serialize block: {}", e))
-        })?;
-        let pb = ProtoBlock { data };
-        Ok(pb)
+        // TODO: Should this be owned to begin with?
+        let (header, txs) = block.clone().into_inner();
+        let proto_header = proto_header_from_header(header);
+        match &block {
+            FuelBlock::V1(_) => {
+                let proto_v1_block = ProtoV1Block {
+                    header: Some(proto_header),
+                    transactions: txs.into_iter().map(proto_tx_from_tx).collect(),
+                };
+                Ok(ProtoBlock {
+                    versioned_block: Some(ProtoVersionedBlock::V1(proto_v1_block)),
+                })
+            }
+        }
     }
+}
+
+fn proto_header_from_header(header: BlockHeader) -> ProtoHeader {
+    todo!()
+}
+
+fn proto_tx_from_tx(tx: FuelTransaction) -> ProtoTransaction {
+    todo!()
 }
