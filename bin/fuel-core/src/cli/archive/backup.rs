71b1d50c87a380105aa093388ee685b96c2f5b78
Saeed Dadkhah <sa.dadkhah@gmail.com>
2025-11-03 13:25:31 +0330

    Add archive to the features of fuel-core

--- bin/fuel-core/src/cli/archive/backup.rs
+++ bin/fuel-core/src/cli/archive/backup.rs
@@ -1,15 +1,23 @@
-use crate::cli::archive::add_to_archive;
 use fuel_core::combined_database::CombinedDatabase;
-use tempfile::TempDir;
 
+#[cfg(not(feature = "archive"))]
 pub fn backup(db_dir: &str, backup_path: &str) -> anyhow::Result<()> {
-    let tmp_backup_dir = TempDir::new()?;
+    let backup_dir = std::path::Path::new(backup_path);
+    let db_dir = std::path::Path::new(db_dir);
+    CombinedDatabase::backup(db_dir, backup_dir)?;
+
+    Ok(())
+}
+
+#[cfg(feature = "archive")]
+pub fn backup(db_dir: &str, backup_path: &str) -> anyhow::Result<()> {
+    let tmp_backup_dir = tempfile::TempDir::new()?;
     let db_dir = std::path::Path::new(db_dir);
     let backup_file = std::path::Path::new(backup_path);
 
     let path = tmp_backup_dir.path();
     CombinedDatabase::backup(db_dir, path)?;
-    add_to_archive(path, backup_file)?;
+    crate::cli::archive::add_to_archive(path, backup_file)?;
 
     Ok(())
 }
--- bin/fuel-core/src/cli/archive/restore.rs
+++ bin/fuel-core/src/cli/archive/restore.rs
@@ -1,14 +1,22 @@
-use crate::cli::archive::extract_from_archive;
 use fuel_core::combined_database::CombinedDatabase;
-use tempfile::TempDir;
 
+#[cfg(not(feature = "archive"))]
+pub fn restore(restore_to: &str, backup_path: &str) -> anyhow::Result<()> {
+    let backup_dir = std::path::Path::new(backup_path);
+    let restore_to = std::path::Path::new(restore_to);
+    CombinedDatabase::restore(restore_to, backup_dir)?;
+
+    Ok(())
+}
+
+#[cfg(feature = "archive")]
 pub fn restore(restore_to: &str, backup_path: &str) -> anyhow::Result<()> {
     let backup_path = std::path::Path::new(backup_path);
     let restore_to = std::path::Path::new(restore_to);
-    let tmp_backup_dir = TempDir::new()?;
+    let tmp_backup_dir = tempfile::TempDir::new()?;
 
     let path = tmp_backup_dir.path();
-    extract_from_archive(backup_path, path)?;
+    crate::cli::archive::extract_from_archive(backup_path, path)?;
     CombinedDatabase::restore(restore_to, path)?;
 
     Ok(())
