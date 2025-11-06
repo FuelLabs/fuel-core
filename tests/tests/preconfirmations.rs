e739e0c635a3d79f085d608c18f3fe2c3de33245
Saeed Dadkhah <sa.dadkhah@gmail.com>
2025-11-06 12:25:35 +0330

    Fix test.

--- tests/tests/preconfirmations.rs
+++ tests/tests/preconfirmations.rs
@@ -226,7 +226,7 @@ async fn preconfirmation__received_after_failed_execution() {
             reason: _,
         } => {
             // Then
-            assert_eq!(tx_pointer, TxPointer::new(BlockHeight::new(1), 1));
+            assert_eq!(tx_pointer, TxPointer::new(BlockHeight::new(1), 0));
             assert_eq!(transaction_id, tx_id);
             let receipts = receipts.unwrap();
             assert_eq!(receipts.len(), 3);
