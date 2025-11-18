7e76298e7e433632577337cc3d325b9f9ee4f522
Saeed Dadkhah <sa.dadkhah@gmail.com>
2025-11-18 12:59:20 +0330

    Add some comments.

--- bin/fuel-core/src/cli/archive.rs
+++ bin/fuel-core/src/cli/archive.rs
@@ -6,31 +6,46 @@ use fuel_core::combined_database::CombinedDatabase;
 
 #[derive(Debug, Subcommand)]
 pub enum Command {
+    /// Back up the database into a tar archive
     Backup(BackupArgs),
+    /// Restore the database from a tar archive
     Restore(RestoreArgs),
 }
 
+pub const DEFAULT_DB_TAR_FILE: &str = "db.tar";
 #[derive(Debug, Parser)]
 pub struct BackupArgs {
-    #[arg(long)]
+    /// Database directory to back up
+    #[arg(long, help = "The source directory of the database to backup")]
     pub from: String,
 
-    #[arg(long)]
+    /// Destination directory where the tar files will be created.
+    #[arg(long, help = "The destination directory for the backup tar files")]
     pub to: String,
+
+    /// Name of the final tar file (default: db.tar)
+    #[arg(
+        long,
+        default_value = DEFAULT_DB_TAR_FILE,
+        help = "The name of the tar file to create"
+    )]
+    pub file_name: String,
 }
 
 #[derive(Debug, Parser)]
 pub struct RestoreArgs {
-    #[arg(long)]
+    /// Path to the backup tar file to restore from
+    #[arg(long, help = "The source tar file to restore data from")]
     pub from: String,
 
-    #[arg(long)]
+    /// Destination directory where the database will be restored
+    #[arg(long, help = "The target directory to restore the database into")]
     pub to: String,
 }
 
 pub fn exec(command: Command) -> anyhow::Result<()> {
     match command {
-        Command::Backup(args) => backup(&args.from, &args.to),
+        Command::Backup(args) => backup(&args.from, &args.to, &args.file_name),
         Command::Restore(args) => restore(&args.to, &args.from),
     }
 }
@@ -42,10 +57,14 @@ pub fn backup(db_dir: &str, backup_path: &str) -> anyhow::Result<()> {
 }
 
 #[cfg(feature = "archive")]
-pub fn backup(db_dir: &str, backup_path: &str) -> anyhow::Result<()> {
+pub fn backup(
+    db_dir: &str,
+    backup_path: &str,
+    tar_file_name: &str,
+) -> anyhow::Result<()> {
     let tmp_dir = tempfile::TempDir::new()?;
     CombinedDatabase::backup(db_dir.as_ref(), tmp_dir.path())?;
-    archiver::add_to_archive(tmp_dir.path(), backup_path.as_ref())?;
+    archiver::add_to_archive(tmp_dir.path(), backup_path.as_ref(), tar_file_name)?;
     Ok(())
 }
 
@@ -88,7 +107,11 @@ mod archiver {
         Header,
     };
 
-    pub fn add_to_archive(src_dir: &Path, dest_path: &Path) -> anyhow::Result<()> {
+    pub fn add_to_archive(
+        src_dir: &Path,
+        dest_path: &Path,
+        db_file_name: &str,
+    ) -> anyhow::Result<()> {
         let top_level_dirs = fs::read_dir(src_dir)?
             .filter_map(|entry| {
                 let entry = entry.ok()?;
@@ -135,7 +158,8 @@ mod archiver {
             })
             .collect::<Result<Vec<_>, _>>()?;
 
-        let mut final_writer = BufWriter::new(File::create(dest_path.join("db.tar"))?);
+        let mut final_writer =
+            BufWriter::new(File::create(dest_path.join(db_file_name))?);
         for tar_path in tar_paths {
             let mut reader = BufReader::new(File::open(tar_path)?);
             std::io::copy(&mut reader, &mut final_writer)?;
