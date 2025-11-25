183aee83018bfc3dbf854307e30cb56eb3f75e71
Saeed Dadkhah <sa.dadkhah@gmail.com>
2025-11-25 19:09:13 +0330

    Refactor FuelClient::with_urls to accept a slice of string references.

--- crates/client/src/client.rs
+++ crates/client/src/client.rs
@@ -300,7 +300,12 @@ impl FuelClient {
         Self::from_str(url.as_ref())
     }
 
-    pub fn with_urls(urls: Vec<Url>) -> anyhow::Result<Self> {
+    pub fn with_urls(urls: &[impl AsRef<str>]) -> anyhow::Result<Self> {
+        let urls = urls
+            .iter()
+            .map(|url| Url::parse(url.as_ref()))
+            .collect::<Result<Vec<_>, _>>()
+            .context("failed to parse provided URLs")?;
         Ok(Self {
             transport: FailoverTransport::new(urls)?,
             require_height: ConsistencyPolicy::Auto {
