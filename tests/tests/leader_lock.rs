992beca1fe1a6dfdc29419b92b5dd4fc996e405a
Mitch Turner <james.mitchell.turner@gmail.com>
2026-02-18 16:04:46 -0700

    Add longer timout for test

--- tests/tests/leader_lock.rs
+++ tests/tests/leader_lock.rs
@@ -96,7 +96,7 @@ async fn leader_lock__four_producers__only_first_leader_produces_blocks() {
 async fn leader_lock__three_producers__leadership_handoffs_are_exclusive() {
     const BLOCK_TIME: Duration = Duration::from_millis(200);
     const LEADER_ELECTION_TIMEOUT: Duration = Duration::from_secs(2);
-    const BLOCK_IMPORT_TIMEOUT: Duration = Duration::from_millis(300);
+    const BLOCK_IMPORT_TIMEOUT: Duration = Duration::from_millis(500);
     const PHASE_BLOCKS: usize = 5;
     const STOP_TIMEOUT: Duration = Duration::from_secs(1);
 
